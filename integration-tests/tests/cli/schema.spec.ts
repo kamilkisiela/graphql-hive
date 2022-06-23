5de7e381a665abe54d670adeff297e319f6d1c7d
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-06-23 11:21:49 +0200

    Support SchemaPublishMissingUrlError in CLI (#157)
    This PR adds a dedicated error message with instructions when a service URL is missing (federated projects) thanks to the recently added `SchemaPublishMissingUrlError` type.

--- integration-tests/tests/cli/schema.spec.ts
+++ integration-tests/tests/cli/schema.spec.ts
@@ -137,3 +137,61 @@ test('service url should be available in supergraph', async () => {
 
   expect(supergraph.body).toMatch('(name: "users" url: "https://api.com/users-subgraph")');
 });
+
+test('service url should be required in Federation', async () => {
+  const { access_token: owner_access_token } = await authenticate('main');
+  const orgResult = await createOrganization(
+    {
+      name: 'foo',
+    },
+    owner_access_token
+  );
+  const org = orgResult.body.data!.createOrganization.ok!.createdOrganizationPayload.organization;
+  const code = org.inviteCode;
+
+  // Join
+  const { access_token: member_access_token } = await authenticate('extra');
+  await joinOrganization(code, member_access_token);
+
+  const projectResult = await createProject(
+    {
+      organization: org.cleanId,
+      type: ProjectType.Federation,
+      name: 'foo',
+    },
+    owner_access_token
+  );
+
+  const project = projectResult.body.data!.createProject.ok!.createdProject;
+  const target = projectResult.body.data!.createProject.ok!.createdTarget;
+
+  // Create a token with write rights
+  const writeTokenResult = await createToken(
+    {
+      name: 'test',
+      organization: org.cleanId,
+      project: project.cleanId,
+      target: target.cleanId,
+      organizationScopes: [],
+      projectScopes: [],
+      targetScopes: [TargetAccessScope.RegistryRead, TargetAccessScope.RegistryWrite],
+    },
+    owner_access_token
+  );
+  expect(writeTokenResult.body.errors).not.toBeDefined();
+  const writeToken = writeTokenResult.body.data!.createToken.ok!.secret;
+
+  await expect(
+    schemaPublish([
+      '--token',
+      writeToken,
+      '--author',
+      'Kamil',
+      '--commit',
+      'abc123',
+      '--service',
+      'users',
+      'fixtures/federation-init.graphql',
+    ])
+  ).rejects.toThrowError('EXIT: 1');
+});
