086d972dcd2519816bb056598238e1297e43a4f3
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-06-30 13:57:40 +0200

    Use different url-prefix when publishing sourcemaps (#214)

---
+++ scripts/upload-sourcmaps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for dir in packages/services/*/dist; do
+  name=$(echo $dir | awk -F / '{print $3}')
+  yarn sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $dir --no-rewrite --no-sourcemap-reference --url-prefix /app/node_modules/\@hive/$name \;
+done
+
+for dir in packages/web/*/dist; do
+  name=$(echo $dir | awk -F / '{print $3}')
+  yarn sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $dir --no-rewrite --no-sourcemap-reference --url-prefix /app/node_modules/\@hive/$name \;
+done
