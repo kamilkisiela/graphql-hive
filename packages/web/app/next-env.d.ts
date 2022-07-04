170b51d5e72a91c8b94a64ef767372b065a05d34
Laurin Quast <laurinquast@googlemail.com>
2022-07-04 10:57:25 +0200

    fix: pages/api/github/callback.ts

--- packages/web/app/next-env.d.ts
+++ packages/web/app/next-env.d.ts
@@ -3,3 +3,9 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    APP_BASE_URL: string;
+  }
+}
