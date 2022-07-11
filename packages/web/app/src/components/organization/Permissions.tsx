kamilkisiela/graphql-hive

commit f6ccd015ae2930aba0465c7e42a3b5ef45731f5c

hive/app: strict null checks (#220)
* fix src/lib/urql-cache.ts

* fix src/lib/mixpanel.ts

* fix: src/lib/hooks/use-route-selector.ts

* fix: src/lib/access/common.ts

* fix: src/components/v2/time-ago.tsx

* fix: src/components/v2/skeleton.tsx

* fix: src/components/v2/modals/delete-organization.tsx (verified functionality)

* fix: src/components/v2/modals/create-target.tsx

* fix: src/components/v2/modals/create-project.tsx

* fix: src/components/v2/modals/create-organization.tsx

* fix: src/components/v2/modals/create-channel.tsx

* fix: src/components/v2/modals/create-alert.tsx

* fix: packages/web/app/src/components/v2/dropdown.tsx

* fix: src/components/v2/data-wrapper.tsx

* fix: src/components/target/operations/Filters.tsx

* fix: pages/[orgId]/[projectId]/[targetId]/history.tsx

* fix: src/components/project/Switcher.tsx

* fix: src/components/organization/Switcher.tsx

* fix: src/components/organization/Permissions.tsx

* fix: pages/settings.tsx

* fix: packages/web/app/src/components/v2/tabs.tsx

* fix: packages/web/app/src/components/v2/avatar.tsx

* fix: packages/web/app/pages/join/[inviteCode].tsx

* fix: packages/web/app/pages/[orgId]/index.tsx

* fix: pages/[orgId]/members.tsx

* fix: pages/[orgId]/settings.tsx

* fix:  pages/[orgId]/subscription/index.tsx

* fix: pages/api/github/callback.ts

* fix: pages/api/github/connect/[orgId].ts

* fix: src/components/common/Navigation.tsx

* fix: src/components/common/UserSettings.tsx

* fix: src/components/common/LoadingAPI.tsx

* fix: packages/web/app/src/components/common/Feedback.tsx

* fix: packages/web/app/src/components/common/DataWrapper.tsx

* fix: src/components/admin/AdminStats.tsx

* fix: pages/index.tsx

* fix: pages/api/proxy.ts

* fix: src/components/auth/AuthProvider.tsx

* fix: pages/[orgId]/[projectId]/index.tsx

* fix:  pages/[orgId]/[projectId]/settings.tsx

packages/web/app/src/components/organization/Permissions.tsx
@@ -47,6 +47,10 @@ function matchScope<T>(list: readonly T[], defaultValue: string, lowerPriority?:
   return defaultValue;
 }
 
+function isBoolean(value: unknown): value is boolean {
+  return typeof value === 'boolean';
+}
+
 function PermissionsSpaceInner(props: Props<OrganizationAccessScope>): React.ReactElement<any, any>;
 function PermissionsSpaceInner(props: Props<ProjectAccessScope>): React.ReactElement<any, any>;
 function PermissionsSpaceInner(props: Props<TargetAccessScope>): React.ReactElement<any, any>;
@@ -62,13 +66,15 @@ function PermissionsSpaceInner<T extends OrganizationAccessScope | ProjectAccess
         <div tw="divide-y-2 divide-gray-100">
           {scopes.map(scope => {
             const possibleScope = [scope.mapping['read-only'], scope.mapping['read-write']].filter(Boolean);
-            const canManageScope = possibleScope.some(checkAccess);
+            const canManageScope = possibleScope.some(isBoolean);
 
             if (!canManageScope) {
               return null;
             }
 
-            const hasReadOnly = !!scope.mapping['read-only'];
+            const readOnlyScope = scope.mapping['read-only'];
+            const hasReadOnly = typeof readOnlyScope !== 'undefined';
+
             const selectedScope = matchScope(
               initialScopes,
               NoAccess,
@@ -103,11 +109,11 @@ function PermissionsSpaceInner<T extends OrganizationAccessScope | ProjectAccess
 
                           if (hasReadOnly) {
                             // Include read-only as well
-                            newScopes.push(scope.mapping['read-only']);
+                            newScopes.push(readOnlyScope);
                           }
-                        } else {
+                        } else if (readOnlyScope) {
                           // just read-only
-                          newScopes.push(scope.mapping['read-only']);
+                          newScopes.push(readOnlyScope);
                         }
 
                         onChange(newScopes);

commit aebbd17b7c028ca7fc0bfdee779e5145c34a1e47

Fix missing scopes when generating a token
Bug introduced by #220

packages/web/app/src/components/organization/Permissions.tsx
@@ -47,8 +47,8 @@ function matchScope<T>(list: readonly T[], defaultValue: string, lowerPriority?:
   return defaultValue;
 }
 
-function isBoolean(value: unknown): value is boolean {
-  return typeof value === 'boolean';
+function isDefined<T>(value: T | null | undefined): value is T {
+  return typeof value !== 'undefined' && value !== null;
 }
 
 function PermissionsSpaceInner(props: Props<OrganizationAccessScope>): React.ReactElement<any, any>;
@@ -65,8 +65,8 @@ function PermissionsSpaceInner<T extends OrganizationAccessScope | ProjectAccess
       <AccordionPanel pb={4}>
         <div tw="divide-y-2 divide-gray-100">
           {scopes.map(scope => {
-            const possibleScope = [scope.mapping['read-only'], scope.mapping['read-write']].filter(Boolean);
-            const canManageScope = possibleScope.some(isBoolean);
+            const possibleScope = [scope.mapping['read-only'], scope.mapping['read-write']].filter(isDefined);
+            const canManageScope = possibleScope.some(checkAccess);
 
             if (!canManageScope) {
               return null;
@@ -96,7 +96,7 @@ function PermissionsSpaceInner<T extends OrganizationAccessScope | ProjectAccess
                       const value = event.target.value as T | string;
 
                       if (value === NoAccess) {
-                        // Remove all posible scopes
+                        // Remove all possible scopes
                         onChange(initialScopes.filter(scope => !possibleScope.includes(scope)));
                       } else {
                         const isReadWrite = value === scope.mapping['read-write'];