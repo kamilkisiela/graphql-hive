b5f288e1a1b656e97725f73deb7d5d1f543136ba
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-07-14 13:29:36 +0200

    Disable plugins in prettier
    Closes #218

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,7 +3,7 @@ const { plugins, ...prettierConfig } = require('@theguild/prettier-config');
 module.exports = {
   ...prettierConfig,
   plugins: [
-    ...plugins,
+    // ...plugins, Look: #218
     // For sort CSS classes
     require('prettier-plugin-tailwindcss'),
   ],
