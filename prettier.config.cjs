5c0665132fdb28ccd4fec1360a799e2291573df7
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-07-14 11:49:30 +0000

    fix

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,7 +3,7 @@ const { plugins, ...prettierConfig } = require('@theguild/prettier-config');
 module.exports = {
   ...prettierConfig,
   plugins: [
-    // ...plugins, Look: #218
+    ...plugins,
     // For sort CSS classes
     require('prettier-plugin-tailwindcss'),
   ],
