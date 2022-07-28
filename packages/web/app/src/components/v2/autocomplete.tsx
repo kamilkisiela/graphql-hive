b3f0e604df50f3ad4ea27e1e758a1c2d17df21a5
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-07-28 18:08:43 +0200

    Use react-window to render long list

--- packages/web/app/src/components/v2/autocomplete.tsx
+++ packages/web/app/src/components/v2/autocomplete.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { StylesConfig } from 'react-select';
 import Select from 'react-select';
+import { FixedSizeList } from 'react-window';
+
+const height = 40;
+
+function MenuList(props: any) {
+  const { options, children, maxHeight, getValue } = props;
+  const [value] = getValue();
+  const initialOffset = options.indexOf(value) * height;
+
+  return (
+    <FixedSizeList
+      width="100%"
+      height={maxHeight}
+      itemCount={children.length}
+      itemSize={height}
+      initialScrollOffset={initialOffset}
+    >
+      {({ index, style }) => <div style={style}>{children[index]}</div>}
+    </FixedSizeList>
+  );
+}
 
 interface Option {
   value: string;
@@ -58,6 +79,9 @@ export function Autocomplete(
       isDisabled={props.disabled}
       isLoading={props.loading}
       placeholder={props.placeholder}
+      components={{
+        MenuList,
+      }}
     />
   );
 }
