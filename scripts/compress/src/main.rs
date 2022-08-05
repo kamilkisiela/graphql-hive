565ee2e542719e613cd2b00db42844cd2ec6265e
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-08-05 17:46:10 +0200

    almost there, docs left

--- scripts/compress/src/main.rs
+++ scripts/compress/src/main.rs
@@ -3,10 +3,7 @@ extern crate flate2;
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::env::args;
-use std::env::consts::EXE_SUFFIX;
 use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
 use std::io::BufWriter;
 use std::path::Path;
 use std::time::Instant;
@@ -16,18 +13,6 @@ fn main() {
         eprintln!("Usage: ./compress `source` `target`");
         return;
     }
-    // let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    // let output = File::create(args().nth(2).unwrap()).unwrap();
-    // let mut encoder = GzEncoder::new(output, Compression::default());
-    // let start = Instant::now();
-    // copy(&mut input, &mut encoder).unwrap();
-    // let output = encoder.finish().unwrap();
-    // println!(
-    //     "Source len: {:?}",
-    //     input.get_ref().metadata().unwrap().len()
-    // );
-    // println!("Target len: {:?}", output.metadata().unwrap().len());
-    // println!("Elapsed: {:?}", start.elapsed());
 
     let start = Instant::now();
     let input_path = args().nth(1).unwrap();
