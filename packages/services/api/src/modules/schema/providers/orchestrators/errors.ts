26261aaaada0b65b653441ad6e8e2586be046764
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-08-11 17:19:05 +0200

    To not pass originalError

--- packages/services/api/src/modules/schema/providers/orchestrators/errors.ts
+++ packages/services/api/src/modules/schema/providers/orchestrators/errors.ts
@@ -2,7 +2,6 @@ import { HiveError } from '../../../../shared/errors';
 
 export class SchemaBuildError extends HiveError {
   constructor(error: Error) {
-    super(`Failed to build schema: ${error.message}`, {});
-    Object.assign(this, { originalError: error });
+    super(`Failed to build schema: ${error.message}`);
   }
 }
