08c8429c08f685d3736666c0097d659173b32fb4
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-09-08 20:47:16 +0200

    Capture session error (SuperTokens)

--- packages/web/app/pages/auth/[[...path]].tsx
+++ packages/web/app/pages/auth/[[...path]].tsx
@@ -32,14 +32,29 @@ export default function Auth(): React.ReactElement {
             content="https://the-guild-og-image.vercel.app/**Manage%20your%20GraphQL%20APIs**.png?theme=light&md=1&fontSize=100px&images=https://graphql-hive.com/logo.svg&widths=800&heights=400"
           />
         </Head>
-        <section tw="h-full text-gray-600">
-          <div tw="container h-full px-5 py-24 mx-auto flex items-center justify-center">
-            <div tw="lg:w-2/6 md:w-1/2 w-full rounded-lg p-8 flex flex-col">
-              <FullLogo tw="text-yellow-500 mx-auto mb-5" width={250} color={{ main: '#fff', sub: '#fff' }} />
-              <SuperTokensComponentNoSSR />
-            </div>
+        <div>
+          <FullLogo className="mx-auto my-5 text-yellow-500" width={150} color={{ main: '#fff', sub: '#fff' }} />
+          <div tw="mx-auto bg-yellow-200 text-black sm:width[420px] width[76%] rounded-lg shadow-lg p-5 text-xs">
+            We recently migrated from Auth0 to SuperTokens. If you have any issues, please contact us at{' '}
+            <a href="mailto:kamil@graphql-hive.com" className="underline">
+              kamil@graphql-hive.com
+            </a>{' '}
+            or using the{' '}
+            <a
+              href="#"
+              className="underline"
+              onClick={() => {
+                if (typeof window !== 'undefined' && (window as any).$crisp) {
+                  (window as any).$crisp.push(['do', 'chat:open']);
+                }
+              }}
+            >
+              in-app chat
+            </a>
+            .
           </div>
-        </section>
+          <SuperTokensComponentNoSSR />
+        </div>
       </>
     </>
   );
