42658b2f3200322e737c248baa847c54d4d65520
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-09-12 17:14:37 +0200

    fastify/cors

--- packages/services/service-common/src/fastify.ts
+++ packages/services/service-common/src/fastify.ts
@@ -1,5 +1,5 @@
 import { fastify } from 'fastify';
-import cors from 'fastify-cors';
+import cors from '@fastify/cors';
 import * as Sentry from '@sentry/node';
 import { useSentryTracing } from './sentry';
 
--- packages/services/service-common/src/metrics.ts
+++ packages/services/service-common/src/metrics.ts
@@ -1,5 +1,5 @@
 import { fastify } from 'fastify';
-import cors from 'fastify-cors';
+import cors from '@fastify/cors';
 import promClient from 'prom-client';
 
 export { promClient as metrics };
