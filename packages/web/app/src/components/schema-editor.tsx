0a2d33e1c2f6afaf836eed630543a00a1fc48a58
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-09-20 18:17:31 +0200

    Use dynamic instead of React.lazy
    Fixes an issue with missing Suspense.fallback

--- packages/web/app/src/components/schema-editor.tsx
+++ packages/web/app/src/components/schema-editor.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import dynamic from 'next/dynamic';
 
-export const SchemaEditor = React.lazy(() =>
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  import('regenerator-runtime/runtime')
-    .then(() => import('@theguild/editor'))
-    .then(({ SchemaEditor }) => ({ default: SchemaEditor }))
+export const SchemaEditor = dynamic(
+  () =>
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    import('regenerator-runtime/runtime')
+      .then(() => import('@theguild/editor'))
+      .then(({ SchemaEditor }) => ({ default: SchemaEditor })),
+  {
+    ssr: false,
+  }
 );
 
 export type { SchemaEditorProps } from '@theguild/editor';
