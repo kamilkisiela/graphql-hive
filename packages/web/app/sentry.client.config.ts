a581130c70f13c7d762670130a95bf16967dcefc
Laurin Quast <laurinquast@googlemail.com>
2022-09-23 11:13:45 +0200

    fix the sentry integration 😌

--- packages/web/app/sentry.client.config.ts
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-import { Integrations } from '@sentry/react';
-import { config } from './sentry.config';
-
-Sentry.init({
-  ...config,
-  integrations: [new Integrations.GlobalHandlers()],
-});
