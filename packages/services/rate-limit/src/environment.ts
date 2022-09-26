4c836ab4de436767ba3ff5b2abee185294ac5013
Laurin Quast <laurinquast@googlemail.com>
2022-09-26 16:58:05 +0200

    feat: decode rate-limit service environment

---
+++ packages/services/rate-limit/src/environment.ts
@@ -0,0 +1,112 @@
+import zod from 'zod';
+
+const isNumberString = (input: unknown) => zod.string().regex(/^\d+$/).safeParse(input).success;
+
+const numberFromNumberOrNumberString = (input: unknown): number | undefined => {
+  if (typeof input == 'number') return input;
+  if (isNumberString(input)) return Number(input);
+};
+
+const NumberFromString = zod.preprocess(numberFromNumberOrNumberString, zod.number().min(1));
+
+const EnvironmentModel = zod.object({
+  PORT: NumberFromString.optional(),
+  ENVIRONMENT: zod.string().optional(),
+  RELEASE: zod.string().optional(),
+  ENCRYPTION_SECRET: zod.string(),
+  USAGE_ESTIMATOR_ENDPOINT: zod.string().url(),
+  EMAILS_ENDPOINT: zod.string().url().optional(),
+  LIMIT_CACHE_UPDATE_INTERVAL_MS: NumberFromString.optional(),
+});
+
+const SentryModel = zod.union([
+  zod.object({
+    SENTRY: zod.literal('0').optional(),
+  }),
+  zod.object({
+    SENTRY: zod.literal('1'),
+    SENTRY_DSN: zod.string(),
+  }),
+]);
+
+const PostgresModel = zod.object({
+  POSTGRES_SSL: zod.union([zod.literal('1'), zod.literal('0')]).optional(),
+  POSTGRES_HOST: zod.string(),
+  POSTGRES_PORT: NumberFromString,
+  POSTGRES_DB: zod.string(),
+  POSTGRES_USER: zod.string(),
+  POSTGRES_PASSWORD: zod.string(),
+});
+
+const PrometheusModel = zod.object({
+  PROMETHEUS_METRICS: zod.union([zod.literal('0'), zod.literal('1')]).optional(),
+  PROMETHEUS_METRICS_LABEL_INSTANCE: zod.string().optional(),
+});
+
+const configs = {
+  // eslint-disable-next-line no-process-env
+  base: EnvironmentModel.safeParse(process.env),
+  // eslint-disable-next-line no-process-env
+  sentry: SentryModel.safeParse(process.env),
+  // eslint-disable-next-line no-process-env
+  postgres: PostgresModel.safeParse(process.env),
+  // eslint-disable-next-line no-process-env
+  prometheus: PrometheusModel.safeParse(process.env),
+};
+
+const environmentErrors: Array<string> = [];
+
+for (const config of Object.values(configs)) {
+  if (config.success === false) {
+    environmentErrors.push(JSON.stringify(config.error.format(), null, 4));
+  }
+}
+
+if (environmentErrors.length) {
+  const fullError = environmentErrors.join(`\n`);
+  console.error('❌ Invalid environment variables:', fullError);
+  process.exit(1);
+}
+
+function extractConfig<Input, Output>(config: zod.SafeParseReturnType<Input, Output>): Output {
+  if (!config.success) {
+    throw new Error('Something went wrong.');
+  }
+  return config.data;
+}
+
+const base = extractConfig(configs.base);
+const postgres = extractConfig(configs.postgres);
+const sentry = extractConfig(configs.sentry);
+const prometheus = extractConfig(configs.prometheus);
+
+export const env = {
+  environment: base.ENVIRONMENT,
+  release: base.RELEASE ?? 'local',
+  encryptionSecret: base.ENCRYPTION_SECRET,
+  hiveServices: {
+    usageEstimator: { endpoint: base.USAGE_ESTIMATOR_ENDPOINT },
+    emails: base.EMAILS_ENDPOINT ? { endpoint: base.EMAILS_ENDPOINT } : null,
+  },
+  limitCacheUpdateIntervalMs: base.LIMIT_CACHE_UPDATE_INTERVAL_MS ?? 60_000,
+  http: {
+    port: base.PORT ?? 4012,
+  },
+  postgres: {
+    host: postgres.POSTGRES_HOST,
+    port: postgres.POSTGRES_PORT,
+    db: postgres.POSTGRES_DB,
+    user: postgres.POSTGRES_USER,
+    password: postgres.POSTGRES_PASSWORD,
+    ssl: postgres.POSTGRES_SSL === '1',
+  },
+  sentry: sentry.SENTRY === '1' ? { dsn: sentry.SENTRY_DSN } : null,
+  prometheus:
+    prometheus.PROMETHEUS_METRICS === '1'
+      ? {
+          labels: {
+            instance: prometheus.PROMETHEUS_METRICS_LABEL_INSTANCE ?? 'rate-limit',
+          },
+        }
+      : null,
+} as const;
