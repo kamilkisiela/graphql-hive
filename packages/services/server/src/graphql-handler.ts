1daf850708e2906deb4639c8288fcdaf07383e46
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-10-03 04:56:54 +0300

    Go

--- packages/services/server/src/graphql-handler.ts
+++ packages/services/server/src/graphql-handler.ts
@@ -241,8 +241,6 @@ export const graphqlHandler = (options: GraphQLHandlerOptions): RouteHandlerMeth
         reply.status(response.status);
 
         reply.send(response.body);
-
-        return reply;
       }
     );
   };
