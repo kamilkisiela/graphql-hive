9327a453f18f44010fe5490f34d5f70481487584
Laurin Quast <laurinquast@googlemail.com>
2022-10-19 13:04:01 +0200

    feat: automatically start okta login when visiting /auth with okta provider query parameter

---
+++ packages/web/app/src/lib/supertokens/start-auth-flow-for-provider.ts
@@ -0,0 +1,16 @@
+import { env } from '@/env/frontend';
+import { getAuthorisationURLWithQueryParamsAndSetState } from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
+
+/**
+ * utility for starting the login flow manually without clicking a button
+ */
+export const startAuthFlowForProvider = async (providerId: 'google' | 'okta' | 'github') => {
+  const authUrl = await getAuthorisationURLWithQueryParamsAndSetState({
+    providerId,
+    // This is where Google should redirect the user back after login or error.
+    // This URL goes on the Google's dashboard as well.
+    authorisationURL: `${env.appBaseUrl}/auth/callback/${providerId}`,
+  });
+
+  window.location.assign(authUrl);
+};
