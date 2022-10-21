726c3e8000ceb37719826b074121262720483808
Laurin Quast <laurinquast@googlemail.com>
2022-10-21 12:28:37 +0200

    add unique constraint for the oidc domain column

--- packages/services/storage/migrations/actions/2022.10.20T08.00.46.oidc-integrations.sql
+++ packages/services/storage/migrations/actions/2022.10.20T08.00.46.oidc-integrations.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS "public"."oidc_integrations" (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4()
-  ,"client_id" TEXT NOT NULL
-  ,"client_secret" TEXT NOT NULL
-  ,"domain" TEXT NOT NULL
-  ,"linked_organization_id" uuid NOT NULL UNIQUE REFERENCES "public"."organizations"("id") ON DELETE CASCADE
-  ,"created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-  ,"updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+  , "client_id" TEXT NOT NULL
+  , "client_secret" TEXT NOT NULL
+  -- A domain should only be usable for one integration
+  , "domain" TEXT NOT NULL UNIQUE
+  , "linked_organization_id" uuid NOT NULL UNIQUE REFERENCES "public"."organizations"("id") ON DELETE CASCADE
+  , "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+  , "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
