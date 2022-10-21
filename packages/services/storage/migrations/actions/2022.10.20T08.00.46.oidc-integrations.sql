f0dcc8637a5d4fd24987c4b81720b6a7e49b142c
Laurin Quast <laurinquast@googlemail.com>
2022-10-21 12:05:45 +0200

    auto generate id

--- packages/services/storage/migrations/actions/2022.10.20T08.00.46.oidc-integrations.sql
+++ packages/services/storage/migrations/actions/2022.10.20T08.00.46.oidc-integrations.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS "public"."oidc_integrations" (
-  "id" UUID NOT NULL
+  "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4()
   ,"client_id" TEXT NOT NULL
   ,"client_secret" TEXT NOT NULL
   ,"domain" TEXT NOT NULL
   ,"linked_organization_id" uuid NOT NULL UNIQUE REFERENCES "public"."organizations"("id") ON DELETE CASCADE
   ,"created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
   ,"updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-  ,PRIMARY KEY ("id")
 );
