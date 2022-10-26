7f1961bcea2a456c3ff3092af2eaf526836d1bd6
Dotan Simha <dotansimha@gmail.com>
2022-10-26 10:37:22 +0900

    update some pulumi deps to latest and fix deployment tsconfig

--- deployment/utils/local-endpoint.ts
+++ deployment/utils/local-endpoint.ts
@@ -3,21 +3,21 @@ import * as pulumi from '@pulumi/pulumi';
 
 export function serviceLocalEndpoint(service: k8s.types.input.core.v1.Service) {
   return pulumi.all([service.metadata, service.spec]).apply(([metadata, spec]) => {
-    const defaultPort = (spec.ports || [])[0];
+    const defaultPort = (spec?.ports || [])[0];
     const portText = defaultPort ? `:${defaultPort.port}` : '';
 
-    return `http://${metadata.name}.${metadata.namespace || 'default'}.svc.cluster.local${portText}`;
+    return `http://${metadata?.name}.${metadata?.namespace || 'default'}.svc.cluster.local${portText}`;
   });
 }
 
 export function serviceLocalHost(service: k8s.types.input.core.v1.Service) {
   return pulumi.all([service.metadata]).apply(([metadata]) => {
-    return `${metadata.name}.${metadata.namespace || 'default'}.svc.cluster.local`;
+    return `${metadata?.name}.${metadata?.namespace || 'default'}.svc.cluster.local`;
   });
 }
 
 export function serviceLocalMetricsEndpoint(service: k8s.types.input.core.v1.Service) {
   return pulumi.all([service.metadata]).apply(([metadata]) => {
-    return `${metadata.name}.${metadata.namespace || 'default'}.svc.cluster.local:10254/metrics`;
+    return `${metadata?.name}.${metadata?.namespace || 'default'}.svc.cluster.local:10254/metrics`;
   });
 }
