38d2e734319da21d9c6b97e80c7c681d2977f6fe
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2022-11-15 09:29:58 -0500

    include operationName and requestId in request log

--- packages/services/service-common/src/request-logs.ts
+++ packages/services/service-common/src/request-logs.ts
@@ -1,10 +1,48 @@
-import type { FastifyInstance, FastifyPluginAsync } from 'fastify';
+import type { FastifyInstance, FastifyPluginAsync, FastifyRequest } from 'fastify';
 import fp from 'fastify-plugin';
+import { cleanRequestId } from './helpers';
+import { parse, getOperationAST } from 'graphql';
+import { z } from 'zod';
+
+const GraphQLPayloadSchema = z.object({
+  query: z.string(),
+  operationName: z.string().optional(),
+});
 
 const plugin: FastifyPluginAsync = async server => {
-  server.addHook('onResponse', async (request, reply) => {
-    const message = `[${reply.statusCode}] (${request.ip}) ${request.method} ${request.url}`;
+  function graphqlOperationName(request: FastifyRequest): string | undefined {
+    let payload;
 
+    if (request.method === 'GET') {
+      payload = GraphQLPayloadSchema.safeParse(request.query);
+    } else if (request.method === 'POST') {
+      payload = GraphQLPayloadSchema.safeParse(request.body);
+    } else {
+      return undefined;
+    }
+
+    if (!payload.success) {
+      return undefined;
+    }
+
+    const { operationName, query } = payload.data;
+    const operation = getOperationAST(parse(query), operationName);
+    return operation?.name?.value;
+  }
+
+  server.addHook('onResponse', async (request, reply) => {
+    const requestId = cleanRequestId(request.headers['x-request-id']);
+    const operationName = graphqlOperationName(request);
+    const message = [
+      `[${reply.statusCode}]`,
+      `(${request.ip})`,
+      request.method,
+      request.url,
+      operationName ? `'${operationName}'` : undefined,
+      `(${requestId})`,
+    ]
+      .filter(s => s)
+      .join(' ');
     if (reply.statusCode < 500) {
       server.log.info(message);
     } else {
