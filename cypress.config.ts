84209986d51e2a4ea711864237b314c812a02cc2
Denis Badurina <denis@domonda.com>
2022-11-25 12:19:19 +0100

    disable video and screenshot

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  video: false, // TODO: can it be useful for CI?
+  screenshotOnRunFailure: false, // TODO: can it be useful for CI?
   e2e: {
     // defaults
   },
