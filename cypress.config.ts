d7a27f7a8623707c922401e1d85f6f83a7dfb1f7
Denis Badurina <denis@domonda.com>
2022-11-25 11:53:35 +0100

    default cypress config and lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  // defaults
 });
