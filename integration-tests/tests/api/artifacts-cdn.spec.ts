31211dba72bd937245811b6e72aac6ee20808647
Laurin Quast <laurinquast@googlemail.com>
2022-11-28 14:04:17 +0100

    test: SDL artifact flow

--- integration-tests/tests/api/artifacts-cdn.spec.ts
+++ integration-tests/tests/api/artifacts-cdn.spec.ts
@@ -1,5 +1,20 @@
-import { S3Client, ListObjectsCommand, DeleteObjectsCommand } from '@aws-sdk/client-s3';
+import {
+  S3Client,
+  ListObjectsCommand,
+  DeleteObjectsCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
 import { fetch } from '@whatwg-node/fetch';
+import { authenticate } from '../../testkit/auth';
+import {
+  createOrganization,
+  createProject,
+  createToken,
+  publishSchema,
+  schemaSyncCDN,
+  createCdnAccess,
+} from '../../testkit/flow';
+import { ProjectType, TargetAccessScope } from '../../testkit/gql/graphql';
 
 const s3Client = new S3Client({
   endpoint: 'http://127.0.0.1:9000',
@@ -36,6 +51,15 @@ async function deleteAllS3BucketObjects(s3Client: S3Client, bucketName: string) 
   }
 }
 
+async function fetchS3ObjectArtifact(bucketName: string, key: string): Promise<string> {
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: key,
+  });
+  const result = await s3Client.send(getObjectCommand);
+  return await result.Body!.transformToString();
+}
+
 beforeEach(async () => {
   await deleteAllS3BucketObjects(s3Client, 'artifacts');
 });
@@ -57,7 +81,8 @@ function runArtifactsCDNTests(name: string, endpointBaseUrl: string) {
       const url = buildEndpointUrl(endpointBaseUrl, 'i-do-not-exist', 'sdl');
       const response = await fetch(url, { method: 'GET' });
       expect(response.status).toEqual(400);
-      expect(await response.text()).toEqual({
+      expect(response.headers.get('content-type')).toContain('application/json');
+      expect(await response.json()).toEqual({
         code: 'MISSING_AUTH_KEY',
         error: 'Hive CDN authentication key is missing',
         description:
@@ -75,7 +100,8 @@ function runArtifactsCDNTests(name: string, endpointBaseUrl: string) {
         },
       });
       expect(response.status).toEqual(403);
-      expect(await response.text()).toEqual({
+      expect(response.headers.get('content-type')).toContain('application/json');
+      expect(await response.json()).toEqual({
         code: 'INVALID_AUTH_KEY',
         error:
           'Hive CDN authentication key is invalid, or it does not match the requested target ID.',
@@ -84,7 +110,120 @@ function runArtifactsCDNTests(name: string, endpointBaseUrl: string) {
       });
       expect(response.headers.get('location')).toEqual(null);
     });
+
+    test('access SDL artifact with valid credentials', async () => {
+      const { access_token } = await authenticate('main');
+
+      // Create Organization
+
+      const orgResult = await createOrganization(
+        {
+          name: 'foo',
+        },
+        access_token,
+      );
+
+      const org =
+        orgResult.body.data!.createOrganization.ok!.createdOrganizationPayload.organization;
+
+      // Create Project
+
+      const projectResult = await createProject(
+        {
+          organization: org.cleanId,
+          type: ProjectType.Single,
+          name: 'foo',
+        },
+        access_token,
+      );
+
+      const project = projectResult.body.data!.createProject.ok!.createdProject;
+      const target = projectResult.body.data?.createProject.ok?.createdTargets.find(
+        t => t.name === 'production',
+      );
+
+      // Create Schema Publish Token
+
+      const tokenResult = await createToken(
+        {
+          name: 'test',
+          organization: org.cleanId,
+          project: project.cleanId,
+          target: target!.cleanId,
+          organizationScopes: [],
+          projectScopes: [],
+          targetScopes: [TargetAccessScope.RegistryRead, TargetAccessScope.RegistryWrite],
+        },
+        access_token,
+      );
+
+      expect(tokenResult.body.errors).not.toBeDefined();
+
+      const token = tokenResult.body.data!.createToken.ok!.secret;
+
+      // Publish Schema
+
+      const publishSchemaResult = await publishSchema(
+        {
+          author: 'Kamil',
+          commit: 'abc123',
+          sdl: `type Query { ping: String }`,
+        },
+        token,
+      );
+
+      expect(publishSchemaResult.body.data?.schemaPublish.__typename).toEqual(
+        'SchemaPublishSuccess',
+      );
+
+      const schemaSyncCDNResult = await schemaSyncCDN(
+        {
+          organization: org.cleanId,
+          project: project.cleanId,
+          target: target!.cleanId,
+        },
+        access_token,
+      );
+
+      expect(schemaSyncCDNResult.body.errors).not.toBeDefined();
+      expect(schemaSyncCDNResult.body.data!.schemaSyncCDN.__typename).toBe('SchemaSyncCDNSuccess');
+
+      const cdnAccessResult = await createCdnAccess(
+        {
+          organization: org.cleanId,
+          project: project.cleanId,
+          target: target!.cleanId,
+        },
+        access_token,
+      );
+
+      expect(cdnAccessResult.body.errors).toBeUndefined();
+
+      const url = buildEndpointUrl(endpointBaseUrl, target!.id, 'sdl');
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'x-hive-cdn-key': cdnAccessResult.body.data!.createCdnToken.token,
+        },
+        redirect: 'manual',
+      });
+
+      expect(response.status).toMatchInlineSnapshot(`302`);
+      expect(await response.text()).toMatchInlineSnapshot(`"Found."`);
+      expect(response.headers.get('location')).toBeDefined();
+
+      const artifactContents = await fetchS3ObjectArtifact(
+        'artifacts',
+        `artifact/${target!.id}/sdl`,
+      );
+      expect(artifactContents).toMatchInlineSnapshot(`
+        "type Query {
+          ping: String
+        }"
+      `);
+    });
   });
 }
 
 runArtifactsCDNTests('API Mirror', 'http://127.0.0.1:3001/artifacts/v1/');
+// runArtifactsCDNTests('Local CDN Mock', 'http://127.0.0.1:3004/artifacts/v1/');
