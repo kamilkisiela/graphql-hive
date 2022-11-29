c913cd773de0fb844f9b9e8bcb2994708b8c6ed3
Laurin Quast <laurinquast@googlemail.com>
2022-11-29 11:53:26 +0100

    add missing fetch import

--- packages/services/api/src/modules/schema/providers/artifact-storage-reader.ts
+++ packages/services/api/src/modules/schema/providers/artifact-storage-reader.ts
@@ -1,5 +1,6 @@
 import { GetObjectCommand, HeadObjectCommand, type S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { fetch } from '@whatwg-node/fetch';
 
 const presignedUrlExpirationSeconds = 60;
 
