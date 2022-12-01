d580b2d63c76374fd8e05c0ef5bee7822ffc60ab
enisdenjo <badurinadenis@gmail.com>
2022-12-01 15:18:06 +0100

    comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   video: false, // TODO: can it be useful for CI?
   screenshotOnRunFailure: false, // TODO: can it be useful for CI?
-  defaultCommandTimeout: 8000,
+  defaultCommandTimeout: 8000, // sometimes the app takes longer to load, especially in the CI
   e2e: {
     // defaults
   },
