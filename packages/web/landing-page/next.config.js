b505ff1cefdac8a736c94e2b19432753e5308fef
Dotan Simha <dotansimha@gmail.com>
2022-12-06 10:49:29 +0100

    Move landing page to be under the unified domain the-guild.dev (#746)

--- packages/web/landing-page/next.config.js
+++ packages/web/landing-page/next.config.js
@@ -1,6 +1,14 @@
+/* eslint-disable no-undef, no-process-env */
 export default {
+  basePath: process.env.NEXT_BASE_PATH,
   poweredByHeader: false,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    newNextLinkBehavior: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
