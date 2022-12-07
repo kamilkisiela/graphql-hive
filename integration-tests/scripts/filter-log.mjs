7266290a75493e2445c0a501683fa03c41b71e91
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-12-07 11:27:06 +0100

    Set LOG_LEVEL to debug

--- integration-tests/scripts/filter-log.mjs
+++ integration-tests/scripts/filter-log.mjs
@@ -14,6 +14,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const logPath = path.resolve(__dirname, '../dockest.log');
 const filteredLogPath = path.resolve(__dirname, '../dockest-filtered.log');
 const logLines = fs.readFileSync(logPath, 'utf-8').split('\n');
-const filteredLogLines = logLines.filter(line => serviceNames.some(name => line.includes(name)));
+const filteredLogLines = logLines.filter(line =>
+  serviceNames.some(name => line.startsWith(`integration-tests-${name}`)),
+);
 
 fs.writeFileSync(filteredLogPath, filteredLogLines.join('\n'));
