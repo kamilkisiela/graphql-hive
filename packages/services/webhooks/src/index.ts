a9d8cc9e9cff8611b1ea7a5488f962b42fff50b3
YassinEldeeb <yassineldeeb94@gmail.com>
2022-12-20 13:28:54 +0200

    refactor: extract router to a different file + use a different trpc adapter

--- packages/services/webhooks/src/index.ts
+++ packages/services/webhooks/src/index.ts
@@ -1,23 +1,16 @@
 #!/usr/bin/env node
 import {
-  createErrorHandler,
   startMetrics,
   registerShutdown,
-  reportReadiness,
-  startHeartbeats,
   FastifyLoggerInstance,
+  startHeartbeats,
 } from '@hive/service-common';
 import * as Sentry from '@sentry/node';
-import type { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
-import { createScheduler } from './scheduler';
-import { webhooksApiRouter } from './api';
-import type { Context } from './types';
 import { env } from './environment';
-import { Router } from 'itty-router';
 import { createServerAdapter } from '@whatwg-node/server';
 import { createServer } from 'http';
+import { webhooksRouter, startSchedular, stopSchedular, schedularReadiness } from './router';
 import { createLogger } from 'packages/services/service-common/src/logger';
-import * as trpcNode from '@trpc/server/adapters/node-http';
 
 async function main() {
   if (env.sentry) {
@@ -30,32 +23,17 @@ async function main() {
     });
   }
 
-  const app = createServerAdapter(Router());
-
   const logger = createLogger(env.log.level) as FastifyLoggerInstance;
-  const errorHandler = createErrorHandler({ log: { error: logger.error } } as any);
+  const app = createServerAdapter(webhooksRouter);
 
   try {
-    const { schedule, readiness, start, stop } = createScheduler({
-      logger,
-      redis: {
-        host: env.redis.host,
-        port: env.redis.port,
-        password: env.redis.password,
-      },
-      webhookQueueName: 'webhook',
-      maxAttempts: 10,
-      backoffDelay: 2000,
-      requestBroker: env.requestBroker,
-    });
-
     const stopHeartbeats = env.heartbeat
       ? startHeartbeats({
           enabled: true,
           endpoint: env.heartbeat.endpoint,
           intervalInMS: 20_000,
           onError: logger.error,
-          isReady: readiness,
+          isReady: schedularReadiness,
         })
       : startHeartbeats({ enabled: false });
 
@@ -63,35 +41,8 @@ async function main() {
       logger,
       async onShutdown() {
         stopHeartbeats();
-        await stop();
+        await stopSchedular();
       },
-    });
-
-    app.all('/trpc', (req: any, res) => {
-      return trpcNode.nodeHTTPRequestHandler({
-        req,
-        res,
-        path: '/trpc',
-        router: webhooksApiRouter,
-        createContext({ req }: CreateFastifyContextOptions): Context {
-          return { logger: req.log, errorHandler, schedule };
-        },
-      });
-    });
-
-    app.get('/_health', () => {
-      return new Response(null, {
-        status: 200,
-      });
-    });
-
-    app.get('/_readiness', () => {
-      const isReady = readiness();
-      reportReadiness(isReady);
-
-      return new Response(null, {
-        status: isReady ? 200 : 400,
-      });
     });
 
     const server = createServer(app);
@@ -100,7 +51,7 @@ async function main() {
       await startMetrics(env.prometheus.labels.instance);
     }
 
-    await start();
+    await startSchedular();
     return new Promise<void>(resolve => {
       server.listen(env.http.port, '0.0.0.0', resolve);
     });
