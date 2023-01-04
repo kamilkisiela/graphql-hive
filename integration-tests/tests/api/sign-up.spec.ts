7c0cabd89341a666fa011d126edbbb93a380250a
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-01-04 17:57:21 +0100

    test: Expect the default retention for a Hobby plan to be 7 days

--- integration-tests/tests/api/sign-up.spec.ts
+++ integration-tests/tests/api/sign-up.spec.ts
@@ -1,6 +1,16 @@
 import { gql } from '@app/gql';
+import { ProjectType } from '@app/gql/graphql';
+import type { RateLimitApi } from '@hive/rate-limit';
+import { createTRPCProxyClient, httpLink } from '@trpc/client';
+import { createFetch } from '@whatwg-node/fetch';
+import { waitFor } from '../../testkit/flow';
 import { execute } from '../../testkit/graphql';
 import { initSeed } from '../../testkit/seed';
+import { getServiceHost } from '../../testkit/utils';
+
+const { fetch } = createFetch({
+  useNodeFetch: true,
+});
 
 test.concurrent('should auto-create an organization for freshly signed-up user', async () => {
   const { ownerToken } = await initSeed().createOwner();
@@ -21,6 +31,49 @@ test.concurrent('should auto-create an organization for freshly signed-up user',
 
   expect(result.organizations.total).toBe(1);
 });
+
+test.concurrent.only(
+  'freshly signed-up user should have a Hobby plan with 7 days of retention',
+  async () => {
+    const { ownerToken, createPersonalProject } = await initSeed().createOwner();
+    const result = await execute({
+      document: gql(/* GraphQL */ `
+        query organizations {
+          organizations {
+            total
+            nodes {
+              id
+              name
+            }
+          }
+        }
+      `),
+      authToken: ownerToken,
+    }).then(r => r.expectNoGraphQLErrors());
+
+    expect(result.organizations.total).toBe(1);
+
+    const { target } = await createPersonalProject(ProjectType.Single);
+
+    await waitFor(3_000); // wait for rate-limit to update (longer then LIMIT_CACHE_UPDATE_INTERVAL_MS)
+
+    const rateLimit = createTRPCProxyClient<RateLimitApi>({
+      links: [
+        httpLink({
+          url: `http://${await getServiceHost('rate-limit', 3009)}/trpc`,
+          fetch,
+        }),
+      ],
+    });
+
+    // Expect the default retention for a Hobby plan to be 7 days
+    await expect(
+      rateLimit.getRetention.query({
+        targetId: target.id,
+      }),
+    ).resolves.toEqual(7);
+  },
+);
 
 test.concurrent(
   'should auto-create an organization for freshly signed-up user with no race-conditions',
