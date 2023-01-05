7a6c948979b978c73788d490c295935ff2df2ce5
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-01-05 16:22:30 +0100

    Use default skipError of @envelop/sentry

--- packages/services/server/src/graphql-handler.ts
+++ packages/services/server/src/graphql-handler.ts
@@ -1,4 +1,3 @@
-import { isGraphQLError } from '@envelop/core';
 import { useGenericAuth } from '@envelop/generic-auth';
 import { useGraphQLModules } from '@envelop/graphql-modules';
 import { useSentry } from '@envelop/sentry';
@@ -153,7 +152,6 @@ export const graphqlHandler = (options: GraphQLHandlerOptions): RouteHandlerMeth
           // It's the readiness check
           return args.operationName === 'readiness';
         },
-        skipError: isGraphQLError,
       }),
       useSentryUser(),
       useErrorHandler(({ errors, context }): void => {
