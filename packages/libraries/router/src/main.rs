4a8fc14c0aa5ce197417cc6d26dbf0696d256d87
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-01-12 14:27:53 +0100

    Add comments to check the layer caching of docker

--- packages/libraries/router/src/main.rs
+++ packages/libraries/router/src/main.rs
@@ -7,8 +7,10 @@ use registry::HiveRegistry;
 use usage::register;
 
 fn main() {
+    // Register the usage reporting plugin
     register();
 
+    // Initialize the Hive Registry and start the Apollo Router
     match HiveRegistry::new(None).and(apollo_router::main()) {
         Ok(_) => {}
         Err(e) => {
