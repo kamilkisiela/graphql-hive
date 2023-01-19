083c27f60270e6774da5e8138e0fd53911753b05
Dotan Simha <dotansimha@gmail.com>
2023-01-19 13:07:06 +0100

    avoid flakiness

--- integration-tests/jest.config.js
+++ integration-tests/jest.config.js
@@ -20,7 +20,7 @@ export default {
     }),
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  testTimeout: 60_000,
+  testTimeout: 90_000,
   setupFilesAfterEnv: ['dotenv/config'],
   collectCoverage: false,
 };
