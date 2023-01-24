3a65811ec32bb7fe2b96b34523138e253fe6cf31
Laurin Quast <laurinquast@googlemail.com>
2023-01-24 14:20:22 +0100

    use base64 for access tokens

--- packages/services/cdn-worker/src/cdn-token.ts
+++ packages/services/cdn-worker/src/cdn-token.ts
@@ -18,8 +18,8 @@ const keyPrefix = 'hv2';
  * Encode a CDN token into a hex string with prefix.
  */
 export function encodeCdnToken(args: CDNToken): string {
-  const arr = encoder.encode([args.keyId, args.privateKey].join(':'));
-  return keyPrefix + buf2hex(arr);
+  const keyContents = [args.keyId, args.privateKey].join(':');
+  return keyPrefix + globalThis.btoa(keyContents);
 }
 
 export function isCDNAccessToken(token: string) {
@@ -38,7 +38,7 @@ export function decodeCdnAccessTokenSafe(token: string) {
 
   token = token.slice(keyPrefix.length);
 
-  const str = decoder.decode(hex2buf(token));
+  const str = globalThis.atob(token);
   const [keyId, privateKey] = str.split(':');
   if (keyId && privateKey) {
     return { type: 'success', token: { keyId, privateKey } } as const;
@@ -51,42 +51,6 @@ export function decodeCdnAccessTokenSafe(token: string) {
  */
 export async function verifyCdnToken(privateKey: string, privateKeyHash: string) {
   return bc.compare(privateKey, privateKeyHash);
-}
-
-const encoder = new TextEncoder();
-const decoder = new TextDecoder();
-
-function buf2hex(buffer: ArrayBuffer): string {
-  // buffer is an ArrayBuffer
-  return [...new Uint8Array(buffer)].map(x => dec2hex(x)).join('');
-}
-
-function hex2buf(hexString: string): ArrayBuffer {
-  // remove the leading 0x
-  hexString = hexString.replace(/^0x/, '');
-
-  // ensure even number of characters
-  if (hexString.length % 2 != 0) {
-    console.log('WARNING: expecting an even number of characters in the hexString');
-  }
-
-  // check for some non-hex characters
-  const bad = hexString.match(/[G-Z\s]/i);
-  if (bad) {
-    console.log('WARNING: found non-hex characters', bad);
-  }
-
-  // split the string into pairs of octets
-  const pairs = hexString.match(/[\dA-F]{2}/gi);
-
-  // convert the octets to integers
-  const integers = (pairs as any).map(function (s: string) {
-    return parseInt(s, 16);
-  });
-
-  const array = new Uint8Array(integers);
-
-  return array.buffer;
 }
 
 export function generatePrivateKey(): string {
