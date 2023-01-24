f9eea86381f884b2c5e7f4f5731a96b7989f0b14
enisdenjo <badurinadenis@gmail.com>
2023-01-24 13:40:24 +0100

    project view

--- packages/web/app/src/components/layouts/project.tsx
+++ packages/web/app/src/components/layouts/project.tsx
@@ -142,12 +142,12 @@ export const ProjectLayout = ({
           </Tabs.Trigger>
           {canAccessProject(ProjectAccessScope.Alerts, org.me) && (
             <Tabs.Trigger value={TabValue.Alerts} asChild>
-              <NextLink href={`/${orgId}/${projectId}/alerts`}>Alerts</NextLink>
+              <NextLink href={`/${orgId}/${projectId}/view/alerts`}>Alerts</NextLink>
             </Tabs.Trigger>
           )}
           {canAccessProject(ProjectAccessScope.Settings, org.me) && (
             <Tabs.Trigger value={TabValue.Settings} asChild>
-              <NextLink href={`/${orgId}/${projectId}/settings`}>Settings</NextLink>
+              <NextLink href={`/${orgId}/${projectId}/view/settings`}>Settings</NextLink>
             </Tabs.Trigger>
           )}
         </Tabs.List>
