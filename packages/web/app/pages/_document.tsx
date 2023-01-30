44dce724894f96f4df8a079911cb197d06875490
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-01-30 18:18:27 +0100

    Add type attribute to <link /> (favicon) (#1186)

--- packages/web/app/pages/_document.tsx
+++ packages/web/app/pages/_document.tsx
@@ -43,7 +43,7 @@ export default class MyDocument extends Document<{
             href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/just-logo.svg" />
+          <link rel="icon" href="/just-logo.svg" type="image/svg+xml" />
           <script
             id="force-dark-mode"
             dangerouslySetInnerHTML={{ __html: "localStorage['chakra-ui-color-mode'] = 'dark';" }}
