b69f63d39ddb1483b7af8017280bc264596b1b2d
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-02-21 15:46:41 +0100

    asd

--- packages/migrations/src/actions/2023.02.21T14.32.24.supertokens-4.0.0.sql
+++ packages/migrations/src/actions/2023.02.21T14.32.24.supertokens-4.0.0.sql
@@ -1,2 +1,2 @@
-ALTER TABLE supertokens_thirdparty_users ALTER COLUMN third_party_user_id TYPE VARCHAR(256);
-ALTER TABLE supertokens_emailpassword_users ALTER COLUMN password_hash TYPE VARCHAR(256);
+ALTER TABLE public.supertokens_thirdparty_users ALTER COLUMN third_party_user_id TYPE VARCHAR(256);
+ALTER TABLE public.supertokens_emailpassword_users ALTER COLUMN password_hash TYPE VARCHAR(256);
