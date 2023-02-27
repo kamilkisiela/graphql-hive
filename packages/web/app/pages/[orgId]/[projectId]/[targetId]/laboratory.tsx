e399846c55aa36c44b9a727da53764c5c9dddb07
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-02-27 15:08:18 +0100

    wip

--- packages/web/app/pages/[orgId]/[projectId]/[targetId]/laboratory.tsx
+++ packages/web/app/pages/[orgId]/[projectId]/[targetId]/laboratory.tsx
@@ -3,15 +3,57 @@ import { GraphiQL } from 'graphiql';
 import { authenticated } from '@/components/authenticated-container';
 import { TargetLayout } from '@/components/layouts';
 import { Button, Title } from '@/components/v2';
-import { HiveLogo, Link2Icon } from '@/components/v2/icon';
+import { HiveLogo } from '@/components/v2/icon';
 import { ConnectLabModal } from '@/components/v2/modals/connect-lab';
 import { graphql } from '@/gql';
-import { useRouteSelector, useToggle } from '@/lib/hooks';
+import { useClipboard, useRouteSelector, useToggle } from '@/lib/hooks';
 import { withSessionProtection } from '@/lib/supertokens/guard';
+import { Menu, Tooltip, useEditorContext } from '@graphiql/react';
 import { createGraphiQLFetcher } from '@graphiql/toolkit';
+import { Link1Icon, Share2Icon } from '@radix-ui/react-icons';
 import 'graphiql/graphiql.css';
 
-const Page = ({ endpoint }: { endpoint: string }): ReactElement => {
+function Share(): ReactElement {
+  const { queryEditor, variableEditor, headerEditor } = useEditorContext({
+    nonNull: true,
+  });
+  const copyToClipboard = useClipboard();
+
+  const headers = headerEditor?.getValue();
+  const variables = variableEditor?.getValue();
+  const query = queryEditor?.getValue();
+
+  const label = 'Share query';
+
+  return (
+    <Menu>
+      <Tooltip label={label}>
+        <Menu.Button className="graphiql-toolbar-button" aria-label={label}>
+          <Share2Icon className="graphiql-toolbar-icon" />
+        </Menu.Button>
+      </Tooltip>
+
+      <Menu.List>
+        {['With headers', 'With variables', 'With both'].map((name, i) => (
+          <Menu.Item
+            key={name}
+            onSelect={async () => {
+              const data: { query: string; headers?: string; variables?: string } = { query };
+              if (i === 0 || i === 3) data.headers = headers;
+              if (i === 1 || i === 3) data.variables = variables;
+
+              await copyToClipboard(JSON.stringify(data));
+            }}
+          >
+            {name}
+          </Menu.Item>
+        ))}
+      </Menu.List>
+    </Menu>
+  );
+}
+
+function Page({ endpoint }: { endpoint: string }): ReactElement {
   return (
     <>
       <p className="mb-5 font-light text-gray-500">
@@ -23,14 +65,19 @@ const Page = ({ endpoint }: { endpoint: string }): ReactElement => {
           --color-primary: 40, 89%, 60%;
         }
       `}</style>
-      <GraphiQL fetcher={createGraphiQLFetcher({ url: endpoint })}>
+      <GraphiQL
+        fetcher={createGraphiQLFetcher({ url: endpoint })}
+        toolbar={{
+          additionalContent: <Share />,
+        }}
+      >
         <GraphiQL.Logo>
           <HiveLogo className="h-6 w-6" />
         </GraphiQL.Logo>
       </GraphiQL>
     </>
   );
-};
+}
 
 const TargetLaboratoryPageQuery = graphql(`
   query TargetLaboratoryPageQuery($organizationId: ID!, $projectId: ID!, $targetId: ID!) {
@@ -54,7 +101,7 @@ const TargetLaboratoryPageQuery = graphql(`
 function LaboratoryPage(): ReactElement {
   const [isModalOpen, toggleModalOpen] = useToggle();
   const router = useRouteSelector();
-  const endpoint = `${location.origin}/api/lab/${router.organizationId}/${router.projectId}/${router.targetId}`;
+  const endpoint = `${window.location.origin}/api/lab/${router.organizationId}/${router.projectId}/${router.targetId}`;
 
   return (
     <>
@@ -67,7 +114,7 @@ function LaboratoryPage(): ReactElement {
           <>
             <Button size="large" variant="primary" onClick={toggleModalOpen} className="ml-auto">
               Connect
-              <Link2Icon className="ml-8 h-4 w-4" />
+              <Link1Icon className="ml-8 h-6 w-auto" />
             </Button>
             <ConnectLabModal
               isOpen={isModalOpen}
