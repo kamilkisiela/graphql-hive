f6097f63b26db637098f0aed05cad66a6cdaf55d
Dotan Simha <dotansimha@gmail.com>
2023-03-13 19:35:55 +0800

    Move Grafana dashboards to live in the repository as JSON files (#1700)

---
+++ deployment/services/grafana.ts
@@ -0,0 +1,50 @@
+import { readdirSync, readFileSync } from 'fs';
+import { join, parse } from 'path';
+import { Dashboard, Folder } from '@lbrlabs/pulumi-grafana';
+import * as pulumi from '@pulumi/pulumi';
+
+const dashboardDirectory = join(__dirname, '../grafana-dashboards/');
+
+export function deployGrafana(envName: string) {
+  const availableFiles = readdirSync(dashboardDirectory).filter(f => f.endsWith('.json'));
+  const folder = new Folder('grafana-hive-folder', {
+    title: `Hive Monitoring (${envName})`,
+  });
+
+  const params = new pulumi.Config('grafanaDashboards').requireObject<Record<string, string>>(
+    'params',
+  );
+  params['ENV_NAME'] = envName;
+
+  const dashboards = availableFiles.map(filePath => {
+    const fullPath = join(dashboardDirectory, filePath);
+    const identifier = parse(fullPath).name;
+    let configString = readFileSync(fullPath, 'utf8');
+
+    for (const [key, value] of Object.entries(params)) {
+      if (configString.includes(key)) {
+        configString = configString.replace(new RegExp(key, 'g'), value);
+      }
+    }
+
+    const configJson = JSON.parse(configString);
+
+    if ('uid' in configJson) {
+      delete configJson.uid;
+    }
+
+    if ('version' in configJson) {
+      delete configJson.version;
+    }
+
+    return new Dashboard(`dashboard-${identifier.toLowerCase()}`, {
+      folder: folder.id,
+      configJson: JSON.stringify(configJson, null, 2),
+    });
+  });
+
+  return {
+    folder,
+    dashboards,
+  };
+}
