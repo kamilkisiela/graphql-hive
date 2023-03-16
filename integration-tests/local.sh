cb8d00502abc71590eda0e20ab438a41f7345514
Dotan Simha <dotansimha@gmail.com>
2023-03-16 15:04:59 +0800

    Added a new script for preparing env for integration tests

---
+++ integration-tests/local.sh
@@ -0,0 +1,32 @@
+#/bin/sh
+
+echo "💀 Killing all running Docker containers..."
+docker kill $(docker ps -q)
+
+echo "🧹 Clearing existing Docker volumes..."
+rm -rf ../docker/.hive || true
+
+echo "✨ Clearing unused Docker images and volumes..."
+docker system prune -f
+
+echo "🔨 Build services and libraries for running locally..."
+pnpm prepare:env
+
+echo "🌲 Configuring environment variables..."
+export COMMIT_SHA="local"
+export RELEASE="local"
+export BRANCH_NAME="local"
+export BUILD_TYPE=""
+export DOCKER_TAG=":local"
+export DOCKER_REGISTRY=""
+
+echo "📦 Building local Docker images..."
+cd ..
+docker buildx bake -f docker/docker.hcl integration-tests --load
+
+echo "⬆️ Running all local containers..."
+docker compose -f ./docker/docker-compose.community.yml -f ./integration-tests/docker-compose.integration.yaml --env-file ./integration-tests/.env up -d --wait
+
+echo "✅ Integration tests environment is ready. To run tests now, use:"
+echo ""
+echo "    pnpm test:integration"
