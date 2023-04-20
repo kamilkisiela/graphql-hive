0e14c96374dd655f1b6ad507c503fa237387e351
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-04-20 17:59:05 +0200

    Use SWC instead of Babel in Next (#2063)

--- packages/web/app/.babelrc.js
+++
@@ -1,6 +0,0 @@
-const { babelPlugin } = require('@graphql-codegen/gql-tag-operations-preset');
-
-module.exports = {
-  presets: [['next/babel']],
-  plugins: [[babelPlugin, { artifactDirectory: './src/gql' }]],
-};
