e952390a83e20dbe31954ef51cf98f4dedd45374
YassinEldeeb <yassineldeeb94@gmail.com>
2023-04-20 13:47:41 +0200

    refactor: use a map of log level scores

--- packages/libraries/router/src/logger.rs
+++ packages/libraries/router/src/logger.rs
@@ -1,47 +1,56 @@
+use once_cell::sync::Lazy;
 use std::env;
 
 pub struct Logger {}
 
-static mut LEVELS_VEC: Option<String> = None;
+static CURRENT_LOG_LEVEL: Lazy<String> =
+    Lazy::new(|| env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string()));
+
+const LOG_LEVEL_SCORES: [(LogLevel, u8); 5] = [
+    ("debug", 5),
+    ("info", 4),
+    ("warn", 3),
+    ("error", 2),
+    ("trace", 1),
+];
+
+type LogLevel = &'static str;
 
 impl Logger {
-    fn should_log(levels: Vec<&str>) -> bool {
-        unsafe {
-            if LEVELS_VEC == None {
-                let log_level = env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
-                LEVELS_VEC = Some(log_level);
-            }
+    fn find_log_level_score(log_level: &str) -> u8 {
+        LOG_LEVEL_SCORES
+            .iter()
+            .find(|(l, _)| *l == log_level)
+            .map(|(_, score)| *score)
+            .unwrap_or(4) // default log level is info
+    }
 
-            if let Some(log_level) = &LEVELS_VEC {
-                levels.contains(&log_level.as_str())
-            } else {
-                false
-            }
-        }
+    fn should_log(level: &str) -> bool {
+        Self::find_log_level_score(CURRENT_LOG_LEVEL.as_str()) >= Self::find_log_level_score(level)
     }
 
     pub fn debug(message: &str) {
-        if Self::should_log(vec!["debug", "info", "warn", "error"]) {
+        if Self::should_log("debug") {
             println!("DEBUG: {}", message);
         }
     }
     pub fn info(message: &str) {
-        if Self::should_log(vec!["info", "warn", "error"]) {
+        if Self::should_log("info") {
             println!("INFO: {}", message);
         }
     }
     pub fn warn(message: &str) {
-        if Self::should_log(vec!["warn", "error"]) {
+        if Self::should_log("warn") {
             println!("WARNING: {}", message);
         }
     }
     pub fn trace(message: &str) {
-        if Self::should_log(vec!["debug", "info", "error", "trace"]) {
+        if Self::should_log("trace") {
             println!("TRACE: {}", message);
         }
     }
     pub fn error(message: &str) {
-        if Self::should_log(vec!["debug", "error"]) {
+        if Self::should_log("error") {
             println!("ERROR: {}", message);
         }
     }
