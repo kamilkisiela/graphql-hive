fa3350465b4f2f91649439cfddcee9d7d1f1b58b
YassinEldeeb <yassineldeeb94@gmail.com>
2023-04-20 14:06:07 +0200

    refactor: find_log_level_score using indexes instead of a map

--- packages/libraries/router/src/logger.rs
+++ packages/libraries/router/src/logger.rs
@@ -6,22 +6,20 @@ pub struct Logger {}
 static CURRENT_LOG_LEVEL: Lazy<String> =
     Lazy::new(|| env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string()));
 
-const LOG_LEVEL_SCORES: [(LogLevel, u8); 5] = [
-    ("debug", 5),
-    ("info", 4),
-    ("warn", 3),
-    ("error", 2),
-    ("trace", 1),
-];
+/// The order of this Array's items is important. The higher the index, the more logs it includes.
+///
+///
+/// ### Example
+/// When log level is equal to `info`, it will include `info` and all of the previous indices like `warn`, `error`, and `trace`.
+const LOG_LEVEL_SCORES: [LogLevel; 5] = ["trace", "error", "warn", "info", "debug"];
 
 type LogLevel = &'static str;
 
 impl Logger {
-    fn find_log_level_score(log_level: &str) -> u8 {
+    fn find_log_level_score(log_level: &str) -> usize {
         LOG_LEVEL_SCORES
             .iter()
-            .find(|(l, _)| *l == log_level)
-            .map(|(_, score)| *score)
+            .position(|&x| x == log_level)
             .expect("Invalid log level")
     }
 
