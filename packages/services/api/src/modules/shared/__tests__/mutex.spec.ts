1810457f3395ad73968e270dab9c88de6f10ae75
enisdenjo <badurinadenis@gmail.com>
2023-04-20 19:47:24 +0200

    multi thread = multi process

--- packages/services/api/src/modules/shared/__tests__/mutex.spec.ts
+++ packages/services/api/src/modules/shared/__tests__/mutex.spec.ts
@@ -3,88 +3,84 @@ import 'reflect-metadata';
 import { Logger } from '../providers/logger';
 import { Mutex } from '../providers/mutex';
 
-describe('Single process', () => {
-  it('should allow only one lock at a time', async ({ expect }) => {
-    const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
+it('should allow only one lock at a time', async ({ expect }) => {
+  const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
 
-    const [signal] = createSignal();
+  const [signal] = createSignal();
 
-    const unlock1 = await mutex.lock('1', { signal });
+  const unlock1 = await mutex.lock('1', { signal });
 
-    const lock2 = mutex.lock('1', { signal });
+  const lock2 = mutex.lock('1', { signal });
 
-    // second lock shouldnt resolve
-    await expect(Promise.race([throwAfter(), lock2])).rejects.toBeTruthy();
+  // second lock shouldnt resolve
+  await expect(Promise.race([throwAfter(), lock2])).rejects.toBeTruthy();
 
-    await unlock1();
+  await unlock1();
 
-    // after the first lock releases, second one resolves
-    await expect(lock2).resolves.toBeTruthy();
-  });
+  // after the first lock releases, second one resolves
+  await expect(lock2).resolves.toBeTruthy();
+});
 
-  it('should allow different locks at any time', async ({ expect }) => {
-    const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
+it('should allow different locks at any time', async ({ expect }) => {
+  const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
 
-    const [signal] = createSignal();
+  const [signal] = createSignal();
 
-    await expect(mutex.lock('1', { signal })).resolves.toBeTruthy();
-    await expect(mutex.lock('2', { signal })).resolves.toBeTruthy();
-    await expect(mutex.lock('3', { signal })).resolves.toBeTruthy();
-  });
+  await expect(mutex.lock('1', { signal })).resolves.toBeTruthy();
+  await expect(mutex.lock('2', { signal })).resolves.toBeTruthy();
+  await expect(mutex.lock('3', { signal })).resolves.toBeTruthy();
+});
 
-  it('should time out after the specified duration', async ({ expect }) => {
-    const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
+it('should time out after the specified duration', async ({ expect }) => {
+  const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
 
-    const [signal] = createSignal();
+  const [signal] = createSignal();
 
-    await mutex.lock('1', { signal });
+  await mutex.lock('1', { signal });
 
-    const lock2 = mutex.lock('1', { signal, timeout: 50, retries: 0 });
+  const lock2 = mutex.lock('1', { signal, timeout: 50, retries: 0 });
 
-    await expect(lock2).rejects.toMatchInlineSnapshot(
-      '[ExecutionError: The operation was unable to achieve a quorum during its retry window.]',
-    );
-  });
+  await expect(lock2).rejects.toMatchInlineSnapshot(
+    '[ExecutionError: The operation was unable to achieve a quorum during its retry window.]',
+  );
+});
 
-  it('should cancel locking on abort signal', async ({ expect }) => {
-    const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
+it('should cancel locking on abort signal', async ({ expect }) => {
+  const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
 
-    const [signal, abort] = createSignal();
+  const [signal, abort] = createSignal();
 
-    const unlock1 = await mutex.lock('1', { signal });
+  const unlock1 = await mutex.lock('1', { signal });
 
-    const lock2 = mutex.lock('1', { signal });
+  const lock2 = mutex.lock('1', { signal });
 
-    abort();
+  abort();
 
-    await expect(lock2).rejects.toMatchInlineSnapshot('[Error: Locking aborted]');
+  await expect(lock2).rejects.toMatchInlineSnapshot('[Error: Locking aborted]');
 
-    await unlock1();
+  await unlock1();
 
-    // make sure that the aborted lock does not lock
-    await expect(mutex.lock('1', { signal: createSignal()[0] })).resolves.toBeTruthy();
-  });
-
-  describe.concurrent('should serialise concurrent threads', () => {
-    const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
-    const [signal] = createSignal();
-
-    let running = false;
-    for (let i = 1; i <= 20; i++) {
-      it.concurrent(`#${i}`, async ({ expect }) => {
-        await mutex.perform('1', { signal }, async () => {
-          expect(running).toBeFalsy();
-          running = true;
-          await sleep();
-          running = false;
-        });
-      });
-    }
-  });
+  // make sure that the aborted lock does not lock
+  await expect(mutex.lock('1', { signal: createSignal()[0] })).resolves.toBeTruthy();
 });
 
 // since vitest uses workers (which are kindof separate processes), this can be tested
-describe.todo('Multiple processes');
+describe.concurrent('should serialise concurrent threads', () => {
+  const mutex = new Mutex(new Tlogger(), new Redis(randomPort()));
+  const [signal] = createSignal();
+
+  let running = false;
+  for (let i = 1; i <= 20; i++) {
+    it.concurrent(`#${i}`, async ({ expect }) => {
+      await mutex.perform('1', { signal }, async () => {
+        expect(running).toBeFalsy();
+        running = true;
+        await sleep();
+        running = false;
+      });
+    });
+  }
+});
 
 class Tlogger implements Logger {
   public info = vi.fn();
