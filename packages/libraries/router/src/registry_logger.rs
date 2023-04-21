eabdfb5d2db06ac0eaa96244ca380d5103730ba7
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-04-21 12:01:28 +0200

    Update packages/libraries/router/src/registry_logger.rs
    Co-authored-by: Yassin Eldeeb <yassineldeeb94@gmail.com>

--- packages/libraries/router/src/registry_logger.rs
+++ packages/libraries/router/src/registry_logger.rs
@@ -4,7 +4,7 @@ static LOG_LEVEL_NAMES: [&str; 5] = ["ERROR", "WARN", "INFO", "DEBUG", "TRACE"];
 
 #[repr(usize)]
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Debug)]
-pub enum Level {
+pub enum LogLevel {
     Error,
     Warn,
     Info,
@@ -12,23 +12,23 @@ pub enum Level {
     Trace,
 }
 
-impl Level {
-    fn from_usize(u: usize) -> Option<Level> {
+impl LogLevel {
+    fn from_usize(u: usize) -> Option<LogLevel> {
         match u {
-            0 => Some(Level::Error),
-            1 => Some(Level::Warn),
-            2 => Some(Level::Info),
-            3 => Some(Level::Debug),
-            4 => Some(Level::Trace),
+            0 => Some(LogLevel::Error),
+            1 => Some(LogLevel::Warn),
+            2 => Some(LogLevel::Info),
+            3 => Some(LogLevel::Debug),
+            4 => Some(LogLevel::Trace),
             _ => None,
         }
     }
 
-    fn from_str(s: &str) -> Level {
+    fn from_str(s: &str) -> LogLevel {
         LOG_LEVEL_NAMES
             .iter()
             .position(|&name| name.eq_ignore_ascii_case(s))
-            .map(|p| Level::from_usize(p).expect("Hive failed to read the log level"))
+            .map(|p| LogLevel::from_usize(p).expect("Hive failed to read the log level"))
             .expect("Hive failed to parse the log level filter")
     }
 
@@ -51,7 +51,7 @@ pub struct Logger {
 impl Logger {
     pub fn new() -> Logger {
         Self {
-            max_level: Level::from_str(
+            max_level: LogLevel::from_str(
                 env::var("HIVE_REGISTRY_LOG")
                     .unwrap_or_else(|_| "info".to_string())
                     .as_str(),
@@ -59,39 +59,39 @@ impl Logger {
         }
     }
 
-    fn should_log(&self, level: Level) -> bool {
+    fn should_log(&self, level: LogLevel) -> bool {
         self.max_level >= level
     }
 
     #[allow(dead_code)]
     pub fn trace(&self, message: &str) {
-        if self.should_log(Level::Trace) {
+        if self.should_log(LogLevel::Trace) {
             println!("TRACE: {}", message);
         }
     }
 
     #[allow(dead_code)]
     pub fn debug(&self, message: &str) {
-        if self.should_log(Level::Debug) {
+        if self.should_log(LogLevel::Debug) {
             println!("DEBUG: {}", message);
         }
     }
 
     pub fn info(&self, message: &str) {
-        if self.should_log(Level::Info) {
+        if self.should_log(LogLevel::Info) {
             println!("INFO: {}", message);
         }
     }
 
     #[allow(dead_code)]
     pub fn warn(&self, message: &str) {
-        if self.should_log(Level::Warn) {
+        if self.should_log(LogLevel::Warn) {
             println!("WARNING: {}", message);
         }
     }
 
     pub fn error(&self, message: &str) {
-        if self.should_log(Level::Error) {
+        if self.should_log(LogLevel::Error) {
             println!("ERROR: {}", message);
         }
     }
