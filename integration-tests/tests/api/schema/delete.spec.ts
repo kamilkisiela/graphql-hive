b329d2ff34a52ce5180ccfb10d534b076e9e737c
Laurin Quast <laurinquast@googlemail.com>
2023-05-11 17:08:13 +0200

    test: check whether changes are persisted

--- integration-tests/tests/api/schema/delete.spec.ts
+++ integration-tests/tests/api/schema/delete.spec.ts
@@ -1,6 +1,27 @@
+import 'reflect-metadata';
 import { initSeed } from 'testkit/seed';
 import { ProjectType, TargetAccessScope } from '@app/gql/graphql';
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { createStorage } from '@hive/storage';
+
+function connectionString() {
+  const {
+    POSTGRES_USER = 'postgres',
+    POSTGRES_PASSWORD = 'postgres',
+    POSTGRES_HOST = 'localhost',
+    POSTGRES_PORT = 5432,
+    POSTGRES_DB = 'registry',
+    POSTGRES_SSL = null,
+    POSTGRES_CONNECTION_STRING = null,
+    // eslint-disable-next-line no-process-env
+  } = process.env;
+  return (
+    POSTGRES_CONNECTION_STRING ||
+    `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${
+      POSTGRES_SSL ? '?sslmode=require' : '?sslmode=disable'
+    }`
+  );
+}
 
 const s3Client = new S3Client({
   endpoint: 'http://127.0.0.1:9000',
@@ -32,7 +53,7 @@ function normalizeSDL(sdl: string): string {
 }
 
 test.concurrent(
-  'can delete a service and updates database and CDN when the super schema is still composable',
+  'can delete a service and updates the CDN when the super schema is still composable',
   async () => {
     const { createOrg } = await initSeed().createOwner();
     const { createProject } = await createOrg();
@@ -95,5 +116,84 @@ test.concurrent(
         }
       `),
     );
+  },
+);
+
+test.concurrent(
+  'the changes and schema sdl is persisted in the database when the super schema schema is composable',
+  async ({ expect }) => {
+    let storage: Awaited<ReturnType<typeof createStorage>>;
+
+    try {
+      storage = await createStorage(connectionString(), 1);
+      const { createOrg } = await initSeed().createOwner();
+      const { createProject, organization } = await createOrg();
+      const { createToken, project, target } = await createProject(ProjectType.Federation);
+
+      const readToken = await createToken({
+        targetScopes: [TargetAccessScope.RegistryRead, TargetAccessScope.RegistryWrite],
+        projectScopes: [],
+        organizationScopes: [],
+      });
+
+      const publishService1Result = await readToken
+        .publishSchema({
+          sdl: /* GraphQL */ `
+            type Query {
+              ping: String
+            }
+          `,
+          service: 'foo',
+          url: 'http://localhost:4000/graphql',
+        })
+        .then(r => r.expectNoGraphQLErrors());
+      expect(publishService1Result.schemaPublish.__typename).toBe('SchemaPublishSuccess');
+
+      const publishService2Result = await readToken
+        .publishSchema({
+          sdl: /* GraphQL */ `
+            type Query {
+              bruv: String
+            }
+          `,
+          service: 'foo1',
+          url: 'http://localhost:4000/graphql',
+        })
+        .then(r => r.expectNoGraphQLErrors());
+      expect(publishService2Result.schemaPublish.__typename).toBe('SchemaPublishSuccess');
+
+      const deleteServiceResult = await readToken
+        .deleteSchema('foo1')
+        .then(r => r.expectNoGraphQLErrors());
+      expect(deleteServiceResult.schemaDelete.__typename).toBe('SchemaDeleteSuccess');
+
+      const latestVersion = await storage.getLatestVersion({
+        target: target.id,
+        project: project.id,
+        organization: organization.id,
+      });
+
+      const changes = await storage.getSchemaChangesForVersion({
+        versionId: latestVersion.id,
+      });
+
+      if (Array.isArray(changes) === false) {
+        throw new Error('No changes were persisted in the database.');
+      }
+
+      expect(changes[0]).toMatchInlineSnapshot(`
+        {
+          meta: {
+            isRemovedFieldDeprecated: false,
+            removedFieldName: bruv,
+            typeName: Query,
+            typeType: object type,
+          },
+          type: FIELD_REMOVED,
+        }
+      `);
+    } finally {
+      await storage?.destroy();
+    }
   },
 );
