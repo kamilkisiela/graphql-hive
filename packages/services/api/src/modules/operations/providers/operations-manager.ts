89c9cee533d84459921e776507460ed777e2baf6
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-06-21 10:35:58 +0200

    Check existence of operations in more efficient way

--- packages/services/api/src/modules/operations/providers/operations-manager.ts
+++ packages/services/api/src/modules/operations/providers/operations-manager.ts
@@ -154,10 +154,9 @@ export class OperationsManager {
 
     return hasCollectedOperationsCached(target, () =>
       this.reader
-        .countOperations({
+        .hasCollectedOperations({
           target,
-        })
-        .then(r => r.total > 0),
+        }),
     );
   }
 
@@ -707,6 +706,7 @@ export class OperationsManager {
   }
 
   async hasOperationsForOrganization(selector: OrganizationSelector): Promise<boolean> {
+    this.logger.info('Checking existence of collected operations (organization=%s)', selector.organization);
     const targets = await this.storage.getTargetIdsOfOrganization(selector);
 
     if (targets.length === 0) {
