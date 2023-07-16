0becf3754fb1ff75435428d9bc72c37c938f1b7b
Laurin Quast <laurinquast@googlemail.com>
2023-07-16 14:20:39 +0200

    try workaround

--- deployment/services/database-cleanup.ts
+++ deployment/services/database-cleanup.ts
@@ -25,7 +25,14 @@ export function deployDatabaseCleanupJob(options: { deploymentEnv: DeploymentEnv
         'psql',
         '$(PG_CONNECTION_STRING)',
         '-c',
-        'DROP SCHEMA public CASCADE; CREATE SCHEMA public;',
+        `
+"DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;"`,
       ],
     },
     [],
