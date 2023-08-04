39cde5333d6ec2661ff5b3b27125be74bb091f92
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-08-04 19:11:26 +0200

    Add sign-out button to /org/new page (#2710)

--- packages/web/app/pages/org/new.tsx
+++ packages/web/app/pages/org/new.tsx
@@ -1,14 +1,25 @@
 import { ReactElement } from 'react';
+import { useRouter } from 'next/router';
+import { LogOutIcon } from 'lucide-react';
 import { authenticated } from '@/components/authenticated-container';
+import { Button } from '@/components/ui/button';
 import { MetaTitle } from '@/components/v2';
 import { CreateOrganizationForm } from '@/components/v2/modals/create-organization';
 import { withSessionProtection } from '@/lib/supertokens/guard';
 
 function CreateOrgPage(): ReactElement {
+  const router = useRouter();
   return (
     <>
       <MetaTitle title="Create Organization" />
       <div className="h-full grow flex items-center">
+        <Button
+          variant="outline"
+          onClick={() => router.push('/logout')}
+          className="absolute top-6 right-6"
+        >
+          <LogOutIcon className="mr-2 h-4 w-4" /> Sign out
+        </Button>
         <div className="container w-1/3">
           <CreateOrganizationForm />
         </div>
