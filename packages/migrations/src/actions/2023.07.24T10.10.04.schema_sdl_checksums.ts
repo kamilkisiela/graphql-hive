29bab017564387914e512f9b2804d05ff668f37f
Andrii Hrachov <andrii.hrachov@deliveryhero.com>
2023-08-05 18:46:14 +0200

    fix migration syntax

--- packages/migrations/src/actions/2023.07.24T10.10.04.schema_sdl_checksums.ts
+++ packages/migrations/src/actions/2023.07.24T10.10.04.schema_sdl_checksums.ts
@@ -12,16 +12,16 @@ CREATE TABLE IF NOT EXISTS "public"."schema_sdl_checksums" (
     PRIMARY KEY(target_id, checksum));
 ALTER TABLE "public"."schema_checks"
   ADD COLUMN "schema_checksum" text,
-  ADD COLUMN "composite_schema_checksum" text
+  ADD COLUMN "composite_schema_checksum" text,
   ADD COLUMN "supergraph_schema_checksum" text,
-  
+
   ADD CONSTRAINT schema_sdl_fk
       FOREIGN KEY(target_id, schema_checksum) 
 	    REFERENCES schema_sdl_checksums(target_id, checksum),
   
   ADD CONSTRAINT schema_composite_sdl_fk
     FOREIGN KEY(target_id, composite_schema_checksum) 
-  REFERENCES schema_sdl_checksums(target_id, checksum)
+  REFERENCES schema_sdl_checksums(target_id, checksum),
 
   ADD CONSTRAINT supergraph_schema_sdl_fk
     FOREIGN KEY(target_id, supergraph_schema_checksum) 
