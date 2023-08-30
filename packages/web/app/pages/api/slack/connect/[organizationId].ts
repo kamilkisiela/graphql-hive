d5a9b8f84048b0b2a081b64ee792fcb7ba5cac7c
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-08-30 11:20:50 +0200

    Fix slack connect endpoint
    Use organizationId instead of orgId

--- packages/web/app/pages/api/slack/connect/[organizationId].ts
+++ packages/web/app/pages/api/slack/connect/[organizationId].ts
@@ -9,11 +9,11 @@ export default async function slackConnectOrg(req: NextApiRequest, res: NextApiR
     logger.error('The Slack integration is not enabled.');
     throw new Error('The Slack integration is not enabled.');
   }
-  const { orgId } = req.query;
-  logger.info('Connect organization to Slack (id=%s)', orgId);
+  const { organizationId } = req.query;
+  logger.info('Connect organization to Slack (id=%s)', organizationId);
 
   const slackUrl = `https://slack.com/oauth/v2/authorize?scope=incoming-webhook,chat:write,chat:write.public,commands&client_id=${env.slack.clientId}`;
   const redirectUrl = `${env.appBaseUrl}/api/slack/callback`;
 
-  res.redirect(`${slackUrl}&state=${orgId}&redirect_uri=${redirectUrl}`);
+  res.redirect(`${slackUrl}&state=${organizationId}&redirect_uri=${redirectUrl}`);
 }
