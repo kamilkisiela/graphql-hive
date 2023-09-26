28ef158efb7979118ae14d743ed349ff48e199ca
Dotan Simha <dotansimha@gmail.com>
2023-09-26 15:13:00 +0300

    Fix metrics server resource allocation (#2955)

--- deployment/utils/azure-helpers.ts
+++ deployment/utils/azure-helpers.ts
@@ -2,6 +2,30 @@ import * as k8s from '@pulumi/kubernetes';
 
 export function optimizeAzureCluster() {
   /**
+   * The following configures the metrics server to use the correct resources limit:
+   * https://learn.microsoft.com/en-us/azure/aks/use-metrics-server-vertical-pod-autoscaler
+   */
+  new k8s.core.v1.ConfigMap('metrics-server-config', {
+    metadata: {
+      name: 'metrics-server-config',
+      namespace: 'kube-system',
+      labels: {
+        'kubernetes.io/cluster-service': 'true',
+        'addonmanager.kubernetes.io/mode': 'EnsureExists',
+      },
+    },
+    data: {
+      NannyConfiguration: `
+apiVersion: nannyconfig/v1alpha1
+kind: NannyConfiguration
+baseCPU: 200m
+cpuPerNode: 3m
+baseMemory: 200Mi
+memoryPerNode: 24Mi`,
+    },
+  });
+
+  /**
    * The following disabled Azure logging. We are not really using it.
    */
   new k8s.core.v1.ConfigMap('optimize-azure-cluster', {
