5a4b0b97ac8882d01da8e9c08788b148b79b7c2b
Laurin Quast <laurinquast@googlemail.com>
2023-10-05 16:54:34 +0200

    fix regex

--- packages/services/api/src/shared/is-github-repository-string.ts
+++ packages/services/api/src/shared/is-github-repository-string.ts
@@ -4,9 +4,10 @@
  */
 export function isGitHubRepositoryString(repository: string): repository is `${string}/${string}` {
   const [owner, name] = repository.split('/');
-  return !!owner && isAlphaNumeric(owner) && !!name && isAlphaNumeric(name);
+  return !!owner && isLegitGitHubName(owner) && !!name && isLegitGitHubName(name);
 }
 
-function isAlphaNumeric(str: string) {
-  return /^[a-z0-9]+$/i.test(str);
+/** @source https://stackoverflow.com/a/59082561 */
+function isLegitGitHubName(str: string) {
+  return /^[\w-\.]+$/i.test(str);
 }
