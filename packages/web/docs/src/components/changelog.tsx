c6d92695019f3e18f152482ca21612dcd88fd524
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-10-10 11:11:18 +0200

    Add recent updates to the changelog (#3019)

--- packages/web/docs/src/components/changelog.tsx
+++ packages/web/docs/src/components/changelog.tsx
@@ -2,32 +2,6 @@ import { ReactElement } from 'react';
 import Link from 'next/link';
 import { format } from 'date-fns';
 
-function LearnMoreLink(props: { href: string }) {
-  return (
-    <Link
-      href={props.href}
-      className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-    >
-      Learn more{' '}
-      <svg
-        className="w-3 h-3 ml-2"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 14 10"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M1 5h12m0 0L9 1m4 4L9 9"
-        />
-      </svg>
-    </Link>
-  );
-}
-
 function ChangelogItem(props: { title: string; date: string; description: string; route: string }) {
   return (
     <li className="mb-10 ml-4">
@@ -38,11 +12,12 @@ function ChangelogItem(props: { title: string; date: string; description: string
       >
         {format(new Date(props.date), 'do MMMM yyyy')}
       </time>
-      <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{props.title}</h3>
+      <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+        <Link href={props.route}>{props.title}</Link>
+      </h3>
       <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
         {props.description}
       </p>
-      <LearnMoreLink href={props.route} />
     </li>
   );
 }
@@ -50,7 +25,10 @@ function ChangelogItem(props: { title: string; date: string; description: string
 export const Changelog = ({ changelogs }): ReactElement => {
   return (
     <>
-      <h1>Changelog</h1>
+      <div className="pb-12">
+        <h1 className="!text-left !m-0">Changelog</h1>
+        <p>The most recent developments from GraphQL Hive.</p>
+      </div>
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
         {changelogs.map(item => (
           <ChangelogItem key={item.href} {...item} />
