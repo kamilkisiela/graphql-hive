7d00b9ed6b36521b2c4e42beba059ab569e3bffa
Kamil Kisiela <kamil.kisiela@gmail.com>
2023-10-24 10:00:04 +0200

    Avoid panicking in Usage Plugin and ensure valid supergraph SDL (#2783)
    Co-authored-by: Yassin Eldeeb <yassineldeeb94@gmail.com>

--- packages/libraries/router/src/usage.rs
+++ packages/libraries/router/src/usage.rs
@@ -1,4 +1,4 @@
-use crate::agent::{ExecutionReport, UsageAgent};
+use crate::agent::{AgentError, ExecutionReport, UsageAgent};
 use apollo_router::layers::ServiceBuilderExt;
 use apollo_router::plugin::Plugin;
 use apollo_router::plugin::PluginInit;
@@ -33,8 +33,16 @@ struct OperationContext {
     pub(crate) dropped: bool,
 }
 
+#[derive(Clone, Debug)]
+struct OperationConfig {
+    sample_rate: f64,
+    exclude: Option<Vec<String>>,
+    client_name_header: String,
+    client_version_header: String,
+}
+
 struct UsagePlugin {
-    config: Config,
+    config: OperationConfig,
     agent: Option<Arc<Mutex<UsageAgent>>>,
 }
 
@@ -73,8 +81,8 @@ impl Default for Config {
             enabled: Some(true),
             sample_rate: Some(1.0),
             exclude: None,
-            client_name_header: None,
-            client_version_header: None,
+            client_name_header: Some(String::from("graphql-client-name")),
+            client_version_header: Some(String::from("graphql-client-version")),
             accept_invalid_certs: Some(false),
             buffer_size: Some(1000),
             connect_timeout: Some(5),
@@ -84,17 +92,10 @@ impl Default for Config {
 }
 
 impl UsagePlugin {
-    fn populate_context(config: Config, req: &supergraph::Request) {
+    fn populate_context(config: OperationConfig, req: &supergraph::Request) {
         let context = &req.context;
         let http_request = &req.supergraph_request;
         let headers = http_request.headers();
-
-        let client_name_header = config
-            .client_name_header
-            .unwrap_or("graphql-client-name".to_string());
-        let client_version_header = config
-            .client_version_header
-            .unwrap_or("graphql-client-version".to_string());
 
         let get_header_value = |key: &str| {
             headers
@@ -106,8 +107,8 @@ impl UsagePlugin {
                 .map(|v| v.to_string())
         };
 
-        let client_name = get_header_value(&client_name_header);
-        let client_version = get_header_value(&client_version_header);
+        let client_name = get_header_value(&config.client_name_header);
+        let client_version = get_header_value(&config.client_version_header);
 
         let operation_name = req.supergraph_request.body().operation_name.clone();
         let operation_body = req
@@ -117,7 +118,6 @@ impl UsagePlugin {
             .clone()
             .expect("operation body should not be empty");
 
-        let sample_rate = config.sample_rate.clone();
         let excluded_operation_names: HashSet<String> = config
             .exclude
             .unwrap_or_else(|| vec![])
@@ -126,13 +126,8 @@ impl UsagePlugin {
             .collect();
 
         let mut rng = rand::thread_rng();
-        let mut dropped = match sample_rate {
-            Some(rate) => {
-                let num: f64 = rng.gen();
-                num > rate
-            }
-            None => false,
-        };
+        let sampled = rng.gen::<f64>() < config.sample_rate;
+        let mut dropped = !sampled;
 
         if !dropped {
             match &operation_name {
@@ -177,19 +172,48 @@ impl Plugin for UsagePlugin {
         };
 
         let default_config = Config::default();
-        let enabled = init.config.enabled.or(default_config.enabled).expect("enabled has default value");
-        let buffer_size = init.config.buffer_size.or(default_config.buffer_size).expect("buffer_size has default value");
-        let accept_invalid_certs = init.config.accept_invalid_certs.or(default_config.accept_invalid_certs).expect("accept_invalid_certs has default value");
-        let connect_timeout = init.config.connect_timeout.or(default_config.connect_timeout).expect("connect_timeout has default value");
-        let request_timeout = init.config.request_timeout.or(default_config.request_timeout).expect("request_timeout has default value");
+        let user_config = init.config;
+        let enabled = user_config
+            .enabled
+            .or(default_config.enabled)
+            .expect("enabled has default value");
+        let buffer_size = user_config
+            .buffer_size
+            .or(default_config.buffer_size)
+            .expect("buffer_size has no default value");
+        let accept_invalid_certs = user_config
+            .accept_invalid_certs
+            .or(default_config.accept_invalid_certs)
+            .expect("accept_invalid_certs has no default value");
+        let connect_timeout = user_config
+            .connect_timeout
+            .or(default_config.connect_timeout)
+            .expect("connect_timeout has no default value");
+        let request_timeout = user_config
+            .request_timeout
+            .or(default_config.request_timeout)
+            .expect("request_timeout has no default value");
 
         if enabled {
             tracing::info!("Starting GraphQL Hive Usage plugin");
         }
 
-
         Ok(UsagePlugin {
-            config: init.config,
+            config: OperationConfig {
+                sample_rate: user_config
+                    .sample_rate
+                    .or(default_config.sample_rate)
+                    .expect("sample_rate has no default value"),
+                exclude: user_config.exclude.or(default_config.exclude),
+                client_name_header: user_config
+                    .client_name_header
+                    .or(default_config.client_name_header)
+                    .expect("client_name_header has no default value"),
+                client_version_header: user_config
+                    .client_version_header
+                    .or(default_config.client_version_header)
+                    .expect("client_version_header has no default value"),
+            },
             agent: match enabled {
                 true => Some(Arc::new(Mutex::new(UsageAgent::new(
                     init.supergraph_sdl.to_string(),
@@ -211,98 +235,120 @@ impl Plugin for UsagePlugin {
             None => ServiceBuilder::new().service(service).boxed(),
             Some(agent) => {
                 ServiceBuilder::new()
-                .map_future_with_request_data(
-                    move |req: &supergraph::Request| {
-                        Self::populate_context(config.clone(), req);
-                        req.context.clone()
-                    },
-                    move |ctx: Context, fut| {
-                        let agent_clone = agent.clone();
-                        async move {
-                            let start = Instant::now();
-    
-                            // nested async block, bc async is unstable with closures that receive arguments
-                            let operation_context = ctx
-                                .get::<_, OperationContext>(OPERATION_CONTEXT)
-                                .unwrap_or_default()
-                                .unwrap();
-    
-                            let result: supergraph::ServiceResult = fut.await;
-    
-                            if operation_context.dropped {
-                                tracing::debug!("Dropping operation (phase: SAMPLING): {}", operation_context.operation_name.clone().or_else(|| Some("anonymous".to_string())).unwrap());
-                                return result;
-                            }
-    
-                            let OperationContext {
-                                client_name,
-                                client_version,
-                                operation_name,
-                                timestamp,
-                                operation_body,
-                                ..
-                            } = operation_context;
-    
-                            let duration = start.elapsed();
-    
-                            match result {
-                                Err(e) => {
-                                    agent_clone
-                                        .clone()
-                                        .lock()
-                                        .expect("Unable to acquire Agent in supergraph_service (error)")
-                                        .add_report(ExecutionReport {
-                                            client_name,
-                                            client_version,
-                                            timestamp,
-                                            duration,
-                                            ok: false,
-                                            errors: 1,
-                                            operation_body,
-                                            operation_name,
-                                        });
-                                    Err(e)
+                    .map_future_with_request_data(
+                        move |req: &supergraph::Request| {
+                            Self::populate_context(config.clone(), req);
+                            req.context.clone()
+                        },
+                        move |ctx: Context, fut| {
+                            let agent_clone = agent.clone();
+                            async move {
+                                let start = Instant::now();
+
+                                // nested async block, bc async is unstable with closures that receive arguments
+                                let operation_context = ctx
+                                    .get::<_, OperationContext>(OPERATION_CONTEXT)
+                                    .unwrap_or_default()
+                                    .unwrap();
+
+                                let result: supergraph::ServiceResult = fut.await;
+
+                                if operation_context.dropped {
+                                    tracing::debug!(
+                                        "Dropping operation (phase: SAMPLING): {}",
+                                        operation_context
+                                            .operation_name
+                                            .clone()
+                                            .or_else(|| Some("anonymous".to_string()))
+                                            .unwrap()
+                                    );
+                                    return result;
                                 }
-                                Ok(router_response) => {
-                                    let is_failure = !router_response.response.status().is_success();
-                                    Ok(router_response.map(move |response_stream| {
-                                        let client_name = client_name.clone();
-                                        let client_version = client_version.clone();
-                                        let operation_body = operation_body.clone();
-                                        let operation_name = operation_name.clone();
-    
-                                        let res = response_stream
-                                            .map(move |response| {
-                                                // make sure we send a single report, not for each chunk
-                                                let response_has_errors = !response.errors.is_empty();
-                                                agent_clone.clone().lock()
-                                                .expect("Unable to acquire Agent in supergraph_service (ok)").add_report(ExecutionReport {
-                                                    client_name: client_name.clone(),
-                                                    client_version: client_version.clone(),
-                                                    timestamp,
-                                                    duration,
-                                                    ok: !is_failure && !response_has_errors,
-                                                    errors: response.errors.len(),
-                                                    operation_body: operation_body.clone(),
-                                                    operation_name: operation_name.clone(),
-                                                });
-    
-                                                response
-                                            })
-                                            .boxed();
-    
-                                        res
-                                    }))
+
+                                let OperationContext {
+                                    client_name,
+                                    client_version,
+                                    operation_name,
+                                    timestamp,
+                                    operation_body,
+                                    ..
+                                } = operation_context;
+
+                                let duration = start.elapsed();
+
+                                match result {
+                                    Err(e) => {
+                                        try_add_report(
+                                            agent_clone.clone(),
+                                            ExecutionReport {
+                                                client_name,
+                                                client_version,
+                                                timestamp,
+                                                duration,
+                                                ok: false,
+                                                errors: 1,
+                                                operation_body,
+                                                operation_name,
+                                            },
+                                        );
+                                        Err(e)
+                                    }
+                                    Ok(router_response) => {
+                                        let is_failure =
+                                            !router_response.response.status().is_success();
+                                        Ok(router_response.map(move |response_stream| {
+                                            let client_name = client_name.clone();
+                                            let client_version = client_version.clone();
+                                            let operation_body = operation_body.clone();
+                                            let operation_name = operation_name.clone();
+
+                                            let res = response_stream
+                                                .map(move |response| {
+                                                    // make sure we send a single report, not for each chunk
+                                                    let response_has_errors =
+                                                        !response.errors.is_empty();
+                                                    try_add_report(
+                                                        agent_clone.clone(),
+                                                        ExecutionReport {
+                                                            client_name: client_name.clone(),
+                                                            client_version: client_version.clone(),
+                                                            timestamp,
+                                                            duration,
+                                                            ok: !is_failure && !response_has_errors,
+                                                            errors: response.errors.len(),
+                                                            operation_body: operation_body.clone(),
+                                                            operation_name: operation_name.clone(),
+                                                        },
+                                                    );
+
+                                                    response
+                                                })
+                                                .boxed();
+
+                                            res
+                                        }))
+                                    }
                                 }
                             }
-                        }
-                    },
-                )
-                .service(service)
-                .boxed()
+                        },
+                    )
+                    .service(service)
+                    .boxed()
             }
         }
     }
+}
+
+fn try_add_report(agent: Arc<Mutex<UsageAgent>>, execution_report: ExecutionReport) {
+    agent
+        .lock()
+        .map_err(|e| {
+AgentError::Lock(e.to_string())
+        })
+        .and_then(|a| a.add_report(execution_report))
+        .unwrap_or_else(|e| {
+            tracing::error!("Error adding report: {}", e);
+        });
 }
 
 impl Drop for UsagePlugin {
