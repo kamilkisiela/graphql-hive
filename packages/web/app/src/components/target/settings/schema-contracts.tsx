7015f839d086e2b0cd34e77b905a1b7f8699aeec
Laurin Quast <laurinquast@googlemail.com>
2024-01-25 16:33:46 +0100

    docs: documentation for schema contracts

--- packages/web/app/src/components/target/settings/schema-contracts.tsx
+++ packages/web/app/src/components/target/settings/schema-contracts.tsx
@@ -173,7 +173,7 @@ export function SchemaContracts() {
             graph.
           </CardDescription>
           <CardDescription>
-            <DocsLink href="/todo" className="text-gray-500 hover:text-gray-300">
+            <DocsLink href="/management/contracts" className="text-gray-500 hover:text-gray-300">
               Learn more about Schema Contracts
             </DocsLink>
           </CardDescription>
