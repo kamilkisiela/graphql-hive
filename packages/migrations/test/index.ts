1bb54ca69908010c8bea95c0b0fbf2f402199b95
Laurin Quast <laurinquast@googlemail.com>
2024-02-05 14:17:49 +0100

    we dont need this anymore

--- packages/migrations/test/index.ts
+++ packages/migrations/test/index.ts
@@ -1,5 +1,4 @@
 import './2023.02.22T09.27.02.delete-personal-org.test';
 import './2023.09.25T15.23.00.github-check-with-project-name.test';
-import './2023.11.02T14.41.41.schema-checks-dedup.test';
 import './2023.11.20T10-00-00.organization-member-roles.test';
 import './2024.01.26T00.00.01.schema-check-purging.test';
