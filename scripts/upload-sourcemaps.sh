39baef6b18e8ac33ea26fd74ad7d0b9f5d8687d9
Dotan Simha <dotansimha@gmail.com>
2024-02-20 16:21:16 +0200

    trying

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -3,11 +3,11 @@
 for dir in packages/services/*/dist; do
   name=$(echo $dir | awk -F / '{print $3}')
   echo $name
-  pnpm sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $dir --no-rewrite --ignore node_modules --no-sourcemap-reference --url-prefix /app/node_modules/\@hive/$name \;
+  pnpm sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $dir --no-rewrite --ignore node_modules --no-sourcemap-reference --url-prefix /usr/src/app/\@hive/$name \;
 done
 
 for dir in packages/web/*/dist; do
   name=$(echo $dir | awk -F / '{print $3}')
   echo $name
-  pnpm sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $dir --no-rewrite --ignore node_modules --no-sourcemap-reference --url-prefix /app/node_modules/\@hive/$name \;
+  pnpm sentry-cli releases files $SENTRY_RELEASE upload-sourcemaps $dir --no-rewrite --ignore node_modules --no-sourcemap-reference --url-prefix /usr/src/app/\@hive/$name \;
 done
