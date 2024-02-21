dd2dad7232a9d9262dbe3b1f09cce0f5174062d0
Dotan Simha <dotansimha@gmail.com>
2024-02-21 17:45:36 +0200

    fixes

--- scripts/upload-sourcemaps.sh
+++ scripts/upload-sourcemaps.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+pnpm sentry-cli releases new $SENTRY_RELEASE
+
 for dir in packages/services/*/dist; do
   name=$(echo $dir | awk -F / '{print $3}')
   echo $name
   pnpm sentry-cli sourcemaps inject $dir
-  pnpm sentry-cli releases new $SENTRY_RELEASE
   pnpm sentry-cli sourcemaps upload --release=$SENTRY_RELEASE $dir --dist $name --url-prefix /usr/src/app/\@hive/$name \;
-  pnpm sentry-cli releases set-commits --auto $SENTRY_RELEASE
-  pnpm sentry-cli releases finalize "$SENTRY_RELEASE"
 done
+
+pnpm sentry-cli releases set-commits --auto $SENTRY_RELEASE
+pnpm sentry-cli releases finalize "$SENTRY_RELEASE"
+
 
 # for dir in packages/web/*/dist; do
 #   name=$(echo $dir | awk -F / '{print $3}')
