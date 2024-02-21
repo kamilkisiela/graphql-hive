950e7c93bf824a6067cf9d70aa16529590344bfc
Dotan Simha <dotansimha@gmail.com>
2024-02-21 16:50:55 +0200

    fix banner

--- scripts/banner.js
+++
@@ -1,4 +0,0 @@
-// Adds missing require function (reason: node_modules are not transpiled)
-import { createRequire as _createRequire } from 'module';
-
-const require = _createRequire(import.meta.url);
