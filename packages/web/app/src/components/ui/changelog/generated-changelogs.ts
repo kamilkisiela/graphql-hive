0c9448bf71dab55752e1035ad80ad0fe73a2fba7
TuvalSimha <tuval.simha@gmail.com>
2024-03-20 17:13:54 +0200

    lint

--- packages/web/app/src/components/ui/changelog/generated-changelogs.ts
+++ packages/web/app/src/components/ui/changelog/generated-changelogs.ts
@@ -1,30 +1,33 @@
 export const latestChangelogs = [
   {
-    "date": "February 26, 2024",
-    "userReadItem": false,
-    "href": "https://the-guild.dev/graphql/hive/product-updates/2024-02-26-schema-check-top-affected-operations",
-    "title": "Top affected operations based on conditional breaking change configuration",
-    "description": "We now show you the top affected operations and clients based on the conditional breaking change configuration. This allows you to see the impact of your changes before you approve or reject them."
+    date: 'February 26, 2024',
+    userReadItem: false,
+    href: 'https://the-guild.dev/graphql/hive/product-updates/2024-02-26-schema-check-top-affected-operations',
+    title: 'Top affected operations based on conditional breaking change configuration',
+    description:
+      'We now show you the top affected operations and clients based on the conditional breaking change configuration. This allows you to see the impact of your changes before you approve or reject them.',
   },
   {
-    "date": "February 8, 2024",
-    "userReadItem": false,
-    "href": "https://the-guild.dev/graphql/hive/product-updates/2024-02-08-upcoming-stitching-project-changes",
-    "title": "Upcoming changes to schema composition for Schema Stitching projects",
-    "description": "Due to stitching limitations we need to slightly alter the schema composition behaviour for more predictable results."
+    date: 'February 8, 2024',
+    userReadItem: false,
+    href: 'https://the-guild.dev/graphql/hive/product-updates/2024-02-08-upcoming-stitching-project-changes',
+    title: 'Upcoming changes to schema composition for Schema Stitching projects',
+    description:
+      'Due to stitching limitations we need to slightly alter the schema composition behaviour for more predictable results.',
   },
   {
-    "date": "February 6, 2024",
-    "userReadItem": false,
-    "href": "https://the-guild.dev/graphql/hive/product-updates/2024-02-06-schema-contracts-for-federation",
-    "title": "Schema Contracts for Federation",
-    "description": "We added support schema contracts for Federation projects."
+    date: 'February 6, 2024',
+    userReadItem: false,
+    href: 'https://the-guild.dev/graphql/hive/product-updates/2024-02-06-schema-contracts-for-federation',
+    title: 'Schema Contracts for Federation',
+    description: 'We added support schema contracts for Federation projects.',
   },
   {
-    "date": "December 5, 2023",
-    "userReadItem": false,
-    "href": "https://the-guild.dev/graphql/hive/product-updates/2023-12-05-member-roles",
-    "title": "Introducing Member Roles",
-    "description": "Assign roles to members of your organization or create custom roles that fit your team perfectly"
-  }
+    date: 'December 5, 2023',
+    userReadItem: false,
+    href: 'https://the-guild.dev/graphql/hive/product-updates/2023-12-05-member-roles',
+    title: 'Introducing Member Roles',
+    description:
+      'Assign roles to members of your organization or create custom roles that fit your team perfectly',
+  },
 ];
