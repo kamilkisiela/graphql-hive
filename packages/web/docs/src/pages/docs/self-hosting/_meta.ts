d4e6a9c0b560461651a2a6cd1756be96982acba5
Tuval Simha <tuval.simha@Gmail.com>
2024-04-16 10:46:34 +0300

    Fix broken pages in docs website (#4531)

--- packages/web/docs/src/pages/docs/self-hosting/_meta.ts
+++ packages/web/docs/src/pages/docs/self-hosting/_meta.ts
@@ -2,7 +2,6 @@ export default {
   'get-started': 'Get Started',
   'oidc-login': 'SSO with OIDC',
   'client-and-cli-configuration': 'Client & CLI Configuration',
-  'federation-2': 'Apollo Federation 2',
   'cdn-artifacts': 'CDN Artifacts',
   's3-provider': 'S3 Storage Provider',
   telemetry: 'Telemetry / Error Reporting',
