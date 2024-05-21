1a96a9eab0bca3bd3ac2212684a51730984db86d
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-05-21 15:24:31 +0200

    eslint

--- rules/index.cjs
+++ rules/index.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   rules: {
     'enforce-deps-in-dev': require('./enforce-deps-in-dev.cjs'),
-    'graphql-require-selection': require('./graphql-require-selection.cjs'),
   },
 };
