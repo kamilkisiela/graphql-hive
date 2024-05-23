f37806e1c0adc4897e93b2c94eaf7e68a24a86a3
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-05-23 11:33:34 +0200

    mhm

--- packages/web/app/src/components/v2/icon.tsx
+++ packages/web/app/src/components/v2/icon.tsx
@@ -486,6 +486,7 @@ export const HiveLogo = ({ className }: IconProps): ReactElement => {
       className={clsx('inline fill-none', className)}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
+      transition={{ type: 'spring', stiffness: 400, damping: 17 }}
     >
       <motion.path
         fillRule="evenodd"
