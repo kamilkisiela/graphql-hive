e0e28450ae54fdb457b7867243ec26c76a1c9d5c
Eddy Nguyen <github@eddeee888.me>
2024-06-07 21:52:49 +1000

    Migrate Target

--- packages/services/api/src/modules/token/resolvers.ts
+++ packages/services/api/src/modules/token/resolvers.ts
@@ -1,16 +1,6 @@
 import { createConnection } from '../../shared/schema';
 import type { TokenModule } from './__generated__/types';
-import { TokenManager } from './providers/token-manager';
 
 export const resolvers: TokenModule.Resolvers = {
-  Target: {
-    tokens(target, _, { injector }) {
-      return injector.get(TokenManager).getTokens({
-        target: target.id,
-        project: target.projectId,
-        organization: target.orgId,
-      });
-    },
-  },
   TokenConnection: createConnection(),
 };
---
+++ packages/services/api/src/modules/token/resolvers/Target.ts
@@ -0,0 +1,12 @@
+import { TokenManager } from '../providers/token-manager';
+import type { TargetResolvers } from './../../../__generated__/types.next';
+
+export const Target: Pick<TargetResolvers, 'tokens'> = {
+  tokens(target, _, { injector }) {
+    return injector.get(TokenManager).getTokens({
+      target: target.id,
+      project: target.projectId,
+      organization: target.orgId,
+    });
+  },
+};
