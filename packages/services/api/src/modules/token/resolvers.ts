5acc01caea8a1a35e1713426eeb32d44652b584f
Eddy Nguyen <github@eddeee888.me>
2024-06-07 21:51:44 +1000

    Migrate Token

--- packages/services/api/src/modules/token/resolvers.ts
+++ packages/services/api/src/modules/token/resolvers.ts
@@ -3,14 +3,6 @@ import type { TokenModule } from './__generated__/types';
 import { TokenManager } from './providers/token-manager';
 
 export const resolvers: TokenModule.Resolvers = {
-  Token: {
-    id(token) {
-      return token.token;
-    },
-    alias(token) {
-      return token.tokenAlias;
-    },
-  },
   Target: {
     tokens(target, _, { injector }) {
       return injector.get(TokenManager).getTokens({
