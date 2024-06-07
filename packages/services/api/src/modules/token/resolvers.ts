8f2a1ed57d45697c9976a772cbbbc3339f740db6
Eddy Nguyen <github@eddeee888.me>
2024-06-07 21:54:49 +1000

    Migrate TokenConnection and completing toke module migration

--- packages/services/api/src/modules/token/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { createConnection } from '../../shared/schema';
-import type { TokenModule } from './__generated__/types';
-
-export const resolvers: TokenModule.Resolvers = {
-  TokenConnection: createConnection(),
-};
---
+++ packages/services/api/src/modules/token/resolvers/TokenConnection.ts
@@ -0,0 +1,4 @@
+import { createConnection } from '../../../shared/schema';
+import type { TokenConnectionResolvers } from './../../../__generated__/types.next';
+
+export const TokenConnection: TokenConnectionResolvers = createConnection();
