2714645a6a4510e53ae35713e91ea83fc86ef6d4
Eddy Nguyen <github@eddeee888.me>
2024-06-21 20:53:27 +1000

    Migrate AdminGeneralStats

--- packages/services/api/src/modules/admin/resolvers.ts
+++ packages/services/api/src/modules/admin/resolvers.ts
@@ -1,20 +1,7 @@
-import { parseDateRangeInput } from '../../shared/helpers';
 import { AdminModule } from './__generated__/types';
 import { AdminManager } from './providers/admin-manager';
 
 export const resolvers: AdminModule.Resolvers = {
-  AdminGeneralStats: {
-    operationsOverTime({ period, resolution }, _, { injector }) {
-      const dateRange = parseDateRangeInput(period);
-      return injector.get(AdminManager).getOperationsOverTime({
-        period: {
-          from: dateRange.from,
-          to: dateRange.to,
-        },
-        resolution,
-      });
-    },
-  },
   AdminOrganizationStats: {
     async operations(stats, _, { injector }) {
       const results = await injector.get(AdminManager).countOperationsPerOrganization({
---
+++ packages/services/api/src/modules/admin/resolvers/AdminGeneralStats.ts
@@ -0,0 +1,16 @@
+import { parseDateRangeInput } from '../../../shared/helpers';
+import { AdminManager } from '../providers/admin-manager';
+import type { AdminGeneralStatsResolvers } from './../../../__generated__/types.next';
+
+export const AdminGeneralStats: AdminGeneralStatsResolvers = {
+  operationsOverTime: async ({ period, resolution }, _, { injector }) => {
+    const dateRange = parseDateRangeInput(period);
+    return injector.get(AdminManager).getOperationsOverTime({
+      period: {
+        from: dateRange.from,
+        to: dateRange.to,
+      },
+      resolution,
+    });
+  },
+};
