3c53032966f2ea7e63f40d22f32e463a4e8fe082
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-06-23 18:09:33 +0200

    add tests

---
+++ packages/web/app/src/components/target/history/labelize.test.ts
@@ -0,0 +1,40 @@
+import { describe, expect, it } from 'vitest';
+import { labelize } from './labelize';
+
+describe('labelize', () => {
+  it('should work', () => {
+    expect(
+      labelize(
+        "Description for argument id on field 'User.email' changed from 'ID&apos;s of &quot;User&quot;' to 'ID&apos;s of &quot;Hello&quot;'",
+      ),
+    ).toMatchInlineSnapshot(`
+      [
+        "Description for argument id on field ",
+        <Label
+          dangerouslySetInnerHTML={
+            {
+              "__html": "User.email",
+            }
+          }
+        />,
+        " changed from ",
+        <Label
+          dangerouslySetInnerHTML={
+            {
+              "__html": "ID&apos;s of &quot;User&quot;",
+            }
+          }
+        />,
+        " to ",
+        <Label
+          dangerouslySetInnerHTML={
+            {
+              "__html": "ID&apos;s of &quot;Hello&quot;",
+            }
+          }
+        />,
+        "",
+      ]
+    `);
+  });
+});
