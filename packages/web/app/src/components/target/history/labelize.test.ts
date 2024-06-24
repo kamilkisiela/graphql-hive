ddad9bcba43812c696bb20acd79d7c36a16a6303
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-06-24 12:51:54 +0200

    should fix tests

--- packages/web/app/src/components/target/history/labelize.test.ts
+++ packages/web/app/src/components/target/history/labelize.test.ts
@@ -1,4 +1,3 @@
-import { describe, expect, it } from 'vitest';
 import { labelize } from './labelize';
 
 describe('labelize', () => {
--- packages/web/app/vite.config.ts
+++ packages/web/app/vite.config.ts
@@ -1,10 +1,13 @@
-import type { UserConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
-export default {
+export default defineConfig({
   root: __dirname,
   plugins: [tsconfigPaths(), react()],
-} satisfies UserConfig;
+  test: {
+    globals: true,
+  },
+});
