b8998e7ead84a2714d13678aaf1e349e648eb90a
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-06-27 16:00:30 +0200

    Shared HTTP client with retries (#5097)

---
+++ .changeset/good-hairs-deliver.md
@@ -0,0 +1,6 @@
+---
+'@graphql-hive/apollo': patch
+'@graphql-hive/yoga': patch
+---
+
+Use built-in retry of http client of the core package
--- packages/libraries/core/src/client/http-client.ts
+++ packages/libraries/core/src/client/http-client.ts
@@ -1,4 +1,10 @@
-import { fetch } from '@whatwg-node/fetch';
+import asyncRetry from 'async-retry';
+import { fetch, URL } from '@whatwg-node/fetch';
+
+type RetryOptions = Parameters<typeof asyncRetry>[1] & {
+  retryWhen(response: Response): boolean;
+  okWhen?(response: Response): boolean;
+};
 
 function get(
   endpoint: string,
@@ -6,12 +12,14 @@ function get(
     headers: Record<string, string>;
     timeout?: number;
     fetchImplementation?: typeof fetch;
+    retry?: RetryOptions;
   },
 ) {
   return makeFetchCall(endpoint, {
     method: 'GET',
     headers: config.headers,
     timeout: config.timeout,
+    retry: config.retry,
     fetchImplementation: config.fetchImplementation,
   });
 }
@@ -22,6 +30,7 @@ function post(
   config: {
     headers: Record<string, string>;
     timeout?: number;
+    retry?: RetryOptions;
     fetchImplementation?: typeof fetch;
   },
 ) {
@@ -30,6 +39,7 @@ function post(
     method: 'POST',
     headers: config.headers,
     timeout: config.timeout,
+    retry: config.retry,
     fetchImplementation: config.fetchImplementation,
   });
 }
@@ -46,24 +56,68 @@ async function makeFetchCall(
     method: 'GET' | 'POST';
     headers: Record<string, string>;
     timeout?: number;
+    retry?: RetryOptions;
     fetchImplementation?: typeof fetch;
   },
 ) {
   const controller = new AbortController();
   let timeoutId: ReturnType<typeof setTimeout> | undefined = undefined;
-  const responsePromise = (config.fetchImplementation ?? fetch)(endpoint, {
-    method: config.method,
-    body: config.body,
-    headers: config.headers,
-    signal: controller.signal,
-  });
 
   if (config.timeout) {
     timeoutId = setTimeout(() => controller.abort(), config.timeout);
   }
 
   try {
-    return await responsePromise;
+    const retryOptions = config.retry;
+    if (!retryOptions) {
+      return await (config.fetchImplementation ?? fetch)(endpoint, {
+        method: config.method,
+        body: config.body,
+        headers: config.headers,
+        signal: controller.signal,
+      });
+    }
+
+    const result = await asyncRetry(
+      async bail => {
+        const res = await (config.fetchImplementation ?? fetch)(endpoint, {
+          method: config.method,
+          body: config.body,
+          headers: config.headers,
+          signal: controller.signal,
+        });
+
+        if (res.ok || retryOptions.okWhen?.(res)) {
+          return res;
+        }
+
+        if (!retryOptions.retryWhen(res)) {
+          bail(
+            new Error(
+              `Failed to fetch ${endpoint}, received: ${res.status} ${res.statusText ?? 'Internal Server Error'}`,
+            ),
+          );
+          return;
+        }
+
+        throw new Error(
+          `Failed to fetch ${endpoint}, received: ${res.status} ${res.statusText ?? 'Internal Server Error'}`,
+        );
+      },
+      {
+        ...retryOptions,
+        retries: retryOptions?.retries ?? 5,
+        minTimeout: retryOptions?.minTimeout ?? 200,
+        maxTimeout: retryOptions?.maxTimeout ?? 2000,
+        factor: retryOptions?.factor ?? 1.2,
+      },
+    );
+
+    if (result === undefined) {
+      throw new Error('Failed to bail out of retry.');
+    }
+
+    return result;
   } catch (error) {
     if (isAggregateError(error)) {
       throw new Error(error.errors.map(e => e.message).join(', '), {
@@ -85,3 +139,5 @@ interface AggregateError extends Error {
 function isAggregateError(error: unknown): error is AggregateError {
   return !!error && typeof error === 'object' && 'errors' in error && Array.isArray(error.errors);
 }
+
+export { URL };
