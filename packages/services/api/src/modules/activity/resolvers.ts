d24b759cdeab1e53f896836eb3e4239d11e42d28
Eddy Nguyen <github@eddeee888.me>
2024-07-05 21:13:13 +1000

    Finish migrating activity module

--- packages/services/api/src/modules/activity/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { createConnection } from '../../shared/schema';
-import { ActivityModule } from './__generated__/types';
-
-export const resolvers: ActivityModule.Resolvers = {
-  ActivityConnection: createConnection(),
-};
