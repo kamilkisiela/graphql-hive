176a930a3768ec5f6ed4c6cbe87c501d65cd3d4f
Eddy Nguyen <github@eddeee888.me>
2024-07-06 20:59:30 +1000

    Migrate ProjectAccessScope

--- packages/services/api/src/modules/auth/module.graphql.mappers.ts
+++ packages/services/api/src/modules/auth/module.graphql.mappers.ts
@@ -1,7 +1,9 @@
 import type { Member, User } from '../../shared/entities';
 import type { OrganizationAccessScope } from './providers/organization-access';
+import type { ProjectAccessScope } from './providers/project-access';
 
 export type OrganizationAccessScopeMapper = OrganizationAccessScope;
+export type ProjectAccessScopeMapper = ProjectAccessScope;
 export type UserConnectionMapper = readonly User[];
 export type MemberConnectionMapper = readonly Member[];
 export type MemberMapper = Member;
--- packages/services/api/src/modules/auth/resolvers.ts
+++ packages/services/api/src/modules/auth/resolvers.ts
@@ -1,25 +1,13 @@
 import { createConnection } from '../../shared/schema';
 import { AuthModule } from './__generated__/types';
 import { AuthManager } from './providers/auth-manager';
-import { ProjectAccessScope } from './providers/project-access';
 import { TargetAccessScope } from './providers/target-access';
 
 export const resolvers: AuthModule.Resolvers & {
-  ProjectAccessScope: {
-    [K in AuthModule.ProjectAccessScope]: ProjectAccessScope;
-  };
   TargetAccessScope: {
     [K in AuthModule.TargetAccessScope]: TargetAccessScope;
   };
 } = {
-  ProjectAccessScope: {
-    READ: ProjectAccessScope.READ,
-    DELETE: ProjectAccessScope.DELETE,
-    ALERTS: ProjectAccessScope.ALERTS,
-    SETTINGS: ProjectAccessScope.SETTINGS,
-    OPERATIONS_STORE_READ: ProjectAccessScope.OPERATIONS_STORE_READ,
-    OPERATIONS_STORE_WRITE: ProjectAccessScope.OPERATIONS_STORE_WRITE,
-  },
   TargetAccessScope: {
     READ: TargetAccessScope.READ,
     REGISTRY_READ: TargetAccessScope.REGISTRY_READ,
