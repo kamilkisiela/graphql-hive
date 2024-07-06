65aa2706d26f5adb3ec33b9146166c0b4c96f965
Eddy Nguyen <github@eddeee888.me>
2024-07-06 21:06:05 +1000

    Migrate UserConnection

--- packages/services/api/src/modules/auth/resolvers.ts
+++ packages/services/api/src/modules/auth/resolvers.ts
@@ -2,6 +2,5 @@ import { createConnection } from '../../shared/schema';
 import { AuthModule } from './__generated__/types';
 
 export const resolvers: AuthModule.Resolvers = {
-  UserConnection: createConnection(),
   MemberConnection: createConnection(),
 };
