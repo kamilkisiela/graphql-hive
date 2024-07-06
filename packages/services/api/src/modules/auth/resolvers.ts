e440305329287cbaa99cbb5db79062b458db9903
Eddy Nguyen <github@eddeee888.me>
2024-07-06 21:04:39 +1000

    Migrate auth.Member

--- packages/services/api/src/modules/auth/resolvers.ts
+++ packages/services/api/src/modules/auth/resolvers.ts
@@ -1,28 +1,7 @@
 import { createConnection } from '../../shared/schema';
 import { AuthModule } from './__generated__/types';
-import { AuthManager } from './providers/auth-manager';
 
 export const resolvers: AuthModule.Resolvers = {
-  Member: {
-    organizationAccessScopes(member, _, { injector }) {
-      return injector.get(AuthManager).getMemberOrganizationScopes({
-        user: member.user.id,
-        organization: member.organization,
-      });
-    },
-    projectAccessScopes(member, _, { injector }) {
-      return injector.get(AuthManager).getMemberProjectScopes({
-        user: member.user.id,
-        organization: member.organization,
-      });
-    },
-    targetAccessScopes(member, _, { injector }) {
-      return injector.get(AuthManager).getMemberTargetScopes({
-        user: member.user.id,
-        organization: member.organization,
-      });
-    },
-  },
   UserConnection: createConnection(),
   MemberConnection: createConnection(),
 };
