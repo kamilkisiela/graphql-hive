b494426dae62ce40f305af460f1f54a22416abb7
Eddy Nguyen <github@eddeee888.me>
2024-07-06 21:02:32 +1000

    Migrate TargetAccessScope

--- packages/services/api/src/modules/auth/resolvers.ts
+++ packages/services/api/src/modules/auth/resolvers.ts
@@ -1,22 +1,8 @@
 import { createConnection } from '../../shared/schema';
 import { AuthModule } from './__generated__/types';
 import { AuthManager } from './providers/auth-manager';
-import { TargetAccessScope } from './providers/target-access';
 
-export const resolvers: AuthModule.Resolvers & {
-  TargetAccessScope: {
-    [K in AuthModule.TargetAccessScope]: TargetAccessScope;
-  };
-} = {
-  TargetAccessScope: {
-    READ: TargetAccessScope.READ,
-    REGISTRY_READ: TargetAccessScope.REGISTRY_READ,
-    REGISTRY_WRITE: TargetAccessScope.REGISTRY_WRITE,
-    DELETE: TargetAccessScope.DELETE,
-    SETTINGS: TargetAccessScope.SETTINGS,
-    TOKENS_READ: TargetAccessScope.TOKENS_READ,
-    TOKENS_WRITE: TargetAccessScope.TOKENS_WRITE,
-  },
+export const resolvers: AuthModule.Resolvers = {
   Member: {
     organizationAccessScopes(member, _, { injector }) {
       return injector.get(AuthManager).getMemberOrganizationScopes({
