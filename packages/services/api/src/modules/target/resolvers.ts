b3fdbc1fda73d9bbd6bc2bb95833a6be1ec777c6
Eddy Nguyen <github@eddeee888.me>
2024-07-09 20:07:59 +1000

    Migrate Query.Project

--- packages/services/api/src/modules/target/resolvers.ts
+++ packages/services/api/src/modules/target/resolvers.ts
@@ -1,15 +1,6 @@
 import { createConnection } from '../../shared/schema';
 import type { TargetModule } from './__generated__/types';
-import { TargetManager } from './providers/target-manager';
 
 export const resolvers: TargetModule.Resolvers = {
-  Project: {
-    targets(project, _, { injector }) {
-      return injector.get(TargetManager).getTargets({
-        project: project.id,
-        organization: project.orgId,
-      });
-    },
-  },
   TargetConnection: createConnection(),
 };
