de315903ac4b78da1a86405aa78dc1a9d1ab818b
Eddy Nguyen <github@eddeee888.me>
2024-07-09 20:04:24 +1000

    Migrate Mutation.updateTargetValidationSettings

--- packages/services/api/src/modules/target/resolvers.ts
+++ packages/services/api/src/modules/target/resolvers.ts
@@ -1,66 +1,11 @@
 import { z } from 'zod';
 import { createConnection } from '../../shared/schema';
-import { OrganizationManager } from '../organization/providers/organization-manager';
 import { IdTranslator } from '../shared/providers/id-translator';
 import type { TargetModule } from './__generated__/types';
 import { TargetManager } from './providers/target-manager';
 
-const PercentageModel = z.number().min(0).max(100);
-
 export const resolvers: TargetModule.Resolvers = {
   Mutation: {
-    async updateTargetValidationSettings(_, { input }, { injector }) {
-      const translator = injector.get(IdTranslator);
-      const [organization, project, target] = await Promise.all([
-        translator.translateOrganizationId(input),
-        translator.translateProjectId(input),
-        translator.translateTargetId(input),
-      ]);
-
-      const org = await injector.get(OrganizationManager).getOrganization({ organization });
-
-      const UpdateTargetValidationSettingsModel = z.object({
-        percentage: PercentageModel,
-        period: z.number().min(1).max(org.monthlyRateLimit.retentionInDays).int(),
-        targets: z.array(z.string()).min(1),
-        excludedClients: z.optional(z.array(z.string())),
-      });
-
-      const result = UpdateTargetValidationSettingsModel.safeParse(input);
-
-      if (!result.success) {
-        return {
-          error: {
-            message: 'Please check your input.',
-            inputErrors: {
-              percentage: result.error.formErrors.fieldErrors.percentage?.[0],
-              period: result.error.formErrors.fieldErrors.period?.[0],
-            },
-          },
-        };
-      }
-
-      const targetManager = injector.get(TargetManager);
-      await targetManager.updateTargetValidationSettings({
-        period: input.period,
-        percentage: input.percentage,
-        target,
-        project,
-        organization,
-        targets: result.data.targets,
-        excludedClients: result.data.excludedClients ?? [],
-      });
-
-      return {
-        ok: {
-          target: await targetManager.getTarget({
-            organization,
-            project,
-            target,
-          }),
-        },
-      };
-    },
     async updateTargetGraphQLEndpointUrl(_, { input }, { injector }) {
       const translator = injector.get(IdTranslator);
       const [organizationId, projectId, targetId] = await Promise.all([
