c8acd4c3f9718e2e4dd54bcd75826e688bef4dc4
Eddy Nguyen <github@eddeee888.me>
2024-07-09 20:06:42 +1000

    Migrate Mutation.experimental__updateTargetSchemaComposition

--- packages/services/api/src/modules/target/resolvers.ts
+++ packages/services/api/src/modules/target/resolvers.ts
@@ -1,26 +1,8 @@
 import { createConnection } from '../../shared/schema';
-import { IdTranslator } from '../shared/providers/id-translator';
 import type { TargetModule } from './__generated__/types';
 import { TargetManager } from './providers/target-manager';
 
 export const resolvers: TargetModule.Resolvers = {
-  Mutation: {
-    async experimental__updateTargetSchemaComposition(_, { input }, { injector }) {
-      const translator = injector.get(IdTranslator);
-      const [organizationId, projectId, targetId] = await Promise.all([
-        translator.translateOrganizationId(input),
-        translator.translateProjectId(input),
-        translator.translateTargetId(input),
-      ]);
-
-      return injector.get(TargetManager).updateTargetSchemaComposition({
-        organizationId,
-        projectId,
-        targetId,
-        nativeComposition: input.nativeComposition,
-      });
-    },
-  },
   Project: {
     targets(project, _, { injector }) {
       return injector.get(TargetManager).getTargets({
