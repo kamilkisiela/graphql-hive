909eb752bcd4fddfaa4bf3f5ad1c55c15c6ffbe7
Eddy Nguyen <github@eddeee888.me>
2024-07-09 20:09:55 +1000

    Migrate TargetConnection

--- packages/services/api/src/modules/target/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { createConnection } from '../../shared/schema';
-import type { TargetModule } from './__generated__/types';
-
-export const resolvers: TargetModule.Resolvers = {
-  TargetConnection: createConnection(),
-};
