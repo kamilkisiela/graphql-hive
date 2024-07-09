8c9c7a1b233f3a574d772016e67d384634832944
Eddy Nguyen <github@eddeee888.me>
2024-07-09 20:05:32 +1000

    Migrate Mutation.updateTargetGraphQLEndpointUrl

--- packages/services/api/src/modules/target/resolvers.ts
+++ packages/services/api/src/modules/target/resolvers.ts
@@ -1,4 +1,3 @@
-import { z } from 'zod';
 import { createConnection } from '../../shared/schema';
 import { IdTranslator } from '../shared/providers/id-translator';
 import type { TargetModule } from './__generated__/types';
@@ -6,35 +5,6 @@ import { TargetManager } from './providers/target-manager';
 
 export const resolvers: TargetModule.Resolvers = {
   Mutation: {
-    async updateTargetGraphQLEndpointUrl(_, { input }, { injector }) {
-      const translator = injector.get(IdTranslator);
-      const [organizationId, projectId, targetId] = await Promise.all([
-        translator.translateOrganizationId(input),
-        translator.translateProjectId(input),
-        translator.translateTargetId(input),
-      ]);
-
-      const result = await injector.get(TargetManager).updateTargetGraphQLEndpointUrl({
-        organizationId,
-        projectId,
-        targetId,
-        graphqlEndpointUrl: input.graphqlEndpointUrl ?? null,
-      });
-
-      if (result.type === 'error') {
-        return {
-          error: {
-            message: result.reason,
-          },
-        };
-      }
-
-      return {
-        ok: {
-          target: result.target,
-        },
-      };
-    },
     async experimental__updateTargetSchemaComposition(_, { input }, { injector }) {
       const translator = injector.get(IdTranslator);
       const [organizationId, projectId, targetId] = await Promise.all([
