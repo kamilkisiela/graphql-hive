c6e886cbcd6bd6d11e38113e62ef690b9ac87c93
Eddy Nguyen <github@eddeee888.me>
2024-07-13 21:53:07 +1000

    Migrate oidc-integrations.Organization

--- packages/services/api/src/modules/oidc-integrations/resolvers.ts
+++ packages/services/api/src/modules/oidc-integrations/resolvers.ts
@@ -2,22 +2,6 @@ import { OidcIntegrationsModule } from './__generated__/types';
 import { OIDCIntegrationsProvider } from './providers/oidc-integrations.provider';
 
 export const resolvers: OidcIntegrationsModule.Resolvers = {
-  Organization: {
-    viewerCanManageOIDCIntegration: (organization, _, { injector }) => {
-      return injector
-        .get(OIDCIntegrationsProvider)
-        .canViewerManageIntegrationForOrganization(organization.id);
-    },
-    oidcIntegration: async (organization, _, { injector }) => {
-      if (injector.get(OIDCIntegrationsProvider).isEnabled() === false) {
-        return null;
-      }
-
-      return await injector
-        .get(OIDCIntegrationsProvider)
-        .getOIDCIntegrationForOrganization({ organizationId: organization.id });
-    },
-  },
   OIDCIntegration: {
     id: oidcIntegration => oidcIntegration.id,
     tokenEndpoint: oidcIntegration => oidcIntegration.tokenEndpoint,
