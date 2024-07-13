2d549c870d68922cb8810e4b58463ecc92afb69a
Eddy Nguyen <github@eddeee888.me>
2024-07-13 21:54:08 +1000

    Migrate OIDCIntegration

--- packages/services/api/src/modules/oidc-integrations/resolvers.ts
+++ packages/services/api/src/modules/oidc-integrations/resolvers.ts
@@ -1,16 +1,6 @@
 import { OidcIntegrationsModule } from './__generated__/types';
-import { OIDCIntegrationsProvider } from './providers/oidc-integrations.provider';
 
 export const resolvers: OidcIntegrationsModule.Resolvers = {
-  OIDCIntegration: {
-    id: oidcIntegration => oidcIntegration.id,
-    tokenEndpoint: oidcIntegration => oidcIntegration.tokenEndpoint,
-    userinfoEndpoint: oidcIntegration => oidcIntegration.userinfoEndpoint,
-    authorizationEndpoint: oidcIntegration => oidcIntegration.authorizationEndpoint,
-    clientId: oidcIntegration => oidcIntegration.clientId,
-    clientSecretPreview: (oidcIntegration, _, { injector }) =>
-      injector.get(OIDCIntegrationsProvider).getClientSecretPreview(oidcIntegration),
-  },
   User: {
     canSwitchOrganization: user => !user.oidcIntegrationId,
   },
