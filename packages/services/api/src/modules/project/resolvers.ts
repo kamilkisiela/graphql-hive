d55b98fa8aafdc04546f5cddc03a68c76da5ea4e
Eddy Nguyen <github@eddeee888.me>
2024-07-15 13:54:14 +1000

    Migrate project.Project

--- packages/services/api/src/modules/project/resolvers.ts
+++ packages/services/api/src/modules/project/resolvers.ts
@@ -1,25 +1,6 @@
-import { ProjectType } from '../../shared/entities';
 import { createConnection } from '../../shared/schema';
-import { OrganizationManager } from '../organization/providers/organization-manager';
 import type { ProjectModule } from './__generated__/types';
 
 export const resolvers: ProjectModule.Resolvers = {
-  Project: {
-    async experimental_nativeCompositionPerTarget(project, _, { injector }) {
-      if (project.type !== ProjectType.FEDERATION) {
-        return false;
-      }
-
-      if (!project.nativeFederation) {
-        return false;
-      }
-
-      const organization = await injector.get(OrganizationManager).getOrganization({
-        organization: project.orgId,
-      });
-
-      return organization.featureFlags.forceLegacyCompositionInTargets.length > 0;
-    },
-  },
   ProjectConnection: createConnection(),
 };
