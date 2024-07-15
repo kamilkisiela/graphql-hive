aebfe7cf27342f201513b3cb7e57d7c390766cc9
Eddy Nguyen <github@eddeee888.me>
2024-07-15 13:56:16 +1000

    Migrate ProjectConnection

--- packages/services/api/src/modules/project/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { createConnection } from '../../shared/schema';
-import type { ProjectModule } from './__generated__/types';
-
-export const resolvers: ProjectModule.Resolvers = {
-  ProjectConnection: createConnection(),
-};
