0b2758140fbe0037bc70933a35e82f4da6c47722
Eddy Nguyen <github@eddeee888.me>
2024-07-15 22:36:27 +1000

    Migrate AlertWebhookChannel

--- packages/services/api/src/modules/alerts/resolvers.ts
+++ packages/services/api/src/modules/alerts/resolvers.ts
@@ -1,14 +1,6 @@
 import type { AlertsModule } from './__generated__/types';
 
 export const resolvers: AlertsModule.Resolvers = {
-  AlertWebhookChannel: {
-    __isTypeOf(channel) {
-      return channel.type === 'WEBHOOK';
-    },
-    endpoint(channel) {
-      return channel.webhookEndpoint!;
-    },
-  },
   TeamsWebhookChannel: {
     __isTypeOf(channel) {
       return channel.type === 'MSTEAMS_WEBHOOK';
