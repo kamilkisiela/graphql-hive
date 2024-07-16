f1b39d67cd4a3838f94fa6927c877ba43f8bbd9c
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-07-16 22:48:36 +0200

    chore: Add PowerShell script for installing Hive CLI on Windows

---
+++ packages/web/docs/public/install.ps1
@@ -0,0 +1,71 @@
+#!/usr/bin/env pwsh
+
+function Install-Binary($install_args) {
+  $old_erroractionpreference = $ErrorActionPreference
+  $ErrorActionPreference = 'stop'
+
+  Initialize-Environment
+
+  # If the HIVE_CLI_VERSION env var is set, we use it instead of the latest version
+  $version = if (Test-Path env:HIVE_CLI_VERSION) {
+    $Env:HIVE_CLI_VERSION
+  } else {
+    'latest'
+  }
+
+  $exe = Download($version)
+  Invoke-Installer "$exe" "$install_args"
+
+  $ErrorActionPreference = $old_erroractionpreference
+}
+
+function Download($version) {
+  $base_url = if ($version -eq 'latest') {
+    "https://cli.graphql-hive.com/channels/stable/hive-"
+  } else {
+    "https://cli.graphql-hive.com/versions/$version/hive-v$version-"
+  }
+
+  # Detect if the system is x86 or x64
+  $arch = if ([Environment]::Is64BitOperatingSystem) {
+    "x64"
+  } else {
+    "x86"
+  }
+
+  # append the arch to the url
+  $url = "$base_url$arch.exe"
+
+  "Downloading Hive CLI from $url" | Out-Host
+  $tmp = New-Temp-Dir
+  $dir_path = "$tmp\hive.exe"
+  $wc = New-Object Net.Webclient
+  $wc.downloadFile($url, $dir_path)
+  "Downloaded Hive CLI to $dir_path" | Out-Host
+  return "$dir_path"
+}
+
+function Invoke-Installer($exe, $install_args) {
+  & "$exe" "install" "$install_args"
+  Remove-Item "$exe" -Recurse -Force
+}
+
+function Initialize-Environment() {
+  # show notification to change execution policy:
+  $allowedExecutionPolicy = @('Unrestricted', 'RemoteSigned', 'ByPass')
+  If ((Get-ExecutionPolicy).ToString() -notin $allowedExecutionPolicy) {
+    Write-Error "PowerShell requires an execution policy in [$($allowedExecutionPolicy -join ", ")] to run Hive CLI."
+    Write-Error "For example, to set the execution policy to 'RemoteSigned' please run :"
+    Write-Error "'Set-ExecutionPolicy RemoteSigned -scope CurrentUser'"
+    break
+  }
+}
+
+function New-Temp-Dir() {
+  $parent = [System.IO.Path]::GetTempPath()
+  [string] $name = [System.Guid]::NewGuid()
+  $dir = New-Item -ItemType Directory -Path (Join-Path $parent $name)
+  return $dir.FullName
+}
+
+Install-Binary "$Args"
