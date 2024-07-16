24becac14b84ffac142f4b10281fe8b3811bda4c
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-07-16 16:09:41 +0200

    lol eslint, you are soooo unuseful

--- packages/web/app/src/components/ui/separator.tsx
+++ packages/web/app/src/components/ui/separator.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-restricted-imports */
 import * as React from 'react';
 import * as SeparatorPrimitive from '@radix-ui/react-separator';
 import { cn } from '@/lib/utils';
