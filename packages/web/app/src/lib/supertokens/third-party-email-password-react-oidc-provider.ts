f12b98b10816ca33369e057b41e0623c038ca565
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-07-19 22:09:46 +0200

    Hint recent authentication method

--- packages/web/app/src/lib/supertokens/third-party-email-password-react-oidc-provider.ts
+++ packages/web/app/src/lib/supertokens/third-party-email-password-react-oidc-provider.ts
@@ -1,6 +1,7 @@
 import { UserInput } from 'supertokens-auth-react/lib/build/recipe/thirdpartyemailpassword/types';
 import { getAuthorisationURLWithQueryParamsAndSetState } from 'supertokens-auth-react/recipe/thirdpartyemailpassword';
 import { env } from '@/env/frontend';
+import { updateLastAuthMethod } from './last-auth-method';
 
 export const createThirdPartyEmailPasswordReactOIDCProvider = () => ({
   id: 'oidc',
@@ -78,6 +79,8 @@ export const startAuthFlowForOIDCProvider = async (oidcId: string) => {
       oidcId,
     },
   });
+
+  updateLastAuthMethod('oidc');
 
   // Redirects to the OIDC provider
   window.location.assign(authUrl);
--- packages/web/app/src/pages/auth-sign-in.tsx
+++ packages/web/app/src/pages/auth-sign-in.tsx
@@ -23,13 +23,47 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Meta } from '@/components/ui/meta';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { useToast } from '@/components/ui/use-toast';
+import { useLastAuthMethod } from '@/lib/supertokens/last-auth-method';
 import { startAuthFlowForProvider } from '@/lib/supertokens/start-auth-flow-for-provider';
 import { enabledProviders, isProviderEnabled } from '@/lib/supertokens/thirdparty';
-import { exhaustiveGuard } from '@/lib/utils';
+import { cn, exhaustiveGuard } from '@/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Slot } from '@radix-ui/react-slot';
 import { useMutation } from '@tanstack/react-query';
 import { Link, Navigate, useRouter } from '@tanstack/react-router';
+
+export function SignInButton(props: {
+  children: React.ReactNode;
+  previousSignIn: boolean;
+  variant?: 'outline' | 'default';
+  tooltipClassName?: string;
+}) {
+  if (props.previousSignIn) {
+    return (
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Slot
+            className={cn(
+              'animate-shimmer bg-[length:200%_100%] transition-colors',
+              props.variant === 'outline'
+                ? `bg-[linear-gradient(110deg,transparent,48%,#202020,52%,transparent)]`
+                : `bg-[linear-gradient(110deg,transparent,30%,#a9a9a9,70%,transparent)]`,
+            )}
+          >
+            {props.children}
+          </Slot>
+        </TooltipTrigger>
+        <TooltipContent className={cn('text-muted bg-white', props.tooltipClassName)} side="top">
+          You signed in with it last time.
+        </TooltipContent>
+      </Tooltip>
+    );
+  }
+
+  return <>{props.children}</>;
+}
 
 const SignInFormSchema = z.object({
   email: z
@@ -44,6 +78,7 @@ type SignInFormValues = z.infer<typeof SignInFormSchema>;
 
 export function AuthSignInPage(props: { redirectToPath: string }) {
   const session = useSessionContext();
+  const [lastAuthMethod, setLastAuthMethod] = useLastAuthMethod();
   const router = useRouter();
   const { toast } = useToast();
   const emailPasswordSignIn = useMutation({
@@ -53,6 +88,7 @@ export function AuthSignInPage(props: { redirectToPath: string }) {
 
       switch (status) {
         case 'OK': {
+          setLastAuthMethod('email');
           void router.navigate({
             to: props.redirectToPath,
           });
@@ -157,99 +193,112 @@ export function AuthSignInPage(props: { redirectToPath: string }) {
         <AuthCardHeader title="Login" description="Sign in to your account" />
         <AuthCardContent>
           <AuthCardStack>
-            <Form {...form}>
-              <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input placeholder="m@example.com" type="email" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <div className="flex items-center">
-                        <FormLabel>Password</FormLabel>
-                        <Link
-                          tabIndex={-1}
-                          to="/auth/reset-password"
-                          search={{
-                            email: form.getValues().email || undefined,
-                            redirectToPath: props.redirectToPath,
-                          }}
-                          className="ml-auto inline-block text-sm underline"
-                        >
-                          Forgot your password?
-                        </Link>
-                      </div>
-                      <FormControl>
-                        <Input type="password" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit" className="w-full" disabled={isPending}>
-                  {emailPasswordSignIn.data?.status === 'OK'
-                    ? 'Redirecting...'
-                    : emailPasswordSignIn.isPending
-                      ? 'Signing in...'
-                      : 'Sign in'}
-                </Button>
-              </form>
-            </Form>
-            {enabledProviders.length ? <AuthOrSeparator /> : null}
-            {isProviderEnabled('google') ? (
-              <Button
-                variant="outline"
-                className="w-full"
-                onClick={() => thirdPartySignIn.mutate('google')}
-                disabled={isPending}
-              >
-                <SiGoogle className="mr-4 size-4" /> Login with Google
-              </Button>
-            ) : null}
-            {isProviderEnabled('github') ? (
-              <Button
-                variant="outline"
-                className="w-full"
-                onClick={() => thirdPartySignIn.mutate('github')}
-                disabled={isPending}
-              >
-                <SiGithub className="mr-4 size-4" /> Login with Github
-              </Button>
-            ) : null}
-            {isProviderEnabled('okta') ? (
-              <Button
-                variant="outline"
-                className="w-full"
-                onClick={() => thirdPartySignIn.mutate('okta')}
-                disabled={isPending}
-              >
-                <SiOkta className="mr-4 size-4" /> Login with Okta
-              </Button>
-            ) : null}
-            {isProviderEnabled('oidc') ? (
-              <Button asChild variant="outline" className="w-full" disabled={isPending}>
-                <Link
-                  to="/auth/sso"
-                  search={{
-                    redirectToPath: props.redirectToPath,
-                  }}
-                >
-                  <FaRegUserCircle className="mr-4 size-4" /> Login with SSO
-                </Link>
-              </Button>
-            ) : null}
+            <TooltipProvider delayDuration={200}>
+              <Form {...form}>
+                <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="m@example.com" type="email" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className="flex items-center">
+                          <FormLabel>Password</FormLabel>
+                          <Link
+                            tabIndex={-1}
+                            to="/auth/reset-password"
+                            search={{
+                              email: form.getValues().email || undefined,
+                              redirectToPath: props.redirectToPath,
+                            }}
+                            className="ml-auto inline-block text-sm underline"
+                          >
+                            Forgot your password?
+                          </Link>
+                        </div>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <SignInButton previousSignIn={lastAuthMethod === 'email'}>
+                    <Button type="submit" className="w-full" disabled={isPending}>
+                      {emailPasswordSignIn.data?.status === 'OK'
+                        ? 'Redirecting...'
+                        : emailPasswordSignIn.isPending
+                          ? 'Signing in...'
+                          : 'Sign in'}
+                    </Button>
+                  </SignInButton>
+                </form>
+              </Form>
+              {enabledProviders.length ? <AuthOrSeparator /> : null}
+              {isProviderEnabled('google') ? (
+                <SignInButton variant="outline" previousSignIn={lastAuthMethod === 'google'}>
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => thirdPartySignIn.mutate('google')}
+                    disabled={isPending}
+                  >
+                    <SiGoogle className="mr-4 size-4" /> Login with Google
+                  </Button>
+                </SignInButton>
+              ) : null}
+              {isProviderEnabled('github') ? (
+                <SignInButton variant="outline" previousSignIn={lastAuthMethod === 'github'}>
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => thirdPartySignIn.mutate('github')}
+                    disabled={isPending}
+                  >
+                    <SiGithub className="mr-4 size-4" /> Login with Github
+                  </Button>
+                </SignInButton>
+              ) : null}
+
+              {isProviderEnabled('okta') ? (
+                <SignInButton variant="outline" previousSignIn={lastAuthMethod === 'okta'}>
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => thirdPartySignIn.mutate('okta')}
+                    disabled={isPending}
+                  >
+                    <SiOkta className="mr-4 size-4" /> Login with Okta
+                  </Button>
+                </SignInButton>
+              ) : null}
+              {isProviderEnabled('oidc') ? (
+                <SignInButton variant="outline" previousSignIn={lastAuthMethod === 'oidc'}>
+                  <Button asChild variant="outline" className="w-full" disabled={isPending}>
+                    <Link
+                      to="/auth/sso"
+                      search={{
+                        redirectToPath: props.redirectToPath,
+                      }}
+                    >
+                      <FaRegUserCircle className="mr-4 size-4" /> Login with SSO
+                    </Link>
+                  </Button>
+                </SignInButton>
+              ) : null}
+            </TooltipProvider>
           </AuthCardStack>
           <div className="mt-4 text-center text-sm">
             Don't have an account?{' '}
