ecdf2a13a4662efa66b0ac01e85c6947652970cd
Tuval Simha <tuval.simha@Gmail.com>
2024-07-19 17:05:41 +0300

    Drop modals from V2: Create Access Token Modal (#5244)

--- packages/web/app/src/components/v2/modals/index.ts
+++ packages/web/app/src/components/v2/modals/index.ts
@@ -1,6 +1,5 @@
 export { ChangePermissionsModal } from './change-permissions';
 export { ConnectSchemaModal } from './connect-schema';
-export { CreateAccessTokenModal } from './create-access-token';
 export { DeleteOrganizationModal } from './delete-organization';
 export { DeleteProjectModal } from './delete-project';
 export { DeleteTargetModal } from './delete-target';
