9108db1027817d4ac40862a85305e224beb9feee
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-07-22 19:32:21 +0200

    export buttonVariants

--- packages/web/app/src/components/ui/button.tsx
+++ packages/web/app/src/components/ui/button.tsx
@@ -22,7 +22,7 @@ export const SIZE = {
   'icon-sm': 'size-7',
 };
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
   {
     variants: {
