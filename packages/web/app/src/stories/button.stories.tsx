1ca06e0c413ff2a9f2c328a8a8385ef8890c59b7
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-07-22 19:19:43 +0200

    fix

--- packages/web/app/src/stories/button.stories.tsx
+++ packages/web/app/src/stories/button.stories.tsx
@@ -1,6 +1,19 @@
 import { ArrowBigDown, ArrowDown } from 'lucide-react';
-import { Button, ButtonProps, buttonVariants } from '@/components/ui/button';
+import { Button, SIZE, VARIANT } from '@/components/ui/button';
 import type { Meta, StoryObj } from '@storybook/react';
+
+const VARIANTS = [
+  'default',
+  'orangeLink',
+  'link',
+  'ghost',
+  'secondary',
+  'outline',
+  'destructive',
+  'primary',
+] as const satisfies (keyof typeof VARIANT)[];
+
+const SIZES = ['default', 'sm', 'lg', 'icon', 'icon-sm'] as const satisfies (keyof typeof SIZE)[];
 
 const meta: Meta<typeof Button> = {
   title: 'Components/Button',
@@ -8,141 +21,97 @@ const meta: Meta<typeof Button> = {
   argTypes: {
     variant: {
       control: { type: 'select' },
-      options: Object.keys(
-        buttonVariants({
-          variant: 'default',
-          size: 'default',
-          className: 'className',
-        }),
-      ),
+      options: VARIANTS,
     },
     size: {
       control: { type: 'select' },
-      options: Object.keys(
-        buttonVariants({
-          variant: 'default',
-          size: 'default',
-          className: 'className',
-        }),
-      ),
+      options: SIZES,
     },
     asChild: {
       control: { type: 'boolean' },
     },
   },
+  args: {
+    variant: 'default',
+    size: 'default',
+  }
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-const Template = (args: ButtonProps) => <Button {...args} />;
-
 export const Default: Story = {
-  name: 'Default',
-  render: Template,
   args: {
-    variant: 'default',
-    size: 'default',
     children: 'Default Button',
   },
 };
 
 export const Destructive: Story = {
-  name: 'Destructive',
-  render: Template,
   args: {
     variant: 'destructive',
-    size: 'default',
     children: 'Destructive Button',
   },
 };
 
 export const Outline: Story = {
-  name: 'Outline',
-  render: Template,
   args: {
     variant: 'outline',
-    size: 'default',
     children: 'Outline Button',
   },
 };
 
 export const Secondary: Story = {
-  name: 'Secondary',
-  render: Template,
   args: {
     variant: 'secondary',
-    size: 'default',
     children: 'Secondary Button',
   },
 };
 
 export const Ghost: Story = {
-  name: 'Ghost',
-  render: Template,
   args: {
     variant: 'ghost',
-    size: 'default',
     children: 'Ghost Button',
   },
 };
 
 export const Link: Story = {
-  name: 'Link',
-  render: Template,
   args: {
     variant: 'link',
-    size: 'default',
     children: 'Link Button',
   },
 };
 
 export const OrangeLink: Story = {
-  name: 'OrangeLink',
-  render: Template,
   args: {
     variant: 'orangeLink',
-    size: 'default',
     children: 'Orange Link Button',
   },
 };
 
 export const Small: Story = {
-  name: 'Small',
-  render: Template,
   args: {
-    variant: 'default',
     size: 'sm',
     children: 'Small Button',
   },
 };
 
 export const Large: Story = {
-  name: 'Large',
-  render: Template,
   args: {
-    variant: 'default',
     size: 'lg',
     children: 'Large Button',
   },
 };
 
 export const Icon: Story = {
-  name: 'Icon',
-  render: Template,
   args: {
-    variant: 'default',
     size: 'icon',
     children: <ArrowBigDown />,
   },
 };
 
 export const IconSmall: Story = {
-  name: 'IconSmall',
-  render: Template,
   args: {
-    variant: 'default',
     size: 'icon-sm',
     children: <ArrowDown />,
   },
