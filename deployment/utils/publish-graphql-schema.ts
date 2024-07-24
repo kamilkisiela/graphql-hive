bb0a49d9cc4e6f24dafc8056b67e4e446aa361d6
Laurin Quast <laurinquast@googlemail.com>
2024-07-24 11:55:45 +0200

    ?

--- deployment/utils/publish-graphql-schema.ts
+++ deployment/utils/publish-graphql-schema.ts
@@ -12,13 +12,13 @@ export function publishGraphQLSchema(args: {
   const command =
     `schema:publish` +
     ` --registry.endpoint ${args.registry.endpoint} --registry.accessToken ${args.registry.accessToken}` +
-    ` --commit ${args.version.commit} --author "Hive CD" /schema.graphqls`;
+    ` --commit ${args.version.commit} --author "Hive CD" ./schema.graphqls`;
 
   return new local.Command(
     'publish-graphql-schema',
     {
       create:
-        `docker run --name "publish-graphql-schema" -v ./schema.graphqls:/schema.graphqls ghcr.io/kamilkisiela/graphql-hive/cli:0.36.0 ` +
+        `docker run --name "publish-graphql-schema" -v ./schema.graphqls:/usr/src/app/schema.graphqls ghcr.io/kamilkisiela/graphql-hive/cli:0.36.0 ` +
         command,
     },
     {
