8630ee021c7222d312302f7332d980c395f0f730
Eddy Nguyen <github@eddeee888.me>
2024-07-24 22:38:12 +1000

    Migrate Mutation.schemaCheck

--- packages/services/api/src/modules/schema/resolvers.ts
+++ packages/services/api/src/modules/schema/resolvers.ts
@@ -141,35 +141,6 @@ function __isTypeOf<
 
 export const resolvers: SchemaModule.Resolvers = {
   Mutation: {
-    async schemaCheck(_, { input }, { injector }) {
-      const [organization, project, target] = await Promise.all([
-        injector.get(OrganizationManager).getOrganizationIdByToken(),
-        injector.get(ProjectManager).getProjectIdByToken(),
-        injector.get(TargetManager).getTargetIdByToken(),
-      ]);
-
-      const result = await injector.get(SchemaPublisher).check({
-        ...input,
-        service: input.service?.toLowerCase(),
-        organization,
-        project,
-        target,
-      });
-
-      if ('changes' in result && result.changes) {
-        return {
-          ...result,
-          changes: result.changes,
-          errors:
-            result.errors?.map(error => ({
-              ...error,
-              path: 'path' in error ? error.path?.split('.') : null,
-            })) ?? [],
-        };
-      }
-
-      return result;
-    },
     async approveFailedSchemaCheck(_, { input }, { injector }) {
       const [organizationId, projectId, targetId] = await Promise.all([
         injector.get(IdTranslator).translateOrganizationId(input),
