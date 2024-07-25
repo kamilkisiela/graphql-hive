d3969980711299485b10af20d3c3ab90fef047db
Eddy Nguyen <github@eddeee888.me>
2024-07-25 22:41:53 +1000

    Migrate SchemaVersion

--- packages/services/api/src/modules/schema/resolvers.ts
+++ packages/services/api/src/modules/schema/resolvers.ts
@@ -34,20 +34,17 @@ import {
 import { CriticalityLevel } from '@graphql-inspector/core';
 import type * as Types from '../../__generated__/types';
 import { type DateRange } from '../../shared/entities';
-import { createPeriod, parseDateRangeInput, PromiseOrValue } from '../../shared/helpers';
-import { buildASTSchema, createConnection, createDummyConnection } from '../../shared/schema';
+import { parseDateRangeInput, PromiseOrValue } from '../../shared/helpers';
+import { createConnection, createDummyConnection } from '../../shared/schema';
 import { OperationsManager } from '../operations/providers/operations-manager';
 import { TargetSelector } from '../shared/providers/storage';
 import { TargetManager } from '../target/providers/target-manager';
 import type { SchemaModule } from './__generated__/types';
-import { onlyDeprecatedDocumentNode } from './lib/deprecated-graphql';
-import { extractSuperGraphInformation, SuperGraphInformation } from './lib/federation-super-graph';
-import { stripUsedSchemaCoordinatesFromDocumentNode } from './lib/unused-graphql';
+import { SuperGraphInformation } from './lib/federation-super-graph';
 import { BreakingSchemaChangeUsageHelper } from './providers/breaking-schema-changes-helper';
 import { ContractsManager } from './providers/contracts-manager';
 import { SchemaCheckManager } from './providers/schema-check-manager';
 import { SchemaManager } from './providers/schema-manager';
-import { SchemaVersionHelper } from './providers/schema-version-helper';
 
 function isSchemaCoordinateUsageForUnusedExplorer(
   value: unknown,
@@ -128,180 +125,6 @@ function __isTypeOf<
 }
 
 export const resolvers: SchemaModule.Resolvers = {
-  SchemaVersion: {
-    isComposable(version) {
-      return version.schemaCompositionErrors === null;
-    },
-    hasSchemaChanges(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getHasSchemaChanges(version);
-    },
-    async log(version, _, { injector }) {
-      const log = await injector.get(SchemaManager).getSchemaLog({
-        commit: version.actionId,
-        organization: version.organization,
-        project: version.project,
-        target: version.target,
-      });
-
-      if (log.kind === 'single') {
-        return {
-          __typename: 'PushedSchemaLog',
-          author: log.author,
-          commit: log.commit,
-          date: log.date as any,
-          id: log.id,
-          service: null,
-          serviceSdl: null,
-        };
-      }
-
-      if (log.action === 'DELETE') {
-        return {
-          __typename: 'DeletedSchemaLog',
-          author: 'system',
-          commit: 'system',
-          date: log.date as any,
-          id: log.id,
-          deletedService: log.service_name,
-          previousServiceSdl: await injector
-            .get(SchemaVersionHelper)
-            .getServiceSdlForPreviousVersionService(version, log.service_name),
-        };
-      }
-
-      return {
-        __typename: 'PushedSchemaLog',
-        author: log.author,
-        commit: log.commit,
-        date: log.date as any,
-        id: log.id,
-        service: log.service_name,
-        serviceSdl: log.sdl,
-        previousServiceSdl: await injector
-          .get(SchemaVersionHelper)
-          .getServiceSdlForPreviousVersionService(version, log.service_name),
-      };
-    },
-    schemas(version, _, { injector }) {
-      return injector.get(SchemaManager).getMaybeSchemasOfVersion({
-        version: version.id,
-        organization: version.organization,
-        project: version.project,
-        target: version.target,
-      });
-    },
-    async schemaCompositionErrors(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getSchemaCompositionErrors(version);
-    },
-    async breakingSchemaChanges(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getBreakingSchemaChanges(version);
-    },
-    async safeSchemaChanges(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getSafeSchemaChanges(version);
-    },
-    async supergraph(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getSupergraphSdl(version);
-    },
-    async sdl(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getCompositeSchemaSdl(version);
-    },
-    async baseSchema(version) {
-      return version.baseSchema ?? null;
-    },
-    async explorer(version, { usage }, { injector }) {
-      const [schemaAst, supergraphAst] = await Promise.all([
-        injector.get(SchemaVersionHelper).getCompositeSchemaAst(version),
-        injector.get(SchemaVersionHelper).getSupergraphAst(version),
-      ]);
-
-      if (!schemaAst) {
-        return null;
-      }
-
-      const supergraph = supergraphAst ? extractSuperGraphInformation(supergraphAst) : null;
-
-      return {
-        schema: buildASTSchema(schemaAst),
-        usage: {
-          period: usage?.period ? parseDateRangeInput(usage.period) : createPeriod('30d'),
-          organization: version.organization,
-          project: version.project,
-          target: version.target,
-        },
-        supergraph,
-      };
-    },
-    async unusedSchema(version, { usage }, { injector }) {
-      const [schemaAst, supergraphAst] = await Promise.all([
-        injector.get(SchemaVersionHelper).getCompositeSchemaAst(version),
-        injector.get(SchemaVersionHelper).getSupergraphAst(version),
-      ]);
-
-      if (!schemaAst) {
-        return null;
-      }
-
-      const usedCoordinates = await injector.get(OperationsManager).getReportedSchemaCoordinates({
-        targetId: version.target,
-        projectId: version.project,
-        organizationId: version.organization,
-        period: usage?.period ? parseDateRangeInput(usage.period) : createPeriod('30d'),
-      });
-
-      const supergraph = supergraphAst ? extractSuperGraphInformation(supergraphAst) : null;
-
-      return {
-        sdl: stripUsedSchemaCoordinatesFromDocumentNode(schemaAst, usedCoordinates),
-        usage: {
-          period: usage?.period ? parseDateRangeInput(usage.period) : createPeriod('30d'),
-          organization: version.organization,
-          project: version.project,
-          target: version.target,
-          usedCoordinates,
-        },
-        supergraph,
-      };
-    },
-    async deprecatedSchema(version, { usage }, { injector }) {
-      const [schemaAst, supergraphAst] = await Promise.all([
-        injector.get(SchemaVersionHelper).getCompositeSchemaAst(version),
-        injector.get(SchemaVersionHelper).getSupergraphAst(version),
-      ]);
-
-      if (!schemaAst) {
-        return null;
-      }
-
-      const supergraph = supergraphAst ? extractSuperGraphInformation(supergraphAst) : null;
-
-      return {
-        sdl: onlyDeprecatedDocumentNode(schemaAst),
-        usage: {
-          period: usage?.period ? parseDateRangeInput(usage.period) : createPeriod('30d'),
-          organization: version.organization,
-          project: version.project,
-          target: version.target,
-        },
-        supergraph,
-      };
-    },
-    date: version => version.createdAt,
-    githubMetadata(version, _, { injector }) {
-      return injector.get(SchemaManager).getGitHubMetadata(version);
-    },
-    valid(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getIsValid(version);
-    },
-    previousDiffableSchemaVersion(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getPreviousDiffableSchemaVersion(version);
-    },
-    isFirstComposableVersion(version, _, { injector }) {
-      return injector.get(SchemaVersionHelper).getIsFirstComposableVersion(version);
-    },
-    contractVersions(version, _, { injector }) {
-      return injector.get(ContractsManager).getContractVersionsForSchemaVersion(version);
-    },
-  },
   SingleSchema: {
     __isTypeOf(obj) {
       return obj.kind === 'single';
