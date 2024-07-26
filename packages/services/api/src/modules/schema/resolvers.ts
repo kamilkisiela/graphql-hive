37d0486c566a4314b473e68f108284fcd39157f1
Eddy Nguyen <github@eddeee888.me>
2024-07-26 22:12:41 +1000

    Migrate GraphQLInputObjectType

--- packages/services/api/src/modules/schema/resolvers.ts
+++ packages/services/api/src/modules/schema/resolvers.ts
@@ -93,31 +93,6 @@ function __isTypeOf<
 }
 
 export const resolvers: SchemaModule.Resolvers = {
-  GraphQLInputObjectType: {
-    __isTypeOf: __isTypeOf(Kind.INPUT_OBJECT_TYPE_DEFINITION),
-    name: t => t.entity.name,
-    description: t => t.entity.description ?? null,
-    fields: t =>
-      t.entity.fields.map(f => ({
-        entity: f,
-        parent: {
-          coordinate: t.entity.name,
-        },
-        usage: t.usage,
-        supergraph: t.supergraph
-          ? {
-              ownedByServiceNames: t.supergraph.getInputFieldOwnedByServices(f.name),
-            }
-          : null,
-      })),
-    usage,
-    supergraphMetadata: t =>
-      t.supergraph
-        ? {
-            ownedByServiceNames: t.supergraph.ownedByServiceNames,
-          }
-        : null,
-  },
   GraphQLScalarType: {
     __isTypeOf: __isTypeOf(Kind.SCALAR_TYPE_DEFINITION),
     name: t => t.entity.name,
