be466d36ae9d6b03115ded45a1b047c77255a3a4
Eddy Nguyen <github@eddeee888.me>
2024-07-26 21:19:06 +1000

    Migrate SchemaCoordinateUsage

--- packages/services/api/src/modules/schema/resolvers.ts
+++ packages/services/api/src/modules/schema/resolvers.ts
@@ -122,36 +122,6 @@ function __isTypeOf<
 }
 
 export const resolvers: SchemaModule.Resolvers = {
-  SchemaCoordinateUsage: {
-    topOperations(source, { limit }, { injector }) {
-      if (!source.isUsed) {
-        return [];
-      }
-
-      return injector
-        .get(OperationsManager)
-        .getTopOperationForCoordinate({
-          organizationId: source.organization,
-          projectId: source.project,
-          targetId: source.target,
-          coordinate: source.coordinate,
-          period: source.period,
-          limit,
-        })
-        .then(operations =>
-          operations.map(op => ({
-            name: op.operationName,
-            hash: op.operationHash,
-            count: op.count,
-          })),
-        );
-    },
-    // Why? GraphQL-JIT goes crazy without this (Expected Iterable, but did not find one for field SchemaCoordinateUsage.usedByClients).
-    // That's why we switched from a getter to a function.
-    usedByClients(parent) {
-      return parent.usedByClients();
-    },
-  },
   SchemaExplorer: {
     async type(source, { name }, { injector }) {
       const entity = source.schema.getType(name);
