c4eabe1e2523706b9082761d8e5de2875cca8a47
Eddy Nguyen <github@eddeee888.me>
2024-07-26 22:08:49 +1000

    Migrate GraphQLInterfaceType

--- packages/services/api/src/modules/schema/resolvers.ts
+++ packages/services/api/src/modules/schema/resolvers.ts
@@ -93,30 +93,6 @@ function __isTypeOf<
 }
 
 export const resolvers: SchemaModule.Resolvers = {
-  GraphQLInterfaceType: {
-    __isTypeOf: __isTypeOf(Kind.INTERFACE_TYPE_DEFINITION),
-    name: t => t.entity.name,
-    description: t => t.entity.description ?? null,
-    fields: t =>
-      t.entity.fields.map(f => ({
-        entity: f,
-        parent: {
-          coordinate: t.entity.name,
-        },
-        usage: t.usage,
-        supergraph: t.supergraph
-          ? { ownedByServiceNames: t.supergraph.getFieldOwnedByServices(f.name) }
-          : null,
-      })),
-    interfaces: t => t.entity.interfaces,
-    usage,
-    supergraphMetadata: t =>
-      t.supergraph
-        ? {
-            ownedByServiceNames: t.supergraph.ownedByServiceNames,
-          }
-        : null,
-  },
   GraphQLUnionType: {
     __isTypeOf: __isTypeOf(Kind.UNION_TYPE_DEFINITION),
     name: t => t.entity.name,
