1a1f3a3e5212bc9422ba6871ad0170d3f561bd23
Eddy Nguyen <github@eddeee888.me>
2024-07-31 16:31:45 +1000

    Migrate Mutation.updateOrganizationSlug

---
+++ packages/services/api/src/modules/organization/resolvers/Mutation/updateOrganizationSlug.ts
@@ -0,0 +1,46 @@
+import { IdTranslator } from '../../../shared/providers/id-translator';
+import { OrganizationManager } from '../../providers/organization-manager';
+import { OrganizationSlugModel } from '../../validation';
+import type { MutationResolvers } from './../../../../__generated__/types.next';
+
+export const updateOrganizationSlug: NonNullable<
+  MutationResolvers['updateOrganizationSlug']
+> = async (_, { input }, { injector }) => {
+  const parsedInput = OrganizationSlugModel.safeParse(input.slug.trim());
+
+  if (!parsedInput.success) {
+    return {
+      error: {
+        message:
+          parsedInput.error.formErrors.fieldErrors?.[0]?.[0] ??
+          'Changing the organization slug failed.',
+      },
+    };
+  }
+
+  const organizationId = await injector.get(IdTranslator).translateOrganizationId(input);
+  const result = await injector.get(OrganizationManager).updateSlug({
+    slug: input.slug,
+    organization: organizationId,
+  });
+
+  if (result.ok) {
+    return {
+      ok: {
+        updatedOrganizationPayload: {
+          selector: {
+            organization: result.organization.cleanId,
+          },
+          organization: result.organization,
+        },
+      },
+    };
+  }
+
+  return {
+    ok: null,
+    error: {
+      message: result.message,
+    },
+  };
+};
--- packages/services/api/src/modules/organization/validation.ts
+++ packages/services/api/src/modules/organization/validation.ts
@@ -1,3 +1,11 @@
+import { z } from 'zod';
 import { NameModel } from '../../shared/entities';
 
 export const OrganizationNameModel = NameModel.min(2).max(50);
+export const OrganizationSlugModel = z
+  .string({
+    required_error: 'Organization slug is required',
+  })
+  .min(1, 'Organization slug is required')
+  .max(50, 'Slug must be less than 50 characters')
+  .regex(/^[a-z0-9-]+$/, 'Slug can only contain lowercase letters, numbers and dashes');
