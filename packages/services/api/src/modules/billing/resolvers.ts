c5c9e1a9060a1e5a0a9893ddeabcf4b6b7dc197b
Eddy Nguyen <github@eddeee888.me>
2024-07-31 17:20:21 +1000

    Migrate billing.Organization

--- packages/services/api/src/modules/billing/resolvers.ts
+++ packages/services/api/src/modules/billing/resolvers.ts
@@ -1,6 +1,5 @@
 import { GraphQLError } from 'graphql';
 import { TRPCClientError } from '@trpc/client';
-import { BillingPlanType } from '../../__generated__/types';
 import { AuthManager } from '../auth/providers/auth-manager';
 import { OrganizationAccessScope } from '../auth/providers/organization-access';
 import { OrganizationManager } from '../organization/providers/organization-manager';
@@ -27,91 +26,6 @@ const USAGE_DEFAULT_LIMITATIONS: Record<
 };
 
 export const resolvers: BillingModule.Resolvers = {
-  Organization: {
-    plan: org => (org.billingPlan || 'HOBBY') as BillingPlanType,
-    billingConfiguration: async (org, _args, { injector }) => {
-      if (org.billingPlan === 'ENTERPRISE') {
-        return {
-          hasActiveSubscription: true,
-          canUpdateSubscription: false,
-          hasPaymentIssues: false,
-          paymentMethod: null,
-          billingAddress: null,
-          invoices: null,
-          upcomingInvoice: null,
-        };
-      }
-
-      const billingRecord = await injector
-        .get(BillingProvider)
-        .getOrganizationBillingParticipant({ organization: org.id });
-
-      if (!billingRecord) {
-        return {
-          hasActiveSubscription: false,
-          canUpdateSubscription: true,
-          hasPaymentIssues: false,
-          paymentMethod: null,
-          billingAddress: null,
-          invoices: null,
-          upcomingInvoice: null,
-        };
-      }
-
-      // This is a special case where customer is on Pro and doesn't have a record for external billing.
-      // This happens when the customer is paying through an external system and not through Stripe.
-      if (org.billingPlan === 'PRO' && billingRecord.externalBillingReference === 'wire') {
-        return {
-          hasActiveSubscription: true,
-          canUpdateSubscription: false,
-          hasPaymentIssues: false,
-          paymentMethod: null,
-          billingAddress: null,
-          invoices: null,
-          upcomingInvoice: null,
-        };
-      }
-
-      const subscriptionInfo = await injector.get(BillingProvider).getActiveSubscription({
-        organizationId: billingRecord.organizationId,
-      });
-
-      if (!subscriptionInfo) {
-        return {
-          hasActiveSubscription: false,
-          canUpdateSubscription: true,
-          hasPaymentIssues: false,
-          paymentMethod: null,
-          billingAddress: null,
-          invoices: null,
-          upcomingInvoice: null,
-        };
-      }
-
-      const [invoices, upcomingInvoice] = await Promise.all([
-        injector.get(BillingProvider).invoices({
-          organizationId: billingRecord.organizationId,
-        }),
-        injector.get(BillingProvider).upcomingInvoice({
-          organizationId: billingRecord.organizationId,
-        }),
-      ]);
-
-      const hasPaymentIssues = invoices?.some(
-        i => i.charge !== null && typeof i.charge === 'object' && i.charge?.failure_code !== null,
-      );
-
-      return {
-        hasActiveSubscription: subscriptionInfo.subscription !== null,
-        canUpdateSubscription: subscriptionInfo.subscription !== null,
-        hasPaymentIssues,
-        paymentMethod: subscriptionInfo.paymentMethod?.card || null,
-        billingAddress: subscriptionInfo.paymentMethod?.billing_details || null,
-        invoices,
-        upcomingInvoice,
-      };
-    },
-  },
   BillingPaymentMethod: {
     brand: bpm => bpm.brand,
     last4: bpm => bpm.last4,
