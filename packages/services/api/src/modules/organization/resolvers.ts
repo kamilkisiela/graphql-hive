9cde3d61af72fc155b1bbb8091b5c9d31a5c0509
Eddy Nguyen <github@eddeee888.me>
2024-07-31 17:09:42 +1000

    Migrate OrganizationConnection

--- packages/services/api/src/modules/organization/resolvers.ts
+++
@@ -1,6 +0,0 @@
-import { createConnection } from '../../shared/schema';
-import type { OrganizationModule } from './__generated__/types';
-
-export const resolvers: OrganizationModule.Resolvers = {
-  OrganizationConnection: createConnection(),
-};
