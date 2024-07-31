3b4fe14ef0c2298a9ecaff1ad81bbc10393f0180
Eddy Nguyen <github@eddeee888.me>
2024-07-31 17:25:26 +1000

    Migrate Query.billingPlans

--- packages/services/api/src/modules/billing/resolvers.ts
+++ packages/services/api/src/modules/billing/resolvers.ts
@@ -26,51 +26,6 @@ const USAGE_DEFAULT_LIMITATIONS: Record<
 };
 
 export const resolvers: BillingModule.Resolvers = {
-  Query: {
-    billingPlans: async (root, args, { injector }) => {
-      const availablePrices = await injector.get(BillingProvider).getAvailablePrices();
-
-      if (!availablePrices) {
-        return [];
-      }
-
-      return [
-        {
-          id: 'HOBBY',
-          planType: 'HOBBY',
-          basePrice: 0,
-          name: 'Hobby',
-          description: 'Free for non-commercial use, startups, side-projects and just experiments.',
-          includedOperationsLimit: USAGE_DEFAULT_LIMITATIONS.HOBBY.operations,
-          rateLimit: 'MONTHLY_LIMITED',
-          pricePerOperationsUnit: 0,
-          retentionInDays: USAGE_DEFAULT_LIMITATIONS.HOBBY.retention,
-        },
-        {
-          id: 'PRO',
-          planType: 'PRO',
-          basePrice: availablePrices.basePrice.unit_amount! / 100,
-          name: 'Pro',
-          description:
-            'For production-ready applications that requires long retention, high ingestion capacity.',
-          includedOperationsLimit: USAGE_DEFAULT_LIMITATIONS.PRO.operations,
-          pricePerOperationsUnit: availablePrices.operationsPrice.tiers![1].unit_amount! / 100,
-          retentionInDays: USAGE_DEFAULT_LIMITATIONS.PRO.retention,
-          rateLimit: 'MONTHLY_QUOTA',
-        },
-        {
-          id: 'ENTERPRISE',
-          planType: 'ENTERPRISE',
-          name: 'Enterprise',
-          description:
-            'For enterprise and organization that requires custom setup and custom data ingestion rates.',
-          includedOperationsLimit: USAGE_DEFAULT_LIMITATIONS.ENTERPRISE.operations,
-          retentionInDays: USAGE_DEFAULT_LIMITATIONS.ENTERPRISE.retention,
-          rateLimit: 'UNLIMITED',
-        },
-      ];
-    },
-  },
   Mutation: {
     generateStripePortalLink: async (_, args, { injector }) => {
       const organizationId = await injector.get(IdTranslator).translateOrganizationId({
