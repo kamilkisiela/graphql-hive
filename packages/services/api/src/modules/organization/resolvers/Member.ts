b208c930c0c7728cfb56d21c95646bfa5816d052
Eddy Nguyen <github@eddeee888.me>
2024-07-31 17:03:56 +1000

    Migrate Member

---
+++ packages/services/api/src/modules/organization/resolvers/Member.ts
@@ -0,0 +1,16 @@
+import { OrganizationManager } from '../providers/organization-manager';
+import type { MemberResolvers } from './../../../__generated__/types.next';
+
+export const Member: Pick<MemberResolvers, 'canLeaveOrganization' | 'isAdmin' | 'role'> = {
+  canLeaveOrganization: async (member, _, { injector }) => {
+    const { result } = await injector.get(OrganizationManager).canLeaveOrganization({
+      organizationId: member.organization,
+      userId: member.user.id,
+    });
+
+    return result;
+  },
+  isAdmin: (member, _, { injector }) => {
+    return member.isOwner || injector.get(OrganizationManager).isAdminRole(member.role);
+  },
+};
