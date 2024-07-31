d83e021aeefad60923fb14e72bb023368ee80e6e
Eddy Nguyen <github@eddeee888.me>
2024-07-31 16:48:05 +1000

    Migrate Mutation.assignMemberRole

---
+++ packages/services/api/src/modules/organization/resolvers/Mutation/assignMemberRole.ts
@@ -0,0 +1,17 @@
+import { IdTranslator } from '../../../shared/providers/id-translator';
+import { OrganizationManager } from '../../providers/organization-manager';
+import type { MutationResolvers } from './../../../../__generated__/types.next';
+
+export const assignMemberRole: NonNullable<MutationResolvers['assignMemberRole']> = async (
+  _,
+  { input },
+  { injector },
+) => {
+  const organizationId = await injector.get(IdTranslator).translateOrganizationId(input);
+
+  return injector.get(OrganizationManager).assignMemberRole({
+    organizationId,
+    userId: input.user,
+    roleId: input.role,
+  });
+};
