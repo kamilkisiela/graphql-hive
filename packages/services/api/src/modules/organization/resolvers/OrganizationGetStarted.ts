b3c7a016df07a73adfbe46cc7aa33edd4997c00c
Eddy Nguyen <github@eddeee888.me>
2024-07-31 17:10:14 +1000

    Migrate OrganizationGetStarted

---
+++ packages/services/api/src/modules/organization/resolvers/OrganizationGetStarted.ts
@@ -0,0 +1,10 @@
+import type { OrganizationGetStartedResolvers } from './../../../__generated__/types.next';
+
+export const OrganizationGetStarted: Pick<
+  OrganizationGetStartedResolvers,
+  | 'checkingSchema'
+  | 'creatingProject'
+  | 'enablingUsageBasedBreakingChanges'
+  | 'invitingMembers'
+  | 'publishingSchema'
+> = {};
