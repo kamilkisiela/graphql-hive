c028a94ed6bf69753b181be6bb1a5cf7d149a731
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-06 15:40:55 +0200

    Configure VSCode Tailwind extension to work in cva and cn args (#5311)

--- packages/web/app/src/components/ui/command.tsx
+++ packages/web/app/src/components/ui/command.tsx
@@ -14,7 +14,7 @@ const Command = React.forwardRef<
   <CommandPrimitive
     ref={ref}
     className={cn(
-      'bg-popover text-popover-foreground flex h-full w-full flex-col overflow-hidden rounded-md',
+      'bg-popover text-popover-foreground flex size-full flex-col overflow-hidden rounded-md',
       className,
     )}
     {...props}
