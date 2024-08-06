2998223e776f931ae8132624d23ec28e2aaeae7b
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-06 13:49:37 +0200

    Implement all button variants

--- packages/web/docs/src/components/call-to-action.tsx
+++ packages/web/docs/src/components/call-to-action.tsx
@@ -2,13 +2,24 @@ import { ComponentPropsWithoutRef } from 'react';
 import { Anchor } from '@theguild/components';
 import { cn } from '../lib';
 
+// todo: use cva later?
+const variantStyles = {
+  primary: cn(
+    'bg-primary hover:bg-green-800 hover:text-white' +
+      ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800',
+  ),
+  'primary-inverted': cn(''),
+};
+
+type CallToActionVariant = keyof typeof variantStyles;
+
 interface CallToActionProps extends ComponentPropsWithoutRef<typeof Anchor> {
   href: string;
   variant: 'primary' | 'primary-inverted' | 'secondary';
 }
 
 /**
- * This is `Button` in the new design system.
+ * This is `Button` in the new Hive brand design system.
  * TODO: Move it to the components library?
  */
 export function CallToAction({ className, variant, ...rest }: CallToActionProps) {
@@ -18,15 +29,7 @@ export function CallToAction({ className, variant, ...rest }: CallToActionProps)
         'inline-flex w-fit flex-row items-center gap-2',
         'text-green-1000 rounded-lg border border-green-800 bg-green-300 px-6 py-3 font-medium leading-6',
         'focus-visible:ring-0 focus-visible:ring-offset-0',
-        // todo: use cva later
-        variant === 'primary'
-          ? 'bg-primary hover:bg-green-800 hover:text-white' +
-              ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800'
-          : variant === 'primary-inverted'
-            ? 'bg-primary hover:bg-white' +
-              ' focus-visible:outline-4 focus-visible:outline-offset-0 focus-visible:outline-white/40'
-            : 'bg-green-300 hover:bg-green-200' +
-              ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-300/40',
+        variantStyles[variant],
         className,
       )}
       {...rest}
