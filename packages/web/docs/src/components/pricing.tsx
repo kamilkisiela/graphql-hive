90fe9dc1ce960e98c8c4ff6b63e58c4817b1abf0
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-12 19:00:02 +0200

    Pricing & Counters (#5322)

--- packages/web/docs/src/components/pricing.tsx
+++ packages/web/docs/src/components/pricing.tsx
@@ -1,86 +1,76 @@
 import { ReactElement, ReactNode } from 'react';
-import Link from 'next/link';
-import clsx from 'clsx';
 import { Arrow, Content, Root, Trigger } from '@radix-ui/react-tooltip';
-import { ArrowIcon } from './arrow-icon';
-
-const linkClass = clsx(
-  'flex flex-row items-center justify-between',
-  'w-full cursor-pointer whitespace-nowrap',
-  'transition-colors duration-150',
-  'text-sm text-white font-medium',
-  'rounded-md py-2 px-3',
-  'bg-gray-300/10 hover:bg-gray-300/20',
-);
+import { CallToAction } from './call-to-action';
+import { Heading } from './heading';
 
 function Tooltip({ content, children }: { content: string; children: ReactNode }) {
   return (
     <Root delayDuration={0}>
-      <Trigger>{children}</Trigger>
-      <Content
-        sideOffset={5}
-        className="rounded-sm bg-white p-2 text-xs font-normal text-black shadow"
-      >
+      <Trigger className="text-left">{children}</Trigger>
+      <Content sideOffset={5} className="bg-green-1000 rounded p-2 font-normal text-white shadow">
         {content}
-        <Arrow className="fill-current text-white" />
+        <Arrow className="fill-green-1000" />
       </Content>
     </Root>
   );
 }
 
-function Plan(plan: {
+const PlanFeaturesSeparator = Symbol('PlanFeaturesSeparator');
+type PlanFeaturesSeparator = typeof PlanFeaturesSeparator;
+
+function Plan(props: {
   name: string;
   description: string;
   price: ReactNode | string;
-  features: (ReactNode | string)[];
+  features: (ReactNode | string | PlanFeaturesSeparator)[];
   linkText: string;
   linkOnClick?: () => void;
-  footer?: ReactNode;
+  adjustable: boolean;
 }): ReactElement {
   return (
-    <div className="flex w-full flex-col items-start rounded-md border border-gray-700 p-4 hover:border-gray-600 md:w-1/3">
-      <div className="flex size-full flex-col justify-between">
-        <div>
-          <h2 className="flex items-center justify-between text-base font-bold text-white">
-            {plan.name}
-          </h2>
-          <div className="text-3xl font-bold text-white">{plan.price}</div>
-          <div className="mt-3 text-sm text-gray-500">{plan.description}</div>
-          <div className="pt-6">
-            {plan.linkOnClick ? (
-              <div onClick={plan.linkOnClick} className={linkClass}>
-                <div>{plan.linkText}</div>
-                <div>
-                  <ArrowIcon className="w-4" />
-                </div>
-              </div>
-            ) : (
-              <Link href="https://app.graphql-hive.com" className={linkClass}>
-                <div>{plan.linkText}</div>
-                <div>
-                  <ArrowIcon className="w-4" />
-                </div>
-              </Link>
-            )}
-          </div>
-          <div>
-            <ul className="mt-6 list-disc px-5 text-gray-500">
-              {plan.features.map((feature, i) => (
-                <li key={i} className="mb-2 box-border">
-                  <div className="flex items-center text-sm text-gray-300">{feature}</div>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <article className="w-1/3">
+      <header className="text-green-800">
+        <div className="flex flex-row items-center gap-2">
+          <h2 className="text-2xl font-medium">{props.name}</h2>
+          {props.adjustable && (
+            <span className="whitespace-nowrap rounded-full bg-green-200 px-3 py-1 text-sm font-medium leading-5">
+              Adjust your plan at any time
+            </span>
+          )}
         </div>
-        {plan.footer && (
-          <div>
-            <div className="mx-auto my-4 w-9/12 border-b border-gray-800" />
-            <div className="text-xs text-gray-300">{plan.footer}</div>
-          </div>
-        )}
+        <p className="mt-2">{props.description}</p>
+      </header>
+      <div className="mt-8 text-5xl leading-[56px] tracking-[-0.48px]">{props.price}</div>
+      <div className="pt-6">
+        <CallToAction
+          variant="primary"
+          {...(props.linkOnClick
+            ? {
+                href: '#',
+                onClick: event => {
+                  event.preventDefault();
+                  props.linkOnClick();
+                },
+              }
+            : { href: 'https://app.graphql-hive.com' })}
+        >
+          {props.linkText}
+        </CallToAction>
       </div>
-    </div>
+      <ul className="mt-8 text-green-800">
+        {props.features.map((feature, i) =>
+          feature === PlanFeaturesSeparator ? (
+            <div key={i} className="py-2 font-medium">
+              Plus:
+            </div>
+          ) : (
+            <li key={i} className="border-green-200 py-2 [&+&]:border-t">
+              {feature}
+            </li>
+          ),
+        )}
+      </ul>
+    </article>
   );
 }
 
@@ -89,98 +79,113 @@ const OPERATIONS_EXPLAINER = 'GraphQL operations reported to GraphQL Hive';
 
 export function Pricing(): ReactElement {
   return (
-    <div className="w-full bg-[#111111]">
-      <div className="mx-auto my-12 box-border w-full max-w-screen-lg px-6">
-        <a id="pricing">
-          <h2 className="bg-clip-text text-2xl font-bold leading-normal text-transparent md:text-3xl">
-            Pricing
-          </h2>
-        </a>
-        <p className="text-gray-400">
-          All features are available on all plans - including the free plan. Our pricing is honest
-          and based only on your real usage.
-        </p>
-        <div className="mt-6 flex flex-col content-start items-stretch justify-start justify-items-start gap-4 md:flex-row">
-          <Plan
-            name="Hobby"
-            description="For personal or small projects"
-            price="Free"
-            linkText="Start for free"
-            features={[
-              'Unlimited seats, projects and organizations',
-              'Unlimited schema pushes & checks',
-              'Full access to all features (including SSO)',
-              <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
-                Limit of 1M operations per month
-              </Tooltip>,
-              <Tooltip key="t2" content={USAGE_DATA_RETENTION_EXPLAINER}>
-                7 days of usage data retention
-              </Tooltip>,
-            ]}
-          />
-          <Plan
-            name="Pro"
-            description="For scaling API and teams"
-            price={
-              <Tooltip content="Base price charged monthly">
-                $10<span className="text-sm text-gray-500">/mo</span>
-              </Tooltip>
-            }
-            linkText="Get started"
-            features={[
-              <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
-                + $10 per 1M operations
-              </Tooltip>,
-              'Adjust your plan at any time',
-              <strong key="s1">Everything in Hobby plan, and:</strong>,
-              <Tooltip key="t2" content={USAGE_DATA_RETENTION_EXPLAINER}>
-                90 days of usage data retention
-              </Tooltip>,
-            ]}
-            footer={<div className="text-sm font-bold">🎉 Free 30 days trial period</div>}
-          />
-          <Plan
-            name="Enterprise"
-            description="Custom plan for large companies"
-            price={
-              <span
-                className="cursor-pointer"
-                onClick={() => {
-                  (window as any).$crisp?.push(['do', 'chat:open']);
-                }}
-              >
-                Contact us
-              </span>
-            }
-            linkText="Contact us"
-            linkOnClick={() => {
-              (window as any).$crisp?.push(['do', 'chat:open']);
-            }}
-            features={[
-              <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
-                Custom limit of operations
-              </Tooltip>,
-              'Change your plan at any time',
-              'Improved pricing as you scale',
-              <Tooltip key="t2" content={USAGE_DATA_RETENTION_EXPLAINER}>
-                12 months of usage data retention
-              </Tooltip>,
-              <span key="s1" className="gap-1">
-                GraphQL / APIs support and guidance from{' '}
-                <a
-                  href="https://the-guild.dev"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="font-medium text-yellow-500 transition-colors hover:underline"
+    <section className="py-12 sm:py-20">
+      <div className="mx-auto box-border w-full max-w-[1200px]">
+        <header className="px-6">
+          <Heading as="h2" size="md" className="text-green-1000 text-center">
+            <a id="pricing">Pricing</a>
+          </Heading>
+          <p className="mx-auto mt-4 max-w-xl text-balance text-center text-green-800 lg:text-wrap">
+            All features are available on all plans — including the free&nbsp;plan. Our pricing is
+            honest and based only on your real usage.
+          </p>
+        </header>
+
+        <div
+          // the padding is here so `overflow-auto` doesn't cut button hover states
+          className="-mx-1 overflow-auto px-1"
+        >
+          <div className="mt-16 flex min-w-[1000px] flex-row items-stretch gap-8 px-6 lg:mt-24 lg:gap-10 xl:gap-12 xl:px-0">
+            <Plan
+              name="Hobby"
+              description="For personal or small projects"
+              adjustable={false}
+              price="Free forever"
+              linkText="Start for free"
+              features={[
+                'Unlimited seats, projects and organizations',
+                'Unlimited schema pushes & checks',
+                <>Full access to all features (including&nbsp;SSO)</>,
+                <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
+                  1M operations per month
+                </Tooltip>,
+                <Tooltip key="t2" content={USAGE_DATA_RETENTION_EXPLAINER}>
+                  7 days of usage data retention
+                </Tooltip>,
+              ]}
+            />
+            <Plan
+              name="Pro"
+              description="For scaling API and teams"
+              adjustable
+              price={
+                <Tooltip content="Base price charged monthly">
+                  $10<span className="text-base leading-normal text-green-800"> / month</span>
+                </Tooltip>
+              }
+              linkText="🎉 Try free for 30 days"
+              features={[
+                'Unlimited seats, projects and organizations',
+                'Unlimited schema pushes & checks',
+                <>Full access to all features (including&nbsp;SSO)</>,
+                <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
+                  1M operations per month
+                </Tooltip>,
+                <Tooltip key="t2" content={USAGE_DATA_RETENTION_EXPLAINER}>
+                  90 days of usage data retention
+                </Tooltip>,
+                PlanFeaturesSeparator,
+                <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
+                  $10 per additional 1M operations
+                </Tooltip>,
+              ]}
+            />
+            <Plan
+              name="Enterprise"
+              description="Custom plan for large companies"
+              adjustable
+              price={
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    (window as any).$crisp?.push(['do', 'chat:open']);
+                  }}
                 >
-                  The Guild
-                </a>
-              </span>,
-            ]}
-            footer="Shape a custom plan for your business"
-          />
+                  Contact us
+                </span>
+              }
+              linkText="Contact us for a custom plan"
+              linkOnClick={() => {
+                (window as any).$crisp?.push(['do', 'chat:open']);
+              }}
+              features={[
+                'Unlimited seats, projects and organizations',
+                'Unlimited schema pushes & checks',
+                <>Full access to all features (including&nbsp;SSO)</>,
+                <Tooltip key="t1" content={OPERATIONS_EXPLAINER}>
+                  Custom limit of operations
+                </Tooltip>,
+                <Tooltip key="t2" content={USAGE_DATA_RETENTION_EXPLAINER}>
+                  12 months of usage data retention
+                </Tooltip>,
+                PlanFeaturesSeparator,
+                'Improved pricing as you scale',
+                <span>
+                  GraphQL / APIs support and guidance from{' '}
+                  <a
+                    href="https://the-guild.dev"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="transition-colors hover:underline"
+                  >
+                    The&nbsp;Guild
+                  </a>
+                </span>,
+              ]}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
