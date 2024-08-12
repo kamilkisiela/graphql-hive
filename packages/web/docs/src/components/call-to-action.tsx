546bfbd97917526860c6441d8f3bef712f3cd541
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-12 18:55:20 +0200

    Ecosystem management (#5314)

--- packages/web/docs/src/components/call-to-action.tsx
+++ packages/web/docs/src/components/call-to-action.tsx
@@ -2,34 +2,52 @@ import { ComponentPropsWithoutRef } from 'react';
 import { Anchor } from '@theguild/components';
 import { cn } from '../lib';
 
-interface CallToActionProps extends ComponentPropsWithoutRef<typeof Anchor> {
+// todo: use cva later?
+const variantStyles = {
+  primary: cn(
+    'bg-primary hover:bg-green-800 hover:text-white' +
+      ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800',
+  ),
+  'primary-inverted': cn(
+    'bg-primary hover:bg-white' +
+      ' focus-visible:outline-4 focus-visible:outline-offset-0 focus-visible:outline-white/40',
+  ),
+  secondary: cn(
+    'bg-green-300 hover:bg-green-200' +
+      ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-300/40',
+  ),
+  'secondary-inverted': cn(
+    'bg-green-800 hover:bg-green-700 text-white' +
+      ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800/40',
+  ),
+};
+
+type CallToActionVariant = keyof typeof variantStyles;
+
+export interface CallToActionProps extends ComponentPropsWithoutRef<typeof Anchor> {
   href: string;
-  variant: 'primary' | 'primary-inverted' | 'secondary';
+  variant: CallToActionVariant;
 }
 
 /**
- * This is `Button` in the new design system.
+ * This is `Button` in the new Hive brand design system.
  * TODO: Move it to the components library?
  */
 export function CallToAction({ className, variant, ...rest }: CallToActionProps) {
   return (
     <Anchor
       className={cn(
-        'inline-flex w-fit flex-row items-center gap-2',
-        'text-green-1000 rounded-lg border border-green-800 bg-green-300 px-6 py-3 font-medium leading-6',
+        'relative w-fit rounded-lg',
+        'text-green-1000 px-6 py-3 font-medium leading-6',
         'focus-visible:ring-0 focus-visible:ring-offset-0',
-        // todo: use cva later
-        variant === 'primary'
-          ? 'bg-primary hover:bg-green-800 hover:text-white' +
-              ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-800'
-          : variant === 'primary-inverted'
-            ? 'bg-primary hover:bg-white' +
-              ' focus-visible:outline-4 focus-visible:outline-offset-0 focus-visible:outline-white/40'
-            : 'bg-green-300 hover:bg-green-200' +
-              ' focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-green-300/40',
+        '[&:hover>:first-child]:inset-[-1px] [&:hover>:first-child]:rounded-[9px]',
+        variantStyles[variant],
         className,
       )}
       {...rest}
-    />
+    >
+      <div className="absolute inset-0 rounded-lg border border-green-800 bg-inherit" />
+      <div className="relative flex flex-row items-center gap-2">{rest.children}</div>
+    </Anchor>
   );
 }
