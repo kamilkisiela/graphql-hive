944d54a7a444a1314f861d4ebf438ea6ceb3299d
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-12 18:09:39 +0200

    Make the info cards rounded

--- packages/web/docs/src/components/info-card.tsx
+++ packages/web/docs/src/components/info-card.tsx
@@ -16,7 +16,7 @@ export function InfoCard({
   ...rest
 }: InfoCardProps) {
   return (
-    <Root className={cn('bg-beige-100 p-12', className)} {...rest}>
+    <Root className={cn('bg-beige-100 rounded-3xl p-12', className)} {...rest}>
       <Stud>{icon}</Stud>
       <h3 className="text-green-1000 mt-6 text-xl font-medium leading-[1.4]">{heading}</h3>
       <p className="mt-4 text-green-800">{children}</p>
