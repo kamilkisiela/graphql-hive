3c61f0b064848064f48ea0a27bcff81027cf4190
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-22 14:00:57 +0200

    Team section mobile (#5382)

--- packages/web/docs/src/components/team-section.tsx
+++ packages/web/docs/src/components/team-section.tsx
@@ -9,16 +9,16 @@ export function TeamSection({ className }: { className?: string }) {
   return (
     <section
       className={cn(
-        'flex h-[748px] flex-col flex-wrap justify-center bg-blue-400' +
-          ' p-24 sm:rounded-3xl md:grid-cols-[467px_1fr] md:py-[120px]',
+        'flex flex-col flex-wrap justify-center bg-blue-400 xl:h-[748px]' +
+          ' grid-cols-1 rounded-3xl px-4 py-6 lg:px-8 lg:py-16 xl:px-24 xl:py-[120px]',
         className,
       )}
     >
-      <Heading as="h3" size="md" className="text-green-1000 w-[468px] max-w-full">
+      <Heading as="h3" size="md" className="text-green-1000 max-w-full text-balance xl:w-[468px]">
         Built by The Guild. Industry veterans.
       </Heading>
 
-      <p className="mt-6 hidden w-[468px] max-w-full text-green-800 md:block">
+      <p className="mt-4 w-[468px] max-w-full text-green-800 lg:mt-6">
         Contrary to most, we believe in long-term sight, not temporary growth. We believe in extreme
         quality, not scrappy pivots. We believe in open, not locked. We fight for a world where
         software liberates, not confines — ensuring technology serves, not subjugates.
@@ -29,14 +29,14 @@ export function TeamSection({ className }: { className?: string }) {
         href="https://the-guild.dev/"
         target="_blank"
         rel="noreferrer"
-        className="mt-12"
+        className="max-xl:order-1 max-md:w-full xl:mt-12"
       >
         Visit The Guild
         <ArrowIcon />
       </CallToAction>
 
       <TeamGallery
-        className="hidden w-[636px] xl:flex"
+        className="w-[calc(100%+2rem)] max-xl:-mx-4 max-xl:px-4 max-xl:py-6 xl:w-[636px]"
         style={{
           '--size': '120px',
         }}
@@ -68,7 +68,9 @@ function TeamGallery(props: React.HTMLAttributes<HTMLElement>) {
     <ul
       {...props}
       className={cn(
-        'flex flex-row flex-wrap gap-8' + ' shrink-0 [&>:nth-child(8n-7)]:ml-[calc(var(--size)/2)]',
+        'flex flex-row gap-2 max-lg:overflow-auto lg:flex-wrap lg:gap-4 xl:gap-8' +
+          ' shrink-0 xl:[&>:nth-child(8n-7)]:ml-[calc(var(--size)/2)]' +
+          ' grid-cols-6 items-stretch justify-items-stretch lg:max-xl:grid',
         props.className,
       )}
     >
@@ -84,8 +86,9 @@ function TeamAvatar({ data: [name, avatar, social] }: { data: TeamMember }) {
     <div className="relative">
       <a
         className={
-          'absolute right-0 top-0 rounded-full border-2 bg-[#222530] p-[9px] text-white hover:border-[#222530]' +
-          ' -translate-y-1/2 translate-x-1/2 border-transparent'
+          'absolute right-0 top-0 rounded-2xl border-2 bg-[#222530] p-[9px] text-white hover:border-[#222530] xl:rounded-full' +
+          ' border-transparent xl:-translate-y-1/2 xl:translate-x-1/2' +
+          ' max-xl:min-size-[var(--size)] max-xl:opacity-0'
         }
         href={social}
       >
@@ -101,13 +104,15 @@ function TeamAvatar({ data: [name, avatar, social] }: { data: TeamMember }) {
       </a>
       <div
         role="presentation"
-        className="size-[var(--size)] rounded-2xl bg-['linear-gradient(0deg,#A2C1C4_0%,#A2C1C4_100%),url(var(--src))_lightgray_50%_/_cover_no-repeat'] bg-blue-300"
+        className="aspect-square min-h-[var(--size)] min-w-[var(--size)] rounded-2xl bg-['linear-gradient(0deg,#A2C1C4_0%,#A2C1C4_100%),url(var(--src))_lightgray_50%_/_cover_no-repeat'] bg-blue-300"
         style={{
           '--src': 'https://place.dog/120/120' + '?' + 'name=' + avatar,
           backgroundBlendMode: 'multiply, normal',
         }}
       />
-      <span className="text-green-1000 mt-2 text-sm font-medium leading-5">{name}</span>
+      <span className="text-green-1000 mt-2 block text-sm font-medium leading-5 lg:max-xl:block lg:max-xl:text-base">
+        {name}
+      </span>
     </div>
   );
 }
