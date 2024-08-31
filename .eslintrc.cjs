64997ee2a81308a50726d10c6c2eb75290976c53
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-08-31 03:49:18 +0200

    Allowlist .never-dark

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -223,6 +223,7 @@ module.exports = {
         },
         tailwindcss: {
           callees: tailwindCallees,
+          whitelist: ['never-dark'],
           config: path.join(__dirname, './packages/web/docs/tailwind.config.cjs'),
         },
       },
