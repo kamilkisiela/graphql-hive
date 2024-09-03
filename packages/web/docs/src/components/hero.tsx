44032985d344660a79f06795af6d3c531b22c5e6
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2024-09-03 18:01:02 +0300

    Add font-medium to the hero features list

--- packages/web/docs/src/components/hero.tsx
+++ packages/web/docs/src/components/hero.tsx
@@ -33,7 +33,7 @@ export function HeroLinks(props: { children: ReactNode }) {
 
 export function HeroFeatures(props: { children: ReactNode }) {
   return (
-    <ul className="mx-auto flex list-none flex-col gap-x-6 gap-y-2 text-sm text-white md:flex-row [&>li]:flex [&>li]:items-center [&>li]:gap-2">
+    <ul className="mx-auto flex list-none flex-col gap-x-6 gap-y-2 text-sm font-medium text-white md:flex-row [&>li]:flex [&>li]:items-center [&>li]:gap-2">
       {props.children}
     </ul>
   );
