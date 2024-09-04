e6925d9ee736f4ec5f59342b47e92bdf455039eb
Arda TANRIKULU <ardatanrikulu@gmail.com>
2024-09-04 13:53:48 +0300

    Prettier

--- packages/web/docs/src/pages/docs/dashboard/_meta.ts
+++ packages/web/docs/src/pages/docs/dashboard/_meta.ts
@@ -1,6 +1,6 @@
 export default {
-    insights: 'Insights',
-    explorer: 'Explorer',
-    laboratory: 'Laboratory',
-    'app-deployments': 'App Deployments (Persisted Documents)',
-}
+  insights: 'Insights',
+  explorer: 'Explorer',
+  laboratory: 'Laboratory',
+  'app-deployments': 'App Deployments (Persisted Documents)',
+};
