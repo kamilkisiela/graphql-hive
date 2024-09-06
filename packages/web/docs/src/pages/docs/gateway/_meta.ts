af697579522e54862f5cb824a1928192eea153a3
Dotan Simha <dotansimha@gmail.com>
2024-09-06 13:51:47 +0300

    move page up

--- packages/web/docs/src/pages/docs/gateway/_meta.ts
+++ packages/web/docs/src/pages/docs/gateway/_meta.ts
@@ -2,6 +2,7 @@ export default {
   index: 'Introduction',
   'getting-started': 'Getting Started',
   'schema-registry': 'Schema Registry',
+  'monitoring-tracing': 'Monitoring/Tracing',
   features: 'Features',
   deployment: 'Deployment',
 };
