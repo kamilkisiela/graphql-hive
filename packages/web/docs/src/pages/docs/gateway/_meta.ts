b81548303c94d6d93825a6712b564a1f13dc0b71
Laurin Quast <laurinquast@googlemail.com>
2024-09-09 12:55:47 -0700

    docs: authorization and authentication (#5576)
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- packages/web/docs/src/pages/docs/gateway/_meta.ts
+++ packages/web/docs/src/pages/docs/gateway/_meta.ts
@@ -3,6 +3,7 @@ export default {
   'getting-started': 'Getting Started',
   'usage-reporting': 'Usage Reporting',
   'persisted-documents': 'Persisted Documents',
+  'authorization-authentication': 'Authorization / Authentication',
   'schema-registry': 'Schema Registry',
   features: 'Features',
   deployment: 'Deployment',
