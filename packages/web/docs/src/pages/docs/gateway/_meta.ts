92fbc377de5f30cae7b2162255e9db1669f38f52
Laurin Quast <laurinquast@googlemail.com>
2024-09-09 12:56:03 -0700

    docs: proposal for usage reporting and persisted documents (#5574)
    Co-authored-by: Arda TANRIKULU <ardatanrikulu@gmail.com>

--- packages/web/docs/src/pages/docs/gateway/_meta.ts
+++ packages/web/docs/src/pages/docs/gateway/_meta.ts
@@ -1,6 +1,9 @@
 export default {
   index: 'Introduction',
   'getting-started': 'Getting Started',
+  'usage-reporting': 'Usage Reporting',
+  'persisted-documents': 'Persisted Documents',
+  'authorization-authentication': 'Authorization / Authentication',
   'schema-registry': 'Schema Registry',
   'monitoring-tracing': 'Monitoring/Tracing',
   features: 'Features',
