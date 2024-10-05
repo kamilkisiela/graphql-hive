66e6b57b78d1b1b0431e9fcd740564918775e3f6
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-10-05 13:04:25 +0300

    db types

--- packages/services/storage/src/db/types.ts
+++ packages/services/storage/src/db/types.ts
@@ -276,7 +276,7 @@ export interface schema_checks {
   updated_at: Date;
 }
 
-export interface schema_cleanup_tracker {
+export interface schema_coordinate_status {
   coordinate: string;
   created_at: Date;
   created_in_version_id: string;
@@ -433,7 +433,7 @@ export interface DBTables {
   projects: projects;
   schema_change_approvals: schema_change_approvals;
   schema_checks: schema_checks;
-  schema_cleanup_tracker: schema_cleanup_tracker;
+  schema_coordinate_status: schema_coordinate_status;
   schema_log: schema_log;
   schema_policy_config: schema_policy_config;
   schema_version_changes: schema_version_changes;
